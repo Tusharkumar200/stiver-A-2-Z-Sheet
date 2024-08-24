8c02e068a7949be159536542f94fcb85c57d0618
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-24 22:11:52 +0530

    Increasing_Number_Triangle_Pattern

---
+++ Basics/Increasing_Number_Triangle_Pattern.java
@@ -0,0 +1,23 @@
+
+/*
+Input Format: N = 3
+Result: 
+1
+2 3
+4 5 6
+
+*/
+
+public class Increasing_Number_Triangle_Pattern {
+    
+    public static void INTP(int num){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Increasing Number Triangle Pattern");
+
+        int num = 5;
+        INTP(num);
+    }
+}
