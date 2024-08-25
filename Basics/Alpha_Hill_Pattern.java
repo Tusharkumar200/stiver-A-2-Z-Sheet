a3e0e73ada81477218f5969a0f829f47acdb599b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-25 10:04:53 +0530

    alpha_hill

--- Basics/Alpha_Hill_Pattern.java
+++ Basics/Alpha_Hill_Pattern.java
@@ -11,9 +11,13 @@ ABCBA
 
 public class Alpha_Hill_Pattern {
     
-
+    public static void alpha_hill(int n){
+        
+    }
 
     public static void main(String[] args) {
         
+        int n=3;
+        alpha_hill(n);
     }
 }
