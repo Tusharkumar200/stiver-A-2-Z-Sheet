933eb083d971e5611e5174b66072e57a416863cc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-25 10:55:37 +0530

    AlphaTrianglePattern

---
+++ Basics/AlphaTrianglePattern.java
@@ -0,0 +1,16 @@
+public class AlphaTrianglePattern {
+    /*
+      Input Format: N = 3
+      Result: 
+        C
+        B C
+        A B C
+     */
+    public static void ATP(int n){
+
+    }
+    public static void main(String[] args) {
+        int n =5;
+        ATP(N);
+    }
+}
--- Basics/Alpha_Hill_Pattern.java
+++ Basics/Alpha_Hill_Pattern.java
@@ -13,33 +13,29 @@ public class Alpha_Hill_Pattern {
     
     public static void alpha_hill(int n){
 
-        for(int i=0; i<n; i++){
+       
+        for (int i = 0; i < n; i++) {
 
-            // printing space
-            for(int j=0; j<n-i-1; j++){
-                System.out.print(" ");
+            // printing leading spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  "); // Two spaces for alignment
             }
 
-            // printing the character
+            // printing the characters
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
 
-            char ch= 'A';
-            int breakpoint = (2*i+1)/2;
+            for (int j = 0; j <= 2 * i; j++) {
+                System.out.print(ch + " ");
 
-            for(int j=1;j<= 2*i+1; j++){
-                System.out.print(ch);
-
-                if(j<= breakpoint)ch++;
+                if (j < breakpoint) ch++;
                 else ch--;
             }
 
-            // printing space
-
-            for(int j=0; j<n-i-1; j++){
-                System.out.print(" ");
-            }
+            // move to the next line
             System.out.println();
-
         }
+
     }
 
     public static void main(String[] args) {
