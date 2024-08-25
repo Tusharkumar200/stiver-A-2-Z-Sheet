fbb67b8c56e953be089792e8f1366c9c1cdc76e6
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-25 10:39:29 +0530

    Alpha_Hill_Pattern

--- Basics/Alpha_Hill_Pattern.java
+++ Basics/Alpha_Hill_Pattern.java
@@ -12,12 +12,39 @@ ABCBA
 public class Alpha_Hill_Pattern {
     
     public static void alpha_hill(int n){
-        
+
+        for(int i=0; i<n; i++){
+
+            // printing space
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+
+            // printing the character
+
+            char ch= 'A';
+            int breakpoint = (2*i+1)/2;
+
+            for(int j=1;j<= 2*i+1; j++){
+                System.out.print(ch);
+
+                if(j<= breakpoint)ch++;
+                else ch--;
+            }
+
+            // printing space
+
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
     }
 
     public static void main(String[] args) {
         
-        int n=3;
+        int n=5;
         alpha_hill(n);
     }
 }
