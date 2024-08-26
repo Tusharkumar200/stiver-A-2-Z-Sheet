f86668a635cc30b44fc3a9890708652083a9fe99
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-26 20:25:30 +0530

    Symmetric_Void_Pattern

---
+++ Basics/Symmetric_Void_Pattern.java
@@ -0,0 +1,19 @@
+/*
+ Input Format: N = 3
+Result: 
+******
+**  **
+*    *
+*    *
+**  **
+******
+*/
+
+
+public class Symmetric_Void_Pattern {
+    
+
+    public static void main(String args[]){
+
+    }
+}
