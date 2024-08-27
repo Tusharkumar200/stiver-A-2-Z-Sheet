bbe5b9b57fae6659ad248f383006fbe86846ff60
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-27 18:46:30 +0530

    The_Number_Pattern

--- Basics/The_Number_Pattern.java
+++ Basics/The_Number_Pattern.java
@@ -7,13 +7,27 @@ public class The_Number_Pattern {
 
     public static void TNP(int n){
 
-        
+        int a = 2 * n-1;
+
+        for(int i= 0; i < a; i++){
+
+            for(int j=0; j<a ; j++){
+                
+                int top =i;
+                int bottom = j;
+                int right = (2*n-2)-j;
+                int left = (2*n-2)-i;
+
+                System.out.print(n -Math.min(Math.min(top, bottom) , Math.min(left , right) ) +" ");
+            }
+            System.out.println();
+        }
 
     }
     
     public static void main(String[] args) {
         
-        int n= 5;
+        int n= 3;
         TNP(n);
     }
 }
