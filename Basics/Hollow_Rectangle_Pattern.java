d68e4e4f5705aaa50be2dddda48dd2286537e45d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-27 18:30:30 +0530

    The_Number_Pattern

--- Basics/Hollow_Rectangle_Pattern.java
+++ Basics/Hollow_Rectangle_Pattern.java
@@ -12,12 +12,22 @@ public class Hollow_Rectangle_Pattern {
     
     public static void HRP(int n){
 
-        for(int i=1; i<=n;i++ ){
-            System.out.println("* ");
+        for(int i=0; i<n;i++ ){
+
+            for(int j=0; j<n; j++){
+                if( i==0 || j==0 || i==n-1 || j==n-1){
+
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
-        int N = 3;
+        int N = 5;
         HRP(N);
     }
 }
---
+++ Basics/The_Number_Pattern.java
@@ -0,0 +1,18 @@
+/*
+ 
+ */
+
+
+public class The_Number_Pattern {
+
+    public static void TNP(int n){
+
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        int n= 5;
+        TNP(n);
+    }
+}
