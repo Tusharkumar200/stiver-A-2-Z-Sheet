9ccca4bcddf750b1b2cfb59edc45df4087590e34
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-27 14:38:35 +0530

    Symmetric_Butterfly_Pattern

--- Basics/Symmetric_Butterfly_Pattern.java
+++ Basics/Symmetric_Butterfly_Pattern.java
@@ -15,17 +15,35 @@ public class Symmetric_Butterfly_Pattern {
    
     public static void SBP(int n){
 
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=n; j++){
+        int space = 2*n-2;
+
+        for(int i=1; i<=2*n-1; i++){
+            
+            int stars =i;
+            if(i>n) stars=2*n-i;
+
+            
+            for(int j=1; j<=stars; j++){
+                System.out.print("*");
+            }
+
+            for(int j=1; j<=space;j++){
+
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<= stars;j++){
                 System.out.print("*");
             }
             System.out.println();
+            if(i<n) space -=2;
+            else space +=2;
         }
     }
    
     public static void main(String[] args) {
          
-        int n=3;
+        int n=6;
         SBP(n);
         
     }    
