073340a956ff8d19fe5658de52396721a3329635
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-28 23:02:55 +0530

    countDigits

---
+++ Basics/know_the_basic_math/count_Digits.java
@@ -0,0 +1,27 @@
+
+
+public class count_Digits {
+
+    public static void countDigits(int n){
+
+        int count = 0;
+        // n --> 12345
+         
+        for( int i=0; i<=10;i++){
+
+            int mod = n%10;
+            
+        }
+        System.out.println(count);
+
+
+        
+    }
+
+    public static void main(String[] args){
+
+        int n = 12345;
+        countDigits(n);
+        System.out.println("hello");
+    }
+}
