947eda80ba1e61e6e0e1e010a5f15f385276eb65
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-30 22:41:35 +0530

    isPalindrome

--- Basics/know_the_basic_math/Check_if_a_number_is_Palindrome_or_Not.java
+++ Basics/know_the_basic_math/Check_if_a_number_is_Palindrome_or_Not.java
@@ -1,13 +1,30 @@
 public class Check_if_a_number_is_Palindrome_or_Not {
     
     public static boolean isPalindrome(int n){
-         
+        //  12321
         
+        int dup = n;
+
+        
+        int revNo= 0;
+        while(n>0){
+            
+            int ld  = n % 10;
+            revNo = (revNo * 10) + ld;
+
+            n = n/10;
+        }
+
+        if(dup == revNo){
+            return true;
+        }
+
         return false;
         
     }
     public static void main(String[] args) {
         int n = 12321;
-        isPalindrome(n);
+        boolean result = isPalindrome(n);
+        System.err.println("the given no is :"+ result);
     }
 }
