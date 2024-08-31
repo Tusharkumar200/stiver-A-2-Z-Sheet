f343018a1c3822cf1fc6b8141c347b6bf9682b4c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-31 21:35:20 +0530

    isArmstrongNumber

--- Basics/know_the_basic_math/isArmstrongNumber.java
+++ Basics/know_the_basic_math/isArmstrongNumber.java
@@ -1,7 +1,30 @@
+import java.lang.Math;
 
 public class isArmstrongNumber{
 
+    public static boolean isArmstrong(int num){
+
+        int k = String.valueOf(num).length();
+        int sum = 0;
+        
+        int n = num;
+        while (n >0) {
+            int ld = n % 10;
+
+            sum += Math.pow(ld, k);
+            num = n /10;
+        }
+        
+        return num == sum ? true : false;
+        
+    }
     public static void main (String[] args){
 
+        int number = 153;
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
     }
 }
