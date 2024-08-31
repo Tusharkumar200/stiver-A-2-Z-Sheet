5fc703b5395475f6303aeb413b5055abfedd67c7
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-31 23:29:26 +0530

    prime

---
+++ Basics/know_the_basic_math/isPrimeNo.java
@@ -0,0 +1,38 @@
+public class isPrimeNo {
+    
+    static boolean prime(int n) {
+        // Initialize a counter variable to
+        // count the number of factors.
+        int cnt = 0;
+        // Loop through numbers from 1 to n.
+        for (int i = 1; i <= n; i++) {
+            // If n is divisible by i
+            // without any remainder.
+            if (n % i == 0) {
+                // Increment the counter.
+                cnt = cnt + 1;
+            }
+        }
+
+        // If the number of
+        // factors is exactly 2
+        if (cnt == 2) {
+            // Return true, indicating
+            // that the number is prime.
+            return true;
+        }
+        // If the number of
+        // factors is not 2.
+        else {
+            // Return false, indicating
+            // that the number is not prime.
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int  num= 1;
+
+        boolean result = prime(num);
+        System.out.println(result);
+    }
+}
