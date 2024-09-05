65863b70fccd152d2d68759ab47bf3ad3c5411b1
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-05 19:00:03 +0530

    solved

--- Basics/Basic_Recursion/FibonacciNumber.java
+++ Basics/Basic_Recursion/FibonacciNumber.java
@@ -1,3 +1,29 @@
+/*
+
+Input: N = 5
+Output: 0 1 1 2 3 5
+Explanation: 0 1 1 2 3 5 is the fibonacci series up to 5th term.(0 based indexing)
+
+*/
+
+
 public class FibonacciNumber {
     
+    public static int fib(int n){
+        if(n <= 1) return n;
+
+
+        int first = fib(n-1);
+        int second = fib(n-2);
+        
+
+        int sum = first + second;
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        
+        int n = 11;
+        System.out.println(fib(n));
+    }
 }
