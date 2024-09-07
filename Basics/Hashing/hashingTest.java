5832982a50533f160e3a5c8cc526ff3bc26d5889
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-07 23:30:57 +0530

    Testing Done

--- Basics/Hashing/hashingTest.java
+++ Basics/Hashing/hashingTest.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 
+
+/*
+ Input: 
+
+5
+1 3 2 1 3
+5
+1 4 2 3 12
+
+Output: 
+
+2
+0
+1
+2
+0
+
+ */
 public class hashingTest{
 
     public static void main(String[] args) {
