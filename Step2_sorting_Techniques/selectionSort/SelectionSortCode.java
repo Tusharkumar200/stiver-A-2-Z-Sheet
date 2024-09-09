fd3b9d356ccbc55c2eb62091ed8e8d082a2ab188
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-09 15:55:45 +0530

    tested

--- Step2_sorting_Techniques/selectionSort/SelectionSortCode.java
+++ Step2_sorting_Techniques/selectionSort/SelectionSortCode.java
@@ -1,12 +1,36 @@
 
 class SelectionSortCode{
 
-    public static void ssort(int num , int[] arr){
+    public static void ssort(int n , int[] arr){
 
+        for(int i=0; i<=n-2; i++){
+
+            int min =i;
+            for(int j =i; j<=n-2;i++){
+
+                if(arr[j] <arr[min]) min = j;
+
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+
+            for(int k =0; k<=arr[i];k++){
+                System.out.println(arr[k]);
+            }
+        }
         
+
     }
 
     public static void main(String[] args) {
         
+        int n = 5;
+        int arr[] = {90,50,70,1,8};
+         ssort(n , arr);
+
+
+
     }
 }
