52ee9bb6121c18abba04ba7c3d44722b9f763e4b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-10 23:30:47 +0530

    bubblesortCode

--- Step2_sorting_Techniques/BubbleSort/bubblesortCode.java
+++ Step2_sorting_Techniques/BubbleSort/bubblesortCode.java
@@ -1,7 +1,30 @@
 public class bubblesortCode {
     
-    
+    public static void code(int[] arr){
+
+        for(int i=0; i<=arr.length; i++){
+
+            for(int j=i+1; j<arr.length;j++){
+
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[]arr){
+
+        for(int i=0; i<=arr.length-1; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String []args){
 
+        int arr[]={82,40,60,98,78,25};
+        code(arr);
+        printArray(arr);
     }
 }
