41b71c3e75df2a8e81685f2f215d3aeb6a23aaf0
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-12 12:01:32 +0530

    mergesort done

--- Step2_sorting_Techniques/MergeSort/mergesort.java
+++ Step2_sorting_Techniques/MergeSort/mergesort.java
@@ -1,8 +1,50 @@
+import java.util.*;
+
 public class mergesort {
 
 
-    public static void sort(int arr[]){
-        
+    public static void merge(int[] arr, int low, int mid, int high){
+
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1; 
+
+        while (left <= mid && right <= high) {
+            
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++; 
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i=low; i <= high; i++){
+            arr[i] = temp.get(i - low);
+        }
+
+
+    }
+
+    public static void sort(int[]arr, int low , int high){
+        if(low >= high) return;
+        int mid = (low + high) / 2;
+        sort(arr, low, mid);
+        sort(arr, mid+1, high);
+        merge(arr, low, mid, high);
+
     }
     public static void printArray(int arr[]){
         for (int i =0; i<arr.length; i++) {
@@ -11,7 +53,13 @@ public class mergesort {
     }
     public static void main(String[] args) {
         int arr[] = {50,80,10,20,30,6,5};
-        sort(arr);
+        
+        System.out.println("Given array is");
+        printArray(arr);
+
+        sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array is");
         printArray(arr);
     }
 }
