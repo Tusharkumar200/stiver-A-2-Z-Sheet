93e307e22f8b7ab97c9fe881ab815c75eab965f6
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-12 14:22:09 +0530

    quicksort

---
+++ Step2_sorting_Techniques/QuickSort/quicksort.java
@@ -0,0 +1,14 @@
+class quicksort{
+
+    static void quick(int arr[], int low, int high){
+
+    }
+
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+    } 
+    public static void main(String[] args) {
+        
+    }
+}
