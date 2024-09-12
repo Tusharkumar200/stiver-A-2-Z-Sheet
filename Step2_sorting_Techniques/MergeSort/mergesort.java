58357b323c523d4a95741f556797a8df7d33bf71
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-12 11:27:59 +0530

    Merge Sort

---
+++ Step2_sorting_Techniques/MergeSort/mergesort.java
@@ -0,0 +1,17 @@
+public class mergesort {
+
+
+    public static void sort(int arr[]){
+        
+    }
+    public static void printArray(int arr[]){
+        for (int i =0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {50,80,10,20,30,6,5};
+        sort(arr);
+        printArray(arr);
+    }
+}
