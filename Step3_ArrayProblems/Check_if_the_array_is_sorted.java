c8046b06076688bcabf8e09478310ea5c3f930aa
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-14 14:12:00 +0530

    Check_if_the_array_is_sorted

---
+++ Step3_ArrayProblems/Check_if_the_array_is_sorted.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Check_if_the_array_is_sorted {
+    
+    public static boolean isSorted(int arr[]){
+
+        int save[] = arr.clone();
+        Arrays.sort(save);
+        boolean check = Arrays.equals(save, arr);
+        
+        
+        if(check){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+        // int sort[] = {10,20,30,40,50};
+        int unsort[] = {5,4,21,8,6,3,0};
+
+        boolean result = isSorted(unsort);
+        // boolean result = isSorted(sort);
+        System.out.println(result);
+    }
+}
