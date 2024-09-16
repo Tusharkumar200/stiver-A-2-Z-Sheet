f5391f119d8e4f8eadc493d8fc3e9c41be8e937a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-16 18:32:31 +0530

    rotateByK

---
+++ Step3_ArrayProblems/Rotate_array_by_K_elements.java
@@ -0,0 +1,13 @@
+public class Rotate_array_by_K_elements {
+    
+    public static void rotateByK(int arr[], int size, int k){
+
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {6,7,1,2,3,4,5};
+        int size = arr.length;
+        int k = 2;
+        rotateByK(arr, size,k);
+    }
+}
