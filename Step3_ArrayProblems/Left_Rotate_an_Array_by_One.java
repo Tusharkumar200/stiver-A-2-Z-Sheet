ab4a2b7a325a2fe0b9d7d61671b9fcf16fec46f8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-16 16:10:15 +0530

    Left_Rotate_an_Array_by_One

--- Step3_ArrayProblems/Left_Rotate_an_Array_by_One.java
+++ Step3_ArrayProblems/Left_Rotate_an_Array_by_One.java
@@ -1,17 +1,23 @@
+/*
+ *Input: 'a' = 5, 'arr' = [1, 2, 3, 4, 5]
+
+Output: [2, 3, 4, 5, 1]
+ */
+
 public class Left_Rotate_an_Array_by_One {
     
     public static void rotate(int arr[]){
         int n = arr.length;
         int temp = arr[0];
-        for(int i=0; i<n;i++){
+        arr[n-1] = temp;
+        for(int i=1; i<n;i++){
             System.out.print(arr[i] +" ");
         }
 
-        for(int j =1; j<n-1; j++){
-            arr[n-1] = temp;
-
+        // for(int j =1; j<n-1; j++){
+        // }
             
-        }
+            
         System.out.println("Rotate array :");
         for( int k: arr){
             System.out.print(k +" ");
