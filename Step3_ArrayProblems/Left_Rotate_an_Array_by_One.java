c9e6507ca2991cf82e56cbb861e08ecf4ef8720e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-16 17:47:37 +0530

    Rotation done

--- Step3_ArrayProblems/Left_Rotate_an_Array_by_One.java
+++ Step3_ArrayProblems/Left_Rotate_an_Array_by_One.java
@@ -1,7 +1,7 @@
 /*
  *Input: 'a' = 5, 'arr' = [1, 2, 3, 4, 5]
 
-Output: [2, 3, 4, 5, 1]
+*Output: [2, 3, 4, 5, 1]
  */
 
 public class Left_Rotate_an_Array_by_One {
@@ -9,16 +9,21 @@ public class Left_Rotate_an_Array_by_One {
     public static void rotate(int arr[]){
         int n = arr.length;
         int temp = arr[0];
-        arr[n-1] = temp;
-        for(int i=1; i<n;i++){
+
+        System.out.println("previous array ");
+        for(int i=0; i<n;i++){
             System.out.print(arr[i] +" ");
         }
 
-        // for(int j =1; j<n-1; j++){
-        // }
+        for(int j =0; j<n-1; j++){
+
+            arr[j] = arr[j+1];
+            
+        }
+        arr[n-1 ]=temp;
             
             
-        System.out.println("Rotate array :");
+        System.out.println("\n Rotate array :");
         for( int k: arr){
             System.out.print(k +" ");
         }
