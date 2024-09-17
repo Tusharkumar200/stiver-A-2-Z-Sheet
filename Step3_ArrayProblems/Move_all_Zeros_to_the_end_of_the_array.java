7bf38b3f6bfc952676c07f3a88343e50a281500f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-17 21:56:01 +0530

    Move_all_Zeros_to_the_end_of_the_array

---
+++ Step3_ArrayProblems/Move_all_Zeros_to_the_end_of_the_array.java
@@ -0,0 +1,11 @@
+public class Move_all_Zeros_to_the_end_of_the_array {
+    
+    public static void MoveZero(int arr[]){
+
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = {0,1,0,3,12};
+        MoveZero(arr);
+    }
+}
--- Step3_ArrayProblems/Rotate_array_by_K_elements.java
+++ Step3_ArrayProblems/Rotate_array_by_K_elements.java
@@ -10,35 +10,37 @@
 public class Rotate_array_by_K_elements {
 
 
-    public static void reverse(int [] arr , int start , int end){
+    public static void reverse(int [] array , int start , int end){
 
-            while (start <= end) {
-                int temp = arr[start];
-
-                arr[start] = arr[end];
-                arr[end] = temp;
-
-                start++;
-                end--;
-            }
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
     }
 
-    public static void rotateByK(int arr[], int n, int k) {
-
+    public static void rotateByK(int arr[], int k) {
+        if (arr == null || arr.length == 0 || k <= 0) {
+            return;
+        }
+        int n = arr.length;
+        k = k % n;
+        reverse(arr,0 ,n-1);
         reverse(arr, 0, k-1);
-        reverse(arr, k,n-1);
-        reverse(arr,0,n-1);
+        reverse(arr,k,n-1);
 
          
-        
+        Move_all_Zeros_to_the_end_of_the_array
     }
 
     public static void main(String[] args) {
 
         int arr[] = {  1, 2, 3, 4, 5 ,6,7};
-        int size = arr.length;
+        
         int k = 3;
-        rotateByK(arr, size, k);
+        rotateByK(arr, k);
 
 
         for (int i = 0; i < arr.length; i++) {
