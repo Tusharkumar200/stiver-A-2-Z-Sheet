635d658ca271a86ecc0675c943892074be3305cd
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-18 22:07:30 +0530

    problem solved

--- Step3_ArrayProblems/Find_the_missing_number_in_an_array.java
+++ Step3_ArrayProblems/Find_the_missing_number_in_an_array.java
@@ -1,19 +1,26 @@
 public class Find_the_missing_number_in_an_array {
     
-    public static void missingNumber(int arr[] ){
-        int n = arr.length;
-        int i;
-        for(i = 0; i< n -1; i++){
-            System.out.println(arr[i]);
+    public static int  missingNumber(int a[] ){
+        int N = a.length;
 
+        int sum = (N * (N + 1)) / 2;
+
+        //Summation of all array elements:
+        int s2 = 0;
+        for (int i = 0; i < N - 1; i++) {
+            s2 += a[i];
         }
-        if( i != arr[i]){
-            System.out.println(i);
-        }
+
+        int missingNum = sum - s2;
+        return missingNum;
     }
     public static void main(String[] args) {
         int arr[] = {1,2,4,5};
         
-        missingNumber(arr);
+        // int res = missingNumber(arr);
+         int res = missingNumber(arr);
+        
+            System.out.println(res);
+        
     }
 }
