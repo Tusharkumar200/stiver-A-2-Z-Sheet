3916a52562b029cd74a2def6d4573a45203c6dd0
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-19 22:54:45 +0530

    optimize solution

--- Step3_ArrayProblems/Union_of_Two_Sorted_Arrays.java
+++ Step3_ArrayProblems/Union_of_Two_Sorted_Arrays.java
@@ -3,38 +3,75 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Union_of_Two_Sorted_Arrays {
-    
-   public static List< Integer > unionArray (int []a, int []b) {
 
+    public static List<Integer> unionArray(int[] a, int[] b) {
 
-        HashSet <Integer> hs = new HashSet<>();
-        ArrayList <Integer> al = new ArrayList<>();
+        HashSet<Integer> hs = new HashSet<>();
+        ArrayList<Integer> al = new ArrayList<>();
 
-        for(int i : a){
+        for (int i : a) {
             hs.add(i);
         }
-        for(int i : b){
+        for (int i : b) {
             hs.add(i);
         }
 
-        for( int j: hs){
+        for (int j : hs) {
 
             al.add(j);
         }
 
-        for(int i=0;i<al.size();i++){
+        for (int i = 0; i < al.size(); i++) {
             System.out.println(al.get(i));
         }
         return al;
 
     }
 
-    public static void main(String[] args) {
-        
-        int arr1[] = {1,2,3,4,6};
-        int arr2[] = {2,3,5,7};
+    public static List<Integer> Union2(int[] arr1, int[] arr2) {
+        int i = 0, j = 0; // pointers
+        int n = arr1.length;
+        int m = arr2.length;
+        ArrayList<Integer> Union = new ArrayList<>(); // Uninon vector
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) // Case 1 and 2
+            {
+                if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i])
+                    Union.add(arr1[i]);
+                i++;
+            } else // case 3
+            {
+                if (Union.size() == 0 || Union.get(Union.size() - 1) != arr2[j])
+                    Union.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < n) // IF any element left in arr1
+        {
+            if (Union.get(Union.size() - 1) != arr1[i])
+                Union.add(arr1[i]);
+            i++;
+        }
+        while (j < m) // If any elements left in arr2
+        {
+            if (Union.get(Union.size() - 1) != arr2[j])
+                Union.add(arr2[j]);
+            j++;
+        }
 
-         unionArray(arr1, arr2);
+        for(int k: Union){
+            System.out.print(k +" ");
+        }
+        return Union;
+    }
+
+    public static void main(String[] args) {
+
+        int arr1[] = { 1, 2, 3, 4, 6 };
+        int arr2[] = { 2, 3, 5, 7 };
+
+        // unionArray(arr1, arr2);
+        Union2(arr1, arr2);
 
     }
 }
