9540224b30c46d8e9a5d47888ac47b24c874c526
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-20 22:50:05 +0530

    Find_The_Single_Element

---
+++ Step3_ArrayProblems/Find_The_Single_Element.java
@@ -0,0 +1,18 @@
+public class Find_The_Single_Element {
+    
+
+    public static int findSingle(int arr[]){
+
+        int  single = 0;
+
+        for(int i: arr) single = single ^ i;
+        return single;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {1, 1, 2,8, 3, 3, 4, 4};
+        int result = findSingle(arr);
+        System.out.println(result);
+    }
+}
--- Step3_ArrayProblems/Max_Consecutive_Ones.java
+++ Step3_ArrayProblems/Max_Consecutive_Ones.java
@@ -2,12 +2,27 @@ public class Max_Consecutive_Ones {
 
     static int findMaxConsecutiveOnes(int nums[]) { 
 
-        
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i : nums){
+
+            if(i == 1){
+                count++;
+            }
+            else{
+                count = 0;
+            }
+
+            maxCount = Math.max(count, maxCount);
+        }
+
+        return maxCount;
 
     }
         public static void main(String[] args) {
         
-        int price[] = {1,1,0,1,1,1};
+        int price[] = {1,1,0,1,1,1,0,1,1,1};
 
         int result = findMaxConsecutiveOnes(price);
         System.out.println(result +" ");
