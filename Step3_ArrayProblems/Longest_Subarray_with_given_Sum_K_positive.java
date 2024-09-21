206652440db5bccfcfcd41059b1b66a0b32c9de8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-21 20:37:50 +0530

    Longest_Subarray_with_given_Sum_K_positive

---
+++ Step3_ArrayProblems/Longest_Subarray_with_given_Sum_K_positive.java
@@ -0,0 +1,27 @@
+/*
+Example 1:
+    Input Format: N = 3, k = 5, array[] = {2,3,5}
+    Result: 2
+    Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+Example 2:
+    Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+    Result: 3
+    Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+ */
+
+class Longest_Subarray_with_given_Sum_K_positive{
+
+    public static void Longest_Subarray(int []arr , int k){
+
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {2,3,5,1,9};
+        int k = 10;
+        Longest_Subarray(arr , k);
+
+    }
+}
