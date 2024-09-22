98e2d72ca863e26b524f98806fe952ae026d9f79
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-22 15:03:03 +0530

    Sort_ZeroOneAndTwo

---
+++ Step3_ArrayProblems/Medium/Sort_ZeroOneAndTwo.java
@@ -0,0 +1,19 @@
+package Medium;
+
+import java.util.Arrays;
+
+public class Sort_ZeroOneAndTwo {
+    
+    
+    public static void main(String[] args) {
+
+        int arr[] = {0 ,1, 2, 2, 1, 0};
+         Arrays.sort(arr);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+        
+    }
+}
