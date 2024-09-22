36b5e4bc7878b19e51c91df9784e52a144e9804a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-22 11:32:11 +0530

    Two_Sum

---
+++ Step3_ArrayProblems/Medium/Two_Sum.java
@@ -0,0 +1,15 @@
+package Medium;
+
+public class Two_Sum {
+    
+    public static int sum(int arr[] , int target){
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = {2,6,5,8,11};
+        int target = 14;
+        sum(arr, target);
+    }
+}
