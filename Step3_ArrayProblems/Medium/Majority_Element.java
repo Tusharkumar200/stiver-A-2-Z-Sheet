6fea526fe2acb0a8ca3ff726a6cedff52009156a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-23 21:50:38 +0530

    Majority_Element

---
+++ Step3_ArrayProblems/Medium/Majority_Element.java
@@ -0,0 +1,16 @@
+package Medium;
+
+import java.util.HashMap;
+
+public class Majority_Element {
+    
+    public static void majority(int num[]){
+            HashMap<Integer , Integer> map = new HashMap<>();
+            
+    }
+    public static void main(String[] args) {
+        int  nums[] = {3,2,3};
+        majority(nums);
+        
+    }
+}
