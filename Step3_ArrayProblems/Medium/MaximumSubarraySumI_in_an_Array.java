0a06bbe7bc79328250ad763b642d1cc68d5a271f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-24 17:04:00 +0530

    maxSubArray

--- Step3_ArrayProblems/Medium/MaximumSubarraySumI_in_an_Array.java
+++ Step3_ArrayProblems/Medium/MaximumSubarraySumI_in_an_Array.java
@@ -14,7 +14,25 @@ Explanation:
  */
 public class MaximumSubarraySumI_in_an_Array {
     
+    
+    public static int maxSubArray(int[] arr){
+
+        int sum = 0;
+        int maxi = arr[0];
+
+        for(int i : arr){
+            sum += i;
+
+            if(sum > maxi) {maxi = sum;}
+            if(sum < 0) {sum  = 0;}
+        }
+        return maxi;
+    }
     public static void main(String[] args) {
-        
+        int arr []= {-2,1,-3,4,-1,2,1,-5,4};
+
+        int result = maxSubArray(arr);
+
+        System.out.println(result);
     }
 }
