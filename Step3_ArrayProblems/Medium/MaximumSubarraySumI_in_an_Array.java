9205e17a735657c36d4a4672680a73d3f3e04a82
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-25 23:55:26 +0530

    Stock_Buy_And_Sell

--- Step3_ArrayProblems/Medium/MaximumSubarraySumI_in_an_Array.java
+++ Step3_ArrayProblems/Medium/MaximumSubarraySumI_in_an_Array.java
@@ -30,7 +30,7 @@ public class MaximumSubarraySumI_in_an_Array {
     }
     public static void main(String[] args) {
         // int arr []= {-2,1,-3,4,-1,2,1,-5,4};
-        int arr []= {4, 3, 1, 5, 6};
+        int arr []= {4, 3, 1, 5, 6  };
 
         int result = maxSubArray(arr);
 
---
+++ Step3_ArrayProblems/Medium/Stock_Buy_And_Sell.java
@@ -0,0 +1,10 @@
+package Medium;
+
+public class Stock_Buy_And_Sell {
+    
+    public static void main(String[] args) {
+        
+        int prices = {7,1,5,3,6,4};
+        
+    }
+}
