a0d939b88ee25c2eb866fa78e5d3b5ffde47a478
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-26 00:02:50 +0530

    BruteForce Approched

--- Step3_ArrayProblems/Medium/Stock_Buy_And_Sell.java
+++ Step3_ArrayProblems/Medium/Stock_Buy_And_Sell.java
@@ -1,10 +1,30 @@
 package Medium;
 
 public class Stock_Buy_And_Sell {
+
+    // BruteForce Approched
+    public static int stock(int []arr){
+       int  maxPro = 0;
+        
+        for(int i =0; i<arr.length; i++){
+
+            for(int j = i+1; j < arr.length; j++){
+
+                if(arr[j] > arr[i]){
+
+                    maxPro = Math.max(arr[j] - arr[i] , maxPro);
+                }
+            }
+        }
+        return maxPro;
+    }
     
     public static void main(String[] args) {
         
-        int prices = {7,1,5,3,6,4};
-        
+        int prices[] = {7,1,5,3,6,4};
+
+        int result = stock(prices);
+        System.out.println(result);
+
     }
 }
