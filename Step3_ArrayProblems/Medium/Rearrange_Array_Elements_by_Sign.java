dee9cacd6788f47aaa37213f9d3b3021b1f9e2e1
Tushar Kumar <tusharnwd2003@gmail.com>
2024-09-27 11:49:33 +0530

    Rearrange_Array_Elements_by_Sign

--- Step3_ArrayProblems/Medium/Rearrange_Array_Elements_by_Sign.java
+++ Step3_ArrayProblems/Medium/Rearrange_Array_Elements_by_Sign.java
@@ -1,10 +1,40 @@
 package Medium;
 
-
-
 public class Rearrange_Array_Elements_by_Sign{
 
-    public static void main(String[] args) {
+    public static int[] reArrange(int[]arr){
+        int n = arr.length/2;
+
+        int pos[] = new int[n];
+        int neg[] = new int[n];
+        int posIndex = 0;
+        int negIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(arr[i] >= 0){
+                pos[posIndex++] = arr[i];
+            }
+            else{
+                neg[negIndex++] = arr[i];
+            }
+        }
+
         
+
+        for (int i = 0; i < n ; i++) {
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, -3, -1, -2, 3};
+        int res[] = reArrange(arr);
+        for (int i : res) {
+            
+            System.out.print(i+" ");
+        }
     }
 } 
