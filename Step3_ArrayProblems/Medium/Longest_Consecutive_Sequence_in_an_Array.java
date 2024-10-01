d283aa1e6a27c01bc87b20ea53ff9324080c3c85
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-01 18:00:37 +0530

    longestSuccessiveElementsOptimal

--- Step3_ArrayProblems/Medium/Longest_Consecutive_Sequence_in_an_Array.java
+++ Step3_ArrayProblems/Medium/Longest_Consecutive_Sequence_in_an_Array.java
@@ -1,42 +1,81 @@
 package Medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Longest_Consecutive_Sequence_in_an_Array {
 
-    public static boolean linearSearch(int[]a , int num){
+    // Brute Force
 
-       
-        for(int i: a){
+    public static boolean linearSearch(int[] a, int num) {
 
-            if(i == num) return true;
+        for (int i : a) {
+
+            if (i == num)
+                return true;
         }
         return false;
     }
 
-    public static int longestSuccessiveElements(int []a){
+    public static int longestSuccessiveElements(int[] a) {
 
-        
-        int longest  =1;
+        int longest = 1;
 
-
-        for(int i: a ){
+        for (int i : a) {
             int x = i;
             int cnt = 1;
 
+            while (linearSearch(a, x + 1) == true) {
 
-            while (linearSearch(a, x+1) == true) {
-                
-                x+=1;
+                x += 1;
                 cnt += 1;
             }
 
-            longest  = Math.max(cnt, longest);
+            longest = Math.max(cnt, longest);
         }
         return longest;
     }
-    
+
+    // Optimal Approched
+
+    public static int longestSuccessiveElementsOptimal(int[] a) {
+
+        int n = a.length;
+        if (n == 0)
+            return 0;
+
+        int longest = 1;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : a) {
+            set.add(i);
+        }
+
+        for (int it : set) {
+
+            int cnt = 1;
+            int x = it;
+
+            if (!set.contains(it - 1)) {
+
+                while (set.contains(x + 1)) {
+
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+
     public static void main(String[] args) {
-        int[] a = {100, 200, 1, 2, 3, 4};
-        int ans = longestSuccessiveElements(a);
+        int[] a = { 100, 200, 1, 2, 3, 4 };
+        // int ans = longestSuccessiveElements(a);
+        int ans = longestSuccessiveElementsOptimal(a);
+
         System.out.println("The longest consecutive sequence is " + ans);
     }
 }
