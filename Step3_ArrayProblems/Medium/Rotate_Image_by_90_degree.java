76fc231853981e116dd353bde5c4bae04a9934ad
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-04 22:49:15 +0530

    Rotate_Image_by_90_degree

--- Step3_ArrayProblems/Medium/Rotate_Image_by_90_degree.java
+++ Step3_ArrayProblems/Medium/Rotate_Image_by_90_degree.java
@@ -1,8 +1,25 @@
 package Medium;
 
 public class Rotate_Image_by_90_degree {
+
+    public static void rotate(int arr[][]){
+
+        int n = arr.length;
+
+        int rotated[][]  = new int[n][n];
+        for(i: arr){
+
+            for(int j: arr){
+
+                rotated[j][n-i-1] = i j;
+            }
+        }
+    }
     
     public static void main(String[] args) {
+        
+        int arr [][]={{1,2,3},{4,5,6},{7,8,9}};
+        rotate(arr);
         
     }
 }
