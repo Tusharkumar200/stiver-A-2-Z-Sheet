f2af77fef7d910176c355cda1832b94d4bc2cd86
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-05 13:48:42 +0530

    BruteForce Approched

--- Step3_ArrayProblems/Medium/Rotate_Image_by_90_degree.java
+++ Step3_ArrayProblems/Medium/Rotate_Image_by_90_degree.java
@@ -2,21 +2,23 @@ package Medium;
 
 public class Rotate_Image_by_90_degree {
 
-    public static int rotate(int arr[][]){
+    // BruteForce Approched
+    public static int[][] rotate(int matrix[][]){
 
-        int n = arr.length;
-
-        int rotated[][]  = new int[n][n];
-        for(int i =0; i< n ; i++){
-
-            for(int j =0 ; j < n; j++){
-
-                rotated[j][n-i-1] =arr[i][j];
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
             }
         }
         return rotated;
     }
     
+
+    public static void optimal_rotate(int[][] matrix){
+        
+    }
     public static void main(String[] args) {
         
         int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
