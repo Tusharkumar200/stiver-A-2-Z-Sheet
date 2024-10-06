fb623e1bda50caab0abb2f12281a04414bc9f8a0
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-06 15:50:12 +0530

    Spiral_Traversal_of_Matrix

--- Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
+++ Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
@@ -1,18 +1,50 @@
-
-
+package Medium;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Spiral_Traversal_of_Matrix {
 
     public static List<Integer> printSpiral(int[][] mat) {
 
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int top = 0, bottom = n - 1, left = 0, right = m - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                ans.add(mat[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                ans.add(mat[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i > left; i--) {
+                    ans.add(mat[bottom][i]);
+                }
+            }
+            bottom--;
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(mat[i][left]);
+                }
+            }
+            left++;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[][] mat = { { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 } };
+        int[][] mat = {
+             { 1, 2, 3, 4 },
+             { 5, 6, 7, 8 },
+             { 9, 10, 11,12 },
+             { 13, 14, 15,16 } };
 
         List<Integer> ans = printSpiral(mat);
 
