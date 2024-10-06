adf8a9adcf4121894f34037a2b478bb51097dd46
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-06 15:11:42 +0530

    spiral

--- Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
+++ Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
@@ -3,7 +3,11 @@
 
 public class Spiral_Traversal_of_Matrix {
     
-    public static void main(String[] args) {
+    public static void spiral(int[][]matrix){
         
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        spiral(matrix);
     }
 }
