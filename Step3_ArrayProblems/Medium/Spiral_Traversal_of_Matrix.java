ba8f3032ab3246114b0ef97edc55e92120edec88
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-06 15:25:04 +0530

    printSpiral

--- Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
+++ Step3_ArrayProblems/Medium/Spiral_Traversal_of_Matrix.java
@@ -1,13 +1,25 @@
 
 
-
+import java.util.ArrayList;
+import java.util.List;
 public class Spiral_Traversal_of_Matrix {
-    
-    public static void spiral(int[][]matrix){
-        
+
+    public static List<Integer> printSpiral(int[][] mat) {
+
     }
+
     public static void main(String[] args) {
-        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        spiral(matrix);
+        int[][] mat = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+
+        List<Integer> ans = printSpiral(mat);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+
+        System.out.println();
     }
 }
