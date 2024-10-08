d01536111ae02c9e84a3157fd4e4bb3660f4d546
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-08 18:36:22 +0530

    generate_Pascals_Triangle

--- Step3_ArrayProblems/Hard/generate_Pascals_Triangle.java
+++ Step3_ArrayProblems/Hard/generate_Pascals_Triangle.java
@@ -1,16 +1,35 @@
 package Hard;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class generate_Pascals_Triangle {
 
-    public List<List<Integer>> pascalsTriangle(int numRows) {
-        
+    public static List<Integer> generateRow(int row){
+
+        long ans = 1; 
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+
+        for(int col =1; col<row; col++){
+            ans = ans*(row - col);
+            ans = ans/col;
+            ansRow.add((int) ans);
+        }
+        return ansRow;
+    }
+    public static List<List<Integer>> pascalsTriangle(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int row =1; row<=numRows; row++){
+            ans.add(generateRow(row));
+        }
+
+        return ans;
     }
     
     public static void main(String[] args) {
         int n = 5;
-        List<List<Integer>> ans = pascalTriangle(n);
+        List<List<Integer>> ans = pascalsTriangle(n);
         for (List<Integer> it : ans) {
             for (int ele : it) {
                 System.out.print(ele + " ");
