bd2d8e0126098ec2fd287f66d3300faf2af17cde
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-09 22:16:54 +0530

    Majority_Element_2

---
+++ Step3_ArrayProblems/Hard/Majority_Element_2.java
@@ -0,0 +1,15 @@
+package Hard;
+
+public class Majority_Element_2 {
+    
+    public static void Majority(int[]arr){
+        
+        int n=arr.length;
+
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[] = {11,33,33,11,33,11};
+        Majority(arr);
+    }
+}
