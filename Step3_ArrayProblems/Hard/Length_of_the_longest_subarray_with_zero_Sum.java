fbc64c8e02d195d7ab5ff141fb4dfcd7814cee8e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-10 22:22:45 +0530

    Bruteforce approach

--- Step3_ArrayProblems/Hard/Length_of_the_longest_subarray_with_zero_Sum.java
+++ Step3_ArrayProblems/Hard/Length_of_the_longest_subarray_with_zero_Sum.java
@@ -1,6 +1,28 @@
+package Hard;
 
+// import java.util.HashMap;
 class Length_of_the_longest_subarray_with_zero_Sum{
 
+// Bruteforce approach
+    public static int findLengthOfLongestSubarrayWithZeroSum(int[]arr){
+
+        int n = arr.length;
+        int max = 0;
+
+        for(int i=0; i<n; i++){
+            
+            int sum = 0;
+            for(int j =i; j<n ; j++){
+                sum += arr[j];
+
+                if(sum == 0){
+                    max = Math.max(max, j-i+1);
+                }
+            }
+        }
+
+        return max;
+    }
     public static void main(String[] args) {
         int[] arr = {6, -2, 2, -8, 1, 7, 4, -10};
         System.out.println("Length of the longest subarray with zero sum is: " + findLengthOfLongestSubarrayWithZeroSum(arr));
