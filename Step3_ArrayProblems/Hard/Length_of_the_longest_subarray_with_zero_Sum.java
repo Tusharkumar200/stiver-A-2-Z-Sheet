356a0e29f4268fbe55022287a83573b69c89c908
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-10 22:25:26 +0530

    Optimal approach

--- Step3_ArrayProblems/Hard/Length_of_the_longest_subarray_with_zero_Sum.java
+++ Step3_ArrayProblems/Hard/Length_of_the_longest_subarray_with_zero_Sum.java
@@ -23,6 +23,15 @@ class Length_of_the_longest_subarray_with_zero_Sum{
 
         return max;
     }
+    
+   
+// Optimal approach   
+    public static int optimalSolution(int[] arr){
+        
+        int n = arr.length;
+        int max = 0;
+        return max;
+    }
     public static void main(String[] args) {
         int[] arr = {6, -2, 2, -8, 1, 7, 4, -10};
         System.out.println("Length of the longest subarray with zero sum is: " + findLengthOfLongestSubarrayWithZeroSum(arr));
