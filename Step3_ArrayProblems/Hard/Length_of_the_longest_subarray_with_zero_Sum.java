8a6274748216f42e6a767194913b510f9c6b7f1c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-10 22:04:07 +0530

    Length_of_the_longest_subarray_with_zero_Sum

---
+++ Step3_ArrayProblems/Hard/Length_of_the_longest_subarray_with_zero_Sum.java
@@ -0,0 +1,8 @@
+
+class Length_of_the_longest_subarray_with_zero_Sum{
+
+    public static void main(String[] args) {
+        int[] arr = {6, -2, 2, -8, 1, 7, 4, -10};
+        System.out.println("Length of the longest subarray with zero sum is: " + findLengthOfLongestSubarrayWithZeroSum(arr));
+    }
+}
