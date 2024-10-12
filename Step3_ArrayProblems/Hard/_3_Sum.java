ebfe7013c6cedcc92b13fa158fa34cf8e5b7ca29
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-12 21:12:32 +0530

    triplet_optimal_Approach

--- Step3_ArrayProblems/Hard/_3_Sum.java
+++ Step3_ArrayProblems/Hard/_3_Sum.java
@@ -27,7 +27,7 @@ public class _3_Sum{
         return ans;
     }
 
-    //* Better approach 
+    //* Better approach using
     public static List<List<Integer>> triplet_Better_Approach(int n, int[] arr){
 
         Set<List<Integer>> st = new HashSet<>();
@@ -52,12 +52,38 @@ public class _3_Sum{
     
     // * Optimal approach
     public static List<List<Integer>> triplet_optimal_Approach(int n, int[]arr){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
 
+        for(int i=0; i<n; i++){
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+            int j =i+1;
+            int k =n-1;
+            while (j<k) {
+                int sum = arr[i] + arr[j] +arr[k];
+                if(sum < 0){j++;}
+                else if(sum > 0){ k--;}
+                else{
+                    List<Integer> temp = Arrays.asList(arr[i] , arr[j] , arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+
+                    while (j < k && arr[j] == arr[j -1]) j++;
+                    while (j < k && arr[k] == arr[k -1]) k--;
+                }
+            }
+
+
+        }
+        return ans;
     }
     public static void main(String[] args) { int[] arr = { -1, 0, 1, 2, -1, -4};
     int n = arr.length;
     // List<List<Integer>> ans = triplet(n, arr);
-    List<List<Integer>> ans = triplet_Better_Approach(n, arr);
+    // List<List<Integer>> ans = triplet_Better_Approach(n, arr);
+    List<List<Integer>> ans = triplet_optimal_Approach(n, arr);
     
     for (List<Integer> it : ans) {
         System.out.print("[");
