49bd91287d7861cbf764f41623a22c28da29df2c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-12 20:57:13 +0530

    triplet_Better_Approach

--- Step3_ArrayProblems/Hard/_3_Sum.java
+++ Step3_ArrayProblems/Hard/_3_Sum.java
@@ -27,9 +27,33 @@ public class _3_Sum{
         return ans;
     }
 
+    // Better approach 
+    public static List<List<Integer>> triplet_Better_Approach(int n, int[] arr){
+
+        Set<List<Integer>> st = new HashSet<>();
+
+        for(int i=0; i< n ; i++){
+            Set<Integer> hashset = new HashSet<>();
+            for(int j = i+1; j< n; j++){
+
+                int third = -(arr[i] + arr[j]);
+
+                if(hashset.contains(third)){
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
     public static void main(String[] args) { int[] arr = { -1, 0, 1, 2, -1, -4};
     int n = arr.length;
-    List<List<Integer>> ans = triplet(n, arr);
+    // List<List<Integer>> ans = triplet(n, arr);
+    List<List<Integer>> ans = triplet_Better_Approach(n, arr);
+    
     for (List<Integer> it : ans) {
         System.out.print("[");
         for (Integer i : it) {
