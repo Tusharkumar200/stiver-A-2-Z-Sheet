bd5550cebe47f2e8d48190b7becd9092914d22e4
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-12 20:45:45 +0530

    Brute force approach using 3 loops

--- Step3_ArrayProblems/Hard/_3_Sum.java
+++ Step3_ArrayProblems/Hard/_3_Sum.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class _3_Sum{
 
+    // Brute force approach using 3 loops 
     public static List<List<Integer>> triplet(int n, int[] arr){
-        
+        Set<List<Integer>> st = new HashSet<>();
+
+        for(int i=0; i< n ; i++){
+            for(int j= i+1; j<n ; j++ ){
+
+                for(int k=j+1; k<n; k++){
+
+                    if(arr[i] + arr[j] + arr[k] == 0){
+                        List<Integer>temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                        temp.sort(null);
+                        st.add(temp);
+                    }
+                }
+            }
+        }
+        List<List<Integer>> ans =  new ArrayList<>(st);
+        return ans;
     }
 
     public static void main(String[] args) { int[] arr = { -1, 0, 1, 2, -1, -4};
