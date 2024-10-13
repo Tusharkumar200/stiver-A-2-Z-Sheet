ecfe3eabada7f862a83539c03f03e05cee758418
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-13 15:27:16 +0530

    _4_Sum

---
+++ Step3_ArrayProblems/Hard/_4_Sum.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class _4_Sum {
+    
+    public static List<List<Integer>> fourSum(int[] nums, int target){
+        int n = nums.length;
+
+
+    }
+
+    public static void main(String[] args) {
+       int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+        int target = 9;
+        List<List<Integer>> ans = fourSum(nums,target);
+        System.out.println(ans);
+    }
+}
