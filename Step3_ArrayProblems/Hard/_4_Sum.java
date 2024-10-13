a79c637d276d328b364a43de2a01e972e1c36329
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-13 19:09:12 +0530

    bruteforce approched

--- Step3_ArrayProblems/Hard/_4_Sum.java
+++ Step3_ArrayProblems/Hard/_4_Sum.java
@@ -1,10 +1,43 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class _4_Sum {
     
-    public static List<List<Integer>> fourSum(int[] nums, int target){
-        int n = nums.length;
-        
+    // *BruteForce Approched
+    public static List<List<Integer>> fourSum(int[] num, int target){
+        int n = num.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+
+        for(int i=0; i < n; i++){
+            for(int j = i+1 ; j< n; j++){
+
+                for(int k=j+1; k<n; k++){
+
+                    for(int l = k+1; l<n; l++){
+
+                        long sum = (long)num[i] + num[j];
+                        sum += num[k];
+                        sum+= num[l];
+
+                        if(sum == target){
+
+                            List<Integer> temp =  Arrays.asList(num[i], num[j], num[k], num[l]);
+                            Collections.sort(temp);
+                            set.add(temp);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
 
     }
 
