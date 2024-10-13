c605f4808ce30261819f6cb94c9c645b451603da
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-13 19:45:39 +0530

    Optimal approched

--- Step3_ArrayProblems/Hard/_4_Sum.java
+++ Step3_ArrayProblems/Hard/_4_Sum.java
@@ -6,27 +6,26 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class _4_Sum {
-    
+
     // *BruteForce Approched
-    public static List<List<Integer>> fourSum(int[] num, int target){
+    public static List<List<Integer>> fourSum(int[] num, int target) {
         int n = num.length;
         Set<List<Integer>> set = new HashSet<>();
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
 
-        for(int i=0; i < n; i++){
-            for(int j = i+1 ; j< n; j++){
+                for (int k = j + 1; k < n; k++) {
 
-                for(int k=j+1; k<n; k++){
+                    for (int l = k + 1; l < n; l++) {
 
-                    for(int l = k+1; l<n; l++){
-
-                        long sum = (long)num[i] + num[j];
+                        long sum = (long) num[i] + num[j];
                         sum += num[k];
-                        sum+= num[l];
+                        sum += num[l];
 
-                        if(sum == target){
+                        if (sum == target) {
 
-                            List<Integer> temp =  Arrays.asList(num[i], num[j], num[k], num[l]);
+                            List<Integer> temp = Arrays.asList(num[i], num[j], num[k], num[l]);
                             Collections.sort(temp);
                             set.add(temp);
 
@@ -41,9 +40,8 @@ public class _4_Sum {
 
     }
 
-
-    //  *Better Approched using fornula(a+b +c=0 --> -c = a+b);
-    public static List<List<Integer>> fourSum_BetterApproched(int[] nums, int target){
+    // *Better Approched using fornula(a+b +c=0 --> -c = a+b);
+    public static List<List<Integer>> fourSum_BetterApproched(int[] nums, int target) {
 
         int n = nums.length; // size of the array
         Set<List<Integer>> st = new HashSet<>();
@@ -57,8 +55,8 @@ public class _4_Sum {
                     long sum = nums[i] + nums[j];
                     sum += nums[k];
                     long fourth = target - sum;
-                    if(hashset.contains(fourth)){
-                        List<Integer> temp  = new ArrayList<>();
+                    if (hashset.contains(fourth)) {
+                        List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
@@ -73,17 +71,58 @@ public class _4_Sum {
         List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
-    
-    
-    // * Optimal approched
-    public static List<List<Integer>> fourSum_OptimalApproched(int[] nums, int target){
 
+    // * Optimal approched
+    public static List<List<Integer>> fourSum_OptimalApproched(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 4) return ans;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) k++; 
+                        while (k < l && nums[l] == nums[l + 1]) l--; 
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
+    
+
     public static void main(String[] args) {
-       int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+        int[] nums = { 4, 3, 3, 4, 4, 2, 1, 2, 1, 1 };
         int target = 9;
         // List<List<Integer>> ans = fourSum(nums,target);
-        List<List<Integer>> ans = fourSumBetterApproched(nums,target);
+        // List<List<Integer>> ans = fourSumBetterApproched(nums, target);
+        List<List<Integer>> ans = fourSum_OptimalApproched(nums, target);
         System.out.println(ans);
     }
 }
