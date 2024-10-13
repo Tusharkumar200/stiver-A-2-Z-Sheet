eda5b0092ad0aa65cebebd3143a4f148cc9b02bc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-13 19:24:01 +0530

    fourSumBetterApproched

--- Step3_ArrayProblems/Hard/_4_Sum.java
+++ Step3_ArrayProblems/Hard/_4_Sum.java
@@ -41,10 +41,43 @@ public class _4_Sum {
 
     }
 
+
+    //  *Better Approched using fornula(a+b +c=0 --> -c = a+b);
+    public static List<List<Integer>> fourSumBetterApproched(int[] nums, int target){
+
+        int n = nums.length; // size of the array
+        Set<List<Integer>> st = new HashSet<>();
+
+        // checking all possible quadruplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Long> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long fourth = target - sum;
+                    if(hashset.contains(fourth)){
+                        List<Integer> temp  = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add((int) fourth);
+                        temp.sort(Integer::compareTo);
+                        st.add(temp);
+                    }
+                    hashset.add((long) nums[k]);
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
     public static void main(String[] args) {
        int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
         int target = 9;
-        List<List<Integer>> ans = fourSum(nums,target);
+        // List<List<Integer>> ans = fourSum(nums,target);
+        List<List<Integer>> ans = fourSumBetterApproched(nums,target);
         System.out.println(ans);
     }
 }
