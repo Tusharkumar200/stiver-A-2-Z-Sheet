7908bfd3dcc430e5cda0786bd28b3d3dbbdd2336
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-14 18:21:14 +0530

    Better Approach

--- Step3_ArrayProblems/Hard/Count_the_number_of_subarrays_with_given_xor_K.java
+++ Step3_ArrayProblems/Hard/Count_the_number_of_subarrays_with_given_xor_K.java
@@ -2,13 +2,30 @@
 
 public class Count_the_number_of_subarrays_with_given_xor_K{
 
-    public static void countSubarrays(int []arr,int  k){
-        
+
+    // Better Approach
+    public static int subarraysWithXorK(int []arr,int  k){
+        int n = arr.length;
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            int xorr = 0;
+
+            for(int j=i; j<n;j++){
+
+                xorr= xorr ^ arr[j];
+                if(xorr ==k){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 2, 2, 6, 4};
+        int[] a = {4, 2, 2, 6, 4};
         int k = 6;
-        System.out.println(countSubarrays(arr, k));
+        int ans = subarraysWithXorK(a, k);
+        System.out.println("The number of subarrays with XOR k is: " + ans);
     }
 }
