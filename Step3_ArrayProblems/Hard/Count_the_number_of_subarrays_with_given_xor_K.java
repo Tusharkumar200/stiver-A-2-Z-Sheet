265d26520f9361ab16ac564c831c297fa6b3cdc2
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-14 17:50:05 +0530

    Count_the_number_of_subarrays_with_given_xor_K

---
+++ Step3_ArrayProblems/Hard/Count_the_number_of_subarrays_with_given_xor_K.java
@@ -0,0 +1,14 @@
+
+
+public class Count_the_number_of_subarrays_with_given_xor_K{
+
+    public static void countSubarrays(int []arr,int  k){
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 2, 6, 4};
+        int k = 6;
+        System.out.println(countSubarrays(arr, k));
+    }
+}
