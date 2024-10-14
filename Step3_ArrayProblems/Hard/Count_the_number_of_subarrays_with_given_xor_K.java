8912b96416dc5e85eb1e57355be36ffd30b7aa50
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-14 18:30:46 +0530

    Optimized Approach

--- Step3_ArrayProblems/Hard/Count_the_number_of_subarrays_with_given_xor_K.java
+++ Step3_ArrayProblems/Hard/Count_the_number_of_subarrays_with_given_xor_K.java
@@ -1,4 +1,5 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Count_the_number_of_subarrays_with_given_xor_K{
 
@@ -22,10 +23,39 @@ public class Count_the_number_of_subarrays_with_given_xor_K{
         return count;
     }
 
+    // Optimized Approach
+
+    public static int subarraysWithXorK_optimized(int []arr,int  k){
+            int n = arr.length;
+            int xr = 0;
+
+            Map<Integer,Integer> map = new HashMap<>();
+            map.put(xr,1);
+            int count = 0;
+
+            for(int i=0; i<n; i++){
+                xr = xr ^ arr[i];
+                int x = xr ^ k;
+
+                if(map.containsKey(x)){
+                    count += map.get(x);
+                }
+
+                if(map.containsKey(xr)){
+                    map.put(xr, map.get(xr)+1); 
+                }
+                else{
+                    map.put(xr,1);
+                }
+            }
+
+            return count;
+    }
     public static void main(String[] args) {
         int[] a = {4, 2, 2, 6, 4};
         int k = 6;
-        int ans = subarraysWithXorK(a, k);
+        // int ans = subarraysWithXorK(a, k);
+        int ans = subarraysWithXorK_optimized(a, k);
         System.out.println("The number of subarrays with XOR k is: " + ans);
     }
 }
