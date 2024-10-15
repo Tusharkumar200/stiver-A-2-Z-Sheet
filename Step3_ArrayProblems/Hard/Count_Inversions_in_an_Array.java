e363cf67795f4b2da5de2283498bc3c98d78321c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-15 12:50:52 +0530

    numberOfInversions_optimal using merge sort

--- Step3_ArrayProblems/Hard/Count_Inversions_in_an_Array.java
+++ Step3_ArrayProblems/Hard/Count_Inversions_in_an_Array.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class Count_Inversions_in_an_Array {
     
-    // Brute Force (O(N^2))
+    //* */ Brute Force (O(N^2))
 
     public static int numberOfInversions(int[] arr, int n){
         int count =0;
@@ -14,11 +16,71 @@ public class Count_Inversions_in_an_Array {
         return count;
     }
 
+
+    //  * optimal solution
+    
+        private static int merge(int[] arr, int low, int mid, int high) {
+            ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+            int left = low;      // starting index of left half of arr
+            int right = mid + 1;   // starting index of right half of arr
+    
+            //Modification 1: cnt variable to count the pairs:
+            int cnt = 0;
+    
+            //storing elements in the temporary array in a sorted manner//
+    
+            while (left <= mid && right <= high) {
+                if (arr[left] <= arr[right]) {
+                    temp.add(arr[left]);
+                    left++;
+                } else {
+                    temp.add(arr[right]);
+                    cnt += (mid - left + 1); //Modification 2
+                    right++;
+                }
+            }
+    
+            // if elements on the left half are still left //
+    
+            while (left <= mid) {
+                temp.add(arr[left]);
+                left++;
+            }
+    
+            //  if elements on the right half are still left //
+            while (right <= high) {
+                temp.add(arr[right]);
+                right++;
+            }
+    
+            // transfering all elements from temporary to arr //
+            for (int i = low; i <= high; i++) {
+                arr[i] = temp.get(i - low);
+            }
+            return cnt; // Modification 3
+        }
+    
+        public static int mergeSort(int[] arr, int low, int high) {
+            int cnt = 0;
+            if (low >= high) return cnt;
+            int mid = (low + high) / 2 ;
+            cnt += mergeSort(arr, low, mid);  // left half
+            cnt += mergeSort(arr, mid + 1, high); // right half
+            cnt += merge(arr, low, mid, high);  // merging sorted halves
+            return cnt;
+        }
+    
+        public static int numberOfInversions_optimal(int []a, int n) {
+            // Write your code here.
+            return mergeSort(a, 0, n - 1);
+        }
+    
     public static void main(String[] args) {
         
         int[] a = {5, 4, 3, 2, 1};
         int n = 5;
-        int cnt = numberOfInversions(a, n);
+        // int cnt = numberOfInversions(a, n);
+        int cnt = numberOfInversions_optimal(a, n);
         System.out.println("The number of inversions is: " + cnt);
     }
 }
