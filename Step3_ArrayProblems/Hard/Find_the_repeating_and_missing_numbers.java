affdb02a83a9becc69b2b066bbab3ef51c191b7d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-20 21:36:58 +0530

    findMissingRepeatingNumbers

---
+++ Step3_ArrayProblems/Hard/Find_the_repeating_and_missing_numbers.java
@@ -0,0 +1,15 @@
+
+class Find_the_repeating_and_missing_numbers{
+
+
+     public static int[] findMissingRepeatingNumbers(int arr[]){
+
+     }
+    public static void main(String[] args) {
+        
+        int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
+        int[] ans = findMissingRepeatingNumbers(a);
+        System.out.println("The repeating and missing numbers are: {"
+                           + ans[0] + ", " + ans[1] + "}");
+    }
+}
