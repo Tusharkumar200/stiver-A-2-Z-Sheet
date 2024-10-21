81c6b9c0dac0d54187ae769535ade49a70f929cc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-22 00:00:26 +0530

    binarySearch

--- Step4_Binary_Search/binarySearch.java
+++ Step4_Binary_Search/binarySearch.java
@@ -3,7 +3,18 @@ package Step4_Binary_Search;
 public class binarySearch {
 
     public static int showBinarySearch(int[]arr , int target){
-            int n = arr
+            int n = arr.length;
+            int low = 0;
+            int high = n -1;
+
+            while (low <= high) {
+                
+                int mid = (low + high) /2;
+                if(arr[mid] == target) return mid;
+                else if(target > arr[mid]) low = mid + 1;
+                else high = mid -1;
+            }
+            return -1;
     }
 
     public static void main(String[] args) {
