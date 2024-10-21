49c57bb77af8b3df00d2247dcd80171a4563567c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-21 23:41:02 +0530

    public class binarySearch

---
+++ Step4_Binary_Search/binarySearch.java
@@ -0,0 +1,20 @@
+package Step4_Binary_Search;
+
+public class binarySearch {
+
+    public static int showBinarySearch(int[]arr , int target){
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        int ind = showBinarySearch(a, target);
+        if (ind == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println("The target is at index: " + ind);
+
+    }
+}
