0a5325e7da79fea7c8c4bf538f29635e4096085f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-25 11:44:42 +0530

    Floor_and_Ceil_in_Sorted_Array

---
+++ Step4_Binary_Search/Floor_and_Ceil_in_Sorted_Array.java
@@ -0,0 +1,15 @@
+public class Floor_and_Ceil_in_Sorted_Array {
+    
+
+    
+    public static int[] getFloorAndCeil(int[] a, int n, int x) {
+
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        int  arr[] ={3, 4, 4, 7, 8, 10};
+        int  x= 5;
+        getFloorAndCeil(arr,n,x);
+
+    }
+}
