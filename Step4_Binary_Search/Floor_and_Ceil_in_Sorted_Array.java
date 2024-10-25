3b367cbc4cdf509f53e080933cbeebc93f3956eb
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-25 11:51:48 +0530

    findFloor

--- Step4_Binary_Search/Floor_and_Ceil_in_Sorted_Array.java
+++ Step4_Binary_Search/Floor_and_Ceil_in_Sorted_Array.java
@@ -1,7 +1,23 @@
 public class Floor_and_Ceil_in_Sorted_Array {
     
+    static int findFloor(int[] arr, int n, int x) {
+        int low=0;
+        int high= n-1;
+        int ans = -1;
 
-    
+        while(low <= high){
+            int mid= (low + high)/2;
+
+            if(arr[mid] <= x){
+                   ans = arr[mid];
+                   low = mid + 1; 
+            }else{
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+
     public static int[] getFloorAndCeil(int[] a, int n, int x) {
 
     }
