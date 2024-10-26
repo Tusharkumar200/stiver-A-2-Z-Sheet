9d154a87ed7c17c9f3577d403c3bb963a436f9b8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-26 18:24:43 +0530

    Last_occurrence_in_a_sorted_array

--- Step4_Binary_Search/Last_occurrence_in_a_sorted_array.java
+++ Step4_Binary_Search/Last_occurrence_in_a_sorted_array.java
@@ -1,7 +1,16 @@
 public class Last_occurrence_in_a_sorted_array {
     
-
-    public static void main(String[] args) {
+    public static int firstOccurrence(int[] arr, int n, int k) {
         
+    }
+
+    public static int lastOccurrence(int[] arr, int n, int k) {
+
+    }
+    public static void main(String[] args) {
+        int[] arr =  {2, 4, 6, 8, 8, 8, 11, 13};
+        int n = 8, x = 8;
+        int ans = count(arr, n, x);
+        System.out.println("The number of occurrences is: " + ans);
     }
 }
