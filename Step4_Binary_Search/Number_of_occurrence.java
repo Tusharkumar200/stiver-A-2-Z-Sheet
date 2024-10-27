c7c697e566e77d9b7723f9ee32ddb9d63bfee9a6
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-27 17:58:32 +0530

    linear search

--- Step4_Binary_Search/Number_of_occurrence.java
+++ Step4_Binary_Search/Number_of_occurrence.java
@@ -1,16 +1,25 @@
 public class Number_of_occurrence {
 
-    public static countOccurrence(int arr[], int x){
+    public static int countOccurrence(int arr[], int x){
+        // int n = arr.length;
+        int count =0;
+        // *linear search
 
-        
+        // for(int i: arr){
+        //     if(i == x)count++;
+        // }
+
+        // * binary search
+
+
+        return count;
+
     }
 
     public static void main(String[] args) {
         int  X = 3 ;
-        int  array[] = {2, 2 , 3 , 3 , 3 , 3 , 4}
+        int  array[] = {2, 2 , 3 , 3 , 3 , 3 , 4};
         int result = countOccurrence(array,X);
-        for(int i: array){
-            System.out.print(i);
-        }
+        System.out.println(result);
     }
 }
