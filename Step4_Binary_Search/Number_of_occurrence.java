20b9bad3f90bb17e738a60e6a44892bda8e78b04
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-27 17:53:15 +0530

    Number_of_occurrence

---
+++ Step4_Binary_Search/Number_of_occurrence.java
@@ -0,0 +1,16 @@
+public class Number_of_occurrence {
+
+    public static countOccurrence(int arr[], int x){
+
+        
+    }
+
+    public static void main(String[] args) {
+        int  X = 3 ;
+        int  array[] = {2, 2 , 3 , 3 , 3 , 3 , 4}
+        int result = countOccurrence(array,X);
+        for(int i: array){
+            System.out.print(i);
+        }
+    }
+}
