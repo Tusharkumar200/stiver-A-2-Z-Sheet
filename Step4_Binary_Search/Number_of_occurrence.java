ef98f5b0b3c23b5fcfd1906f7563bff02dfe30dc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-27 18:21:19 +0530

    firstOccurrence

--- Step4_Binary_Search/Number_of_occurrence.java
+++ Step4_Binary_Search/Number_of_occurrence.java
@@ -1,25 +1,53 @@
 public class Number_of_occurrence {
 
-    public static int countOccurrence(int arr[], int x){
-        // int n = arr.length;
-        int count =0;
+    public static int countOccurrence(int arr[], int x) {
         // *linear search
 
-        // for(int i: arr){
-        //     if(i == x)count++;
-        // }
-
-        // * binary search
-
+        int count = 0;
+        for (int i : arr) {
+            if (i == x)
+                count++;
+        }
 
         return count;
+    }
+    // * binary search
 
+    public static int firstOccurrence(int[] arr, int n, int k) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == x) {
+                first = mid;
+                right = mid - 1;
+            } else if (arr[mid] < x) {
+
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    public static int lastOccurrence(int[] arr, int n, int k) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+        }
     }
 
     public static void main(String[] args) {
-        int  X = 3 ;
-        int  array[] = {2, 2 , 3 , 3 , 3 , 3 , 4};
-        int result = countOccurrence(array,X);
+        int X = 2;
+        int array[] = { 2, 2, 3, 3, 3, 3, 4 };
+        int result = countOccurrence(array, X);
         System.out.println(result);
     }
 }
