67e12f19e50fe5e58f1b50d59a22b5700d8858ff
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-28 10:50:14 +0530

    findKRotation

--- Step4_Binary_Search/Find_Kth_Rotation.java
+++ Step4_Binary_Search/Find_Kth_Rotation.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class Find_Kth_Rotation {
     
-
+    public int findKRotation(List<Integer> arr) {
+            
+    }
     public static void main(String []a){
         int []arr = {3,4,5,1,2};
     }
