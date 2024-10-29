f546086af632603c1756ee1cd54a72b1b80f2856
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-29 18:04:50 +0530

    singleNonDuplicate

---
+++ Step4_Binary_Search/Search_Single_Element_in_a_sorted_array.java
@@ -0,0 +1,18 @@
+public class Search_Single_Element_in_a_sorted_array {
+    
+
+    public static int singleNonDuplicate(int arr[]){
+        // int n = arr.length;
+        int ans = 0;
+
+        for(int i: arr){
+            ans  = ans ^ i;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+       int  arr[] = {1,1,2,2,3,3,4,5,5,6,6};
+       int result = singleNonDuplicate(arr);
+       System.out.println(result);
+    }
+}
