0123bd9b985866f90e78e5614704b08a512792ec
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-31 11:26:26 +0530

    Linear Approched!

--- Step4_Binary_Search/Search_Element_in_Rotated_Sorted_Array_II.java
+++ Step4_Binary_Search/Search_Element_in_Rotated_Sorted_Array_II.java
@@ -2,10 +2,13 @@ public class Search_Element_in_Rotated_Sorted_Array_II {
     
     public static boolean searchInARotatedSortedArrayII(int []arr, int k){
 
+        /* Linear Approched!
         for(int i: arr){
             if(i == k) return true;
         }
         return false;
+
+        */
     }
     public static void main(String[] args) {
         int arr[] = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6}, k = 3;
