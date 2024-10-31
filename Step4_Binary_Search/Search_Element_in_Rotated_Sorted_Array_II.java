ef28e9b0dfdf000388796de11d9c63eb295a924a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-31 11:20:58 +0530

    searchInARotatedSortedArrayII

---
+++ Step4_Binary_Search/Search_Element_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,10 @@
+public class Search_Element_in_Rotated_Sorted_Array_II {
+    
+    public static boolean searchInARotatedSortedArrayII(int []arr, int k){
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6}, k = 3;
+
+    }
+}
