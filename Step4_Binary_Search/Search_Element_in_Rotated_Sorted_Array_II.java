19f86a980db69401601135a1efa25a0158149ad3
Tushar Kumar <tusharnwd2003@gmail.com>
2024-10-31 11:24:26 +0530

    Linear approched

--- Step4_Binary_Search/Search_Element_in_Rotated_Sorted_Array_II.java
+++ Step4_Binary_Search/Search_Element_in_Rotated_Sorted_Array_II.java
@@ -1,10 +1,15 @@
 public class Search_Element_in_Rotated_Sorted_Array_II {
     
     public static boolean searchInARotatedSortedArrayII(int []arr, int k){
-        
+
+        for(int i: arr){
+            if(i == k) return true;
+        }
+        return false;
     }
     public static void main(String[] args) {
         int arr[] = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6}, k = 3;
-
+        boolean res =  searchInARotatedSortedArrayII(arr,  k);
+        System.out.println(res);
     }
 }
