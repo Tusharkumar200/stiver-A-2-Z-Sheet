76ea4570ae04339b9ba504b8eb8c2d617a880f37
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-01 21:29:57 +0530

    find_peak_element

---
+++ Step4_Binary_Search/find_peak_element.java
@@ -0,0 +1,13 @@
+public class find_peak_element {
+    
+
+    public static int peakElement(int[]arr){
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5,6,7,8,5,1};
+        int res = peakElement(arr);
+    }
+}
