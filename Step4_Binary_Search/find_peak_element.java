e30648142b3eb110cbd48ec202dd9afc4073adae
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-01 21:50:45 +0530

    Binary Search

--- Step4_Binary_Search/find_peak_element.java
+++ Step4_Binary_Search/find_peak_element.java
@@ -3,6 +3,7 @@ public class find_peak_element {
 
     public static int peakElement(int[]arr){
 
+      /* //Linear Search  
         int n = arr.length;
 
         for(int i=0; i<n;  i++){
@@ -10,6 +11,31 @@ public class find_peak_element {
             if((i==0 || arr[i-1] < arr[i]) && (i == n-1 || arr[i] > arr[i + 1])){
                 return i;
             }
+        }
+        return -1;
+
+        */
+
+        // * Binary Search
+
+        int n = arr.length;
+
+        if(n == 1) return 0;
+        if(arr[0] > arr[1])return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+
+        int low = 1, high = n-2;
+
+        while (low <= high) {
+            int mid = (low + high)/ 2;
+
+            if(arr[mid -1] < arr[mid] && arr[mid] > arr[mid + 1]){ return mid;}
+
+            if(arr[mid] > arr[mid-1]){
+                low= mid +1;
+            }
+
+            else high = mid -1;
         }
         return -1;
     }
