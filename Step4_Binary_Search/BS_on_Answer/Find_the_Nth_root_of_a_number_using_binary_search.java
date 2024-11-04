f1a1a325d08e658db141eac558086254b5716c66
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-04 11:49:40 +0530

    Find_the_Nth_root_of_a_number_using_binary_search

---
+++ Step4_Binary_Search/BS_on_Answer/Find_the_Nth_root_of_a_number_using_binary_search.java
@@ -0,0 +1,14 @@
+package BS_on_Answer;
+
+public class Find_the_Nth_root_of_a_number_using_binary_search {
+    
+    public static  int NthRoot(int n, int m) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 27;
+        int ans = NthRoot(n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
