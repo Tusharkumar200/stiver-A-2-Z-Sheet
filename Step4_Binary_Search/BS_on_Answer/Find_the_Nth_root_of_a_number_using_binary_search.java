14701b1b0de0ddf01c7fc8ad76eb7d9b1cbea7a8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-04 11:54:09 +0530

    func created for the mid

--- Step4_Binary_Search/BS_on_Answer/Find_the_Nth_root_of_a_number_using_binary_search.java
+++ Step4_Binary_Search/BS_on_Answer/Find_the_Nth_root_of_a_number_using_binary_search.java
@@ -2,7 +2,19 @@ package BS_on_Answer;
 
 public class Find_the_Nth_root_of_a_number_using_binary_search {
     
+    public static int func(int mid, int n, int m){
+        long ans =1;
+
+        for(int i=1; i<=n ; i++){
+            ans *= mid;
+            if(ans > m)return 2;
+        }
+        if(ans == m) return 1;
+        return 0;
+    }
+
     public static  int NthRoot(int n, int m) {
+        
         return -1;
     }
 
