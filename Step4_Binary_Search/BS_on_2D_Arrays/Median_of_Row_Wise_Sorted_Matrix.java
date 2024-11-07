d7c7ee80b0148d4ca9592bebff119fcd98a7f227
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-07 11:22:13 +0530

    Linear_median

--- Step4_Binary_Search/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.java
+++ Step4_Binary_Search/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class Median_of_Row_Wise_Sorted_Matrix {
     
-    public static int median(int[][] matrix, int m, int n){
-        //* Linear Search -T.C ->  O(MXN) + O(MXN(log(MXN)))
+    //* Linear Search -T.C ->  O(MXN) + O(MXN(log(MXN)))
+    public static int Linear_median(int[][] matrix, int m, int n){
         List <Integer> lst = new ArrayList<>();
 
         for(int i=0; i< m; i++){
@@ -16,6 +16,12 @@ public class Median_of_Row_Wise_Sorted_Matrix {
         Collections.sort(lst);
         return lst.get((m * n)/2 );
     }
+
+    //* Binary Search - T.c -> 
+    public static int Binary_median(int[][] matrix, int m, int n){
+
+        return -1
+    }
     public static void main(String[] args) {
         int matrix[][] = {
             {1, 2, 3, 4, 5},
@@ -23,7 +29,7 @@ public class Median_of_Row_Wise_Sorted_Matrix {
             {21, 23, 25, 27, 29}
         };
         int m = matrix.length, n = matrix[0].length;
-        int ans = median(matrix, m, n);
+        int ans = Linear_median(matrix, m, n);
         System.out.println("The median element is: " + ans);
     }
 }
