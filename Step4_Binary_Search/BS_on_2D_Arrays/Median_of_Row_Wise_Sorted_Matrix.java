03f99a91633b913d1eed306ed0705553862ff921
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-07 10:55:44 +0530

    median function created

--- Step4_Binary_Search/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.java
+++ Step4_Binary_Search/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.java
@@ -2,7 +2,10 @@ package BS_on_2D_Arrays;
 
 public class Median_of_Row_Wise_Sorted_Matrix {
     
+    public static int median(int[]matrix, int m, int n){
 
+        return -1;
+    }
     public static void main(String[] args) {
         int matrix[][] = {
             {1, 2, 3, 4, 5},
