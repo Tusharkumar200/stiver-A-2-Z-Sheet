4712283ed51f9eb01dc269c9de13b5a349cfc358
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-07 10:54:27 +0530

    Median_of_Row_Wise_Sorted_Matrix

---
+++ Step4_Binary_Search/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.java
@@ -0,0 +1,16 @@
+package BS_on_2D_Arrays;
+
+public class Median_of_Row_Wise_Sorted_Matrix {
+    
+
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1, 2, 3, 4, 5},
+            {8, 9, 11, 12, 13},
+            {21, 23, 25, 27, 29}
+        };
+        int m = matrix.length, n = matrix[0].length;
+        int ans = median(matrix, m, n);
+        System.out.println("The median element is: " + ans);
+    }
+}
