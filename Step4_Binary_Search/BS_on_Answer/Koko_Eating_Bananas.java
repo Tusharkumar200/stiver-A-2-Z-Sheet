ae23253b35da758a52c2e3cb687afa93407cefaa
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-08 15:39:10 +0530

    Koko_Eating_Bananas

---
+++ Step4_Binary_Search/BS_on_Answer/Koko_Eating_Bananas.java
@@ -0,0 +1,14 @@
+public class Koko_Eating_Bananas {
+    
+    public static int minimumRateToEatBananas(int[] v, int h) {
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+        int[] v = {7, 15, 6, 3};
+        int h = 8;
+        int ans = minimumRateToEatBananas(v, h);
+        System.out.println("Koko should eat at least " + ans + " bananas/hr.");
+    }
+}
