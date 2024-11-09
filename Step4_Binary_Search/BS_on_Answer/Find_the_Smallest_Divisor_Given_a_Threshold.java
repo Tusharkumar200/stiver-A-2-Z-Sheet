a3800f030e1b6584a523e5508e2941ae20ea8dd8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-09 11:46:44 +0530

    smallestDivisor

--- Step4_Binary_Search/BS_on_Answer/Find_the_Smallest_Divisor_Given_a_Threshold.java
+++ Step4_Binary_Search/BS_on_Answer/Find_the_Smallest_Divisor_Given_a_Threshold.java
@@ -1,11 +1,38 @@
 public class Find_the_Smallest_Divisor_Given_a_Threshold {
     
     public static int sumByD(int[] arr, int div) {
+        int n = arr.length;
+        int sum = 0;
 
-        return 0;
+        for(int i=0; i< n; i++){
+
+            sum += Math.ceil((double) arr[i]/(double) div );
+        }
+        return sum;
     }
     public static int smallestDivisor(int[] arr, int limit) {
-        return 0;
+        int n = arr.length;
+
+        if(n > limit) return -1;
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i=0; i< n; i++){
+            maxi = Math.max(arr[i], maxi);
+        }
+        int low =0, high = n-1;
+
+        while (low <= high) {
+            int mid = (low + high)/2;
+
+            if(sumByD(arr, mid) <= limit){
+
+                high = mid - 1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return low;
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
