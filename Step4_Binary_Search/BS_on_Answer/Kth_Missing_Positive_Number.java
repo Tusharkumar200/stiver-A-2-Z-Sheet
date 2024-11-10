c8c6e7cdf60d7c406d9b90915b48e073e6ddabe8
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-10 10:05:39 +0530

    Binary Search!

--- Step4_Binary_Search/BS_on_Answer/Kth_Missing_Positive_Number.java
+++ Step4_Binary_Search/BS_on_Answer/Kth_Missing_Positive_Number.java
@@ -3,11 +3,28 @@ public class Kth_Missing_Positive_Number {
 
     public static int missingK(int arr[], int n, int k){
         //* Linear Search
-        for(int i: arr){
-            if(i <= k) k++;
-            else break;
+        // for(int i: arr){
+        //     if(i <= k) k++;
+        //     else break;
+        // }
+        // return k;
+
+        //* Binary Search!
+
+        int low=0 , heigh = n-1;
+        while(low <= heigh){
+            int mid = (low + heigh)/2;
+            int missing = arr[mid] - (mid +1);
+
+            if(missing < k){
+                low = mid +1;
+            }
+            else{
+                heigh = mid -1;
+            }
         }
-        return k;
+
+        return k + heigh + 1;
     }
     public static void main(String[] args) {
         int[] vec = {4, 7, 9, 10};
