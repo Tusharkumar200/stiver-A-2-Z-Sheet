23352e90d08a65bc5ede42c5f9f647de528bc933
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-10 09:45:52 +0530

    Kth_Missing_Positive_Number

---
+++ Step4_Binary_Search/BS_on_Answer/Kth_Missing_Positive_Number.java
@@ -0,0 +1,14 @@
+public class Kth_Missing_Positive_Number {
+    
+
+    public static int missingK(int arr[], int n, int k){
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] vec = {4, 7, 9, 10};
+        int n = 4, k = 4;
+        int ans = missingK(vec, n, k);
+        System.out.println("The missing number is: " + ans);
+    }
+}
