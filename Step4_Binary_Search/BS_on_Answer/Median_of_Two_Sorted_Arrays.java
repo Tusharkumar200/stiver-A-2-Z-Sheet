6fcdaefe5b592340982499768fc02485cc3fbccd
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-12 18:26:09 +0530

    condition added for the both arrays

--- Step4_Binary_Search/BS_on_Answer/Median_of_Two_Sorted_Arrays.java
+++ Step4_Binary_Search/BS_on_Answer/Median_of_Two_Sorted_Arrays.java
@@ -2,6 +2,28 @@
 public class Median_of_Two_Sorted_Arrays{
 
     public static double median(int[] a, int[] b) {
+        int n1 = a.length;
+        int n2 = b.length;
+
+        if (n1 > n2) return median(b, a);
+
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2
+        int low  =0;
+        int high = n1;
+
+        while(low <= high){
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+
+            int r1 = (mid1 < n1)? a[mid1]: Integer.MAX_VALUE;
+            int r2 = (mid2 < n2)? b[mid2]: Integer.MAX_VALUE;
+
+            
+        }
 
         return 0;
     }
