cfcdae3572ae122296dbc88ed42bc4ea3605040e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-12 18:10:17 +0530

    Median_of_Two_Sorted_Arrays

---
+++ Step4_Binary_Search/BS_on_Answer/Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,14 @@
+
+public class Median_of_Two_Sorted_Arrays{
+
+    public static double median(int[] a, int[] b) {
+
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] a = {1, 4, 7, 10, 12};
+        int[] b = {2, 3, 6, 15};
+        System.out.println("The median of two sorted arrays is " + median(a, b));
+    }
+}
