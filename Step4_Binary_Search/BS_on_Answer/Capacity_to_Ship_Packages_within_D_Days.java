cf4a367b681bbed674a18ffa8b6256f33582b560
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-14 00:46:26 +0530

    leastWeightCapacity

--- Step4_Binary_Search/BS_on_Answer/Capacity_to_Ship_Packages_within_D_Days.java
+++ Step4_Binary_Search/BS_on_Answer/Capacity_to_Ship_Packages_within_D_Days.java
@@ -1,8 +1,46 @@
+
+
 public class Capacity_to_Ship_Packages_within_D_Days {
+
+    public static int findDays(int[] weights  , int capacity){
+        int days = 1;
+        int load =0;
+        int n = weights.length;
+
+        for(int i=0; i<n; i++){
+            if(load + weights[i] > capacity){
+                days +=1;
+                load = weights[i];
+
+            }
+            else{
+                load = weights[i];
+            }
+        }
+        return days;
+    }
     
     public static int leastWeightCapacity(int[] weights, int d) {
 
-        return 0;
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+
+        for(int i =0; i< weights.length;i++){
+            high += weights[i];
+            low = Math.max(low , weights[i]);
+        }
+        while (low <= high) {
+            int mid = (low + high) /2;
+            int numOfDay = findDays(weights, mid);
+
+            if(numOfDay <= d){
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 
     public static void main(String[] args) {
