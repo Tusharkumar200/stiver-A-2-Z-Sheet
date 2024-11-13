fa0c629614b0099626c988191c3ad15fac889f1d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-14 01:14:16 +0530

    error solve

--- Step4_Binary_Search/BS_on_Answer/Capacity_to_Ship_Packages_within_D_Days.java
+++ Step4_Binary_Search/BS_on_Answer/Capacity_to_Ship_Packages_within_D_Days.java
@@ -2,41 +2,39 @@ package BS_on_Answer;
 
 public class Capacity_to_Ship_Packages_within_D_Days {
 
-    public static int findDays(int[] weights  , int capacity){
-        int days = 1;
-        int load =0;
-        int n = weights.length;
-
-        for(int i=0; i<n; i++){
-            if(load + weights[i] > capacity){
-                days +=1;
-                load = weights[i];
-
-            }
-            else{
-                load = weights[i];
+    public static int findDays(int[] weights  , int cap){
+        int days = 1; //First day.
+        int load = 0;
+        int n = weights.length; //size of array.
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1; //move to next day
+                load = weights[i]; //load the weight.
+            } else {
+                
+                load += weights[i];
             }
         }
         return days;
     }
     
+    
     public static int leastWeightCapacity(int[] weights, int d) {
 
-        int low = Integer.MIN_VALUE;
-        int high = 0;
-
-        for(int i =0; i< weights.length;i++){
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int i = 0; i < weights.length; i++) {
             high += weights[i];
-            low = Math.max(low , weights[i]);
+            low = Math.max(low, weights[i]);
         }
-        while (low <= high) {
-            int mid = (low + high) /2;
-            int numOfDay = findDays(weights, mid);
 
-            if(numOfDay <= d){
-                high = mid-1;
-            }
-            else{
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+            if (numberOfDays <= d) {
+                //eliminate right half
+                high = mid - 1;
+            } else {
+                //eliminate left half
                 low = mid + 1;
             }
         }
