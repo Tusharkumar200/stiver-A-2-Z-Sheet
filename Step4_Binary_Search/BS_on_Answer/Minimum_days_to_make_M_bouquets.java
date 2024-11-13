700bf58f475e8e8a870bec55762d82fd9705781a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-13 10:34:03 +0530

    Minimum_days_to_make_M_bouquets

---
+++ Step4_Binary_Search/BS_on_Answer/Minimum_days_to_make_M_bouquets.java
@@ -0,0 +1,18 @@
+public class Minimum_days_to_make_M_bouquets {
+    
+    public static int roseGarden(int[] arr, int k, int m) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 7, 7, 7, 13, 11, 12, 7};
+        int k = 3;
+        int m = 2;
+        int ans = roseGarden(arr, k, m);
+        if (ans == -1)
+            System.out.println("We cannot make m bouquets.");
+        else
+            System.out.println("We can make bouquets on day " + ans);
+    
+    }
+}
