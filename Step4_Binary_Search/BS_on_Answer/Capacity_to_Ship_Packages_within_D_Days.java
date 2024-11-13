6fa84941a389e2674567242e56c49af5bc667333
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-14 00:21:50 +0530

    Capacity_to_Ship_Packages_within_D_Days

---
+++ Step4_Binary_Search/BS_on_Answer/Capacity_to_Ship_Packages_within_D_Days.java
@@ -0,0 +1,14 @@
+public class Capacity_to_Ship_Packages_within_D_Days {
+    
+    public static int leastWeightCapacity(int[] weights, int d) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {5, 4, 5, 2, 3, 4, 5, 6};
+        int d = 5;
+        int ans = leastWeightCapacity(weights, d);
+        System.out.println("The minimum capacity should be: " + ans);
+    }
+}
