9c6ad7fdb0749c7de58b9d23b81d9547f7e15cee
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-13 11:52:58 +0530

    error fixed

--- Step4_Binary_Search/BS_on_Answer/Minimum_days_to_make_M_bouquets.java
+++ Step4_Binary_Search/BS_on_Answer/Minimum_days_to_make_M_bouquets.java
@@ -1,23 +1,47 @@
+package BS_on_Answer;
+
 public class Minimum_days_to_make_M_bouquets {
     
     public static boolean possible(int[] arr, int day, int m, int k) {
         int n = arr.length;
-        int cnt =0;
-        int nOfB =0;
-        for(int i=0; i<n; i++){
-            if(arr[i] <= day) cnt++;
-            else {
-                nOfB = cnt/k;
+        int cnt = 0;
+        int nOfB = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+                if (cnt == k) {
+                    nOfB++;
+                    cnt = 0;
+                }
+            } else {
                 cnt = 0;
             }
-
         }
-        nOfB  =cnt/k;
-        return noOfB >= m;  
+        return nOfB >= m;
     }
+
     public static int roseGarden(int[] arr, int k, int m) {
+        long val = (long) m * k;
+        int n = arr.length; 
+        if (val > n) return -1; 
         
-        return 0;
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        // Apply binary search:
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 
     public static void main(String[] args) {
