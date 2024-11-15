57777e87d3536851911c8689085028d8f618c3b9
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-15 18:57:45 +0530

    canWePlace fun complete

--- Step4_Binary_Search/BS_on_Answer/Aggressive_Cows.java
+++ Step4_Binary_Search/BS_on_Answer/Aggressive_Cows.java
@@ -3,10 +3,23 @@ package BS_on_Answer;
 public class Aggressive_Cows {
     public static boolean canWePlace(int[] stalls, int dist, int cows) {
 
+        int n = stalls.length;
+        int cntCow = 1;
+        int last = stalls[0];
+
+        for(int i=1; i<n; i++){
+
+            if(stalls[i] - last >= dist){
+                cntCow++;
+                last =stalls[i];
+            }
+            if(cntCow >= cows)return true;
+        }
+
         return false;
     }
     public static int aggressiveCows(int[] stalls, int k) {
-        
+
         return 0;
     }
     public static void main(String[] args) {
