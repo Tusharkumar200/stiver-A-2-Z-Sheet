023e59753bb1dbb23b0868569c9960cb22ebe65c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-15 18:06:56 +0530

    Aggressive_Cows

---
+++ Step4_Binary_Search/BS_on_Answer/Aggressive_Cows.java
@@ -0,0 +1,14 @@
+package BS_on_Answer;
+
+public class Aggressive_Cows {
+    
+    public static int aggressiveCows(int[] stalls, int k) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] stalls = {0, 3, 4, 7, 10, 9};
+        int k = 4;
+        int ans = aggressiveCows(stalls, k);
+        System.out.println("The maximum possible minimum distance is: " + ans);
+    }
+}
