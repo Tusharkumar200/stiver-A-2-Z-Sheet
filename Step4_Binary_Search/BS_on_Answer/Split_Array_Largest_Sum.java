55e2e1ccccd84fdee997e6e953023ce768096a1f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-16 15:47:40 +0530

    testing done

--- Step4_Binary_Search/BS_on_Answer/Split_Array_Largest_Sum.java
+++ Step4_Binary_Search/BS_on_Answer/Split_Array_Largest_Sum.java
@@ -24,8 +24,25 @@ public class Split_Array_Largest_Sum {
     }
     
     public static int largestSubarraySumMinimized(int[] a, int k) {
+        int low = a[0];
+        int high = 0;
 
-        return 0;
+        for(int i=0; i< a.length; i++){
+            low = Math.max(low, a[i]);
+            high +=a[i];
+        }
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            int partitions = countPartitions(a, mid);
+            if(partitions > k ){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return low;
     }
     public static void main(String[] args) {
         int[] a = {10, 20, 30, 40};
