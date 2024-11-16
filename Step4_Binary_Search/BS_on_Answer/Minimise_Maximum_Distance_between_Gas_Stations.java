987e6e50ffe7f2744667a9a94acf11e34da973d5
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-17 01:01:24 +0530

    numberOfGasStationsRequired fun completed

--- Step4_Binary_Search/BS_on_Answer/Minimise_Maximum_Distance_between_Gas_Stations.java
+++ Step4_Binary_Search/BS_on_Answer/Minimise_Maximum_Distance_between_Gas_Stations.java
@@ -1,7 +1,21 @@
 public class Minimise_Maximum_Distance_between_Gas_Stations {
     
+    public static int numberOfGasStationsRequired(double dist, int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        for(int i=1; i< n; i++){
+            int numberInBetween = (int)((arr[i] - arr[i - 1]) / dist);
+
+            if( (arr[i] - arr[i -1]) == (dist * numberInBetween)){
+                numberInBetween--;
+            }
+            count += numberInBetween;
+        }
+        return count;
+    }
     public static double minimiseMaxDistance(int[] arr, int k) {
-        
+
         return 2.2;
     }
     public static void main(String[] args) {
