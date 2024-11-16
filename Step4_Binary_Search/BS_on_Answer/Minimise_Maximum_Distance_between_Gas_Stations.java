568880fc817dbea162e05198250061fd1383301b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-17 00:42:46 +0530

    minimiseMaxDistance and main function created

--- Step4_Binary_Search/BS_on_Answer/Minimise_Maximum_Distance_between_Gas_Stations.java
+++ Step4_Binary_Search/BS_on_Answer/Minimise_Maximum_Distance_between_Gas_Stations.java
@@ -1,6 +1,13 @@
 public class Minimise_Maximum_Distance_between_Gas_Stations {
     
-    public static void main(String[] args) {
+    public static double minimiseMaxDistance(int[] arr, int k) {
         
+        return 2.2;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
     }
 }
