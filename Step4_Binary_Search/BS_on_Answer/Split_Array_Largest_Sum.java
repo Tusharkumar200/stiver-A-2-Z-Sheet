3baa04be414da0102b4a2433a4fb6245a847e35b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-16 15:19:45 +0530

    countPartitions

--- Step4_Binary_Search/BS_on_Answer/Split_Array_Largest_Sum.java
+++ Step4_Binary_Search/BS_on_Answer/Split_Array_Largest_Sum.java
@@ -1,8 +1,30 @@
 package BS_on_Answer;
 
 public class Split_Array_Largest_Sum {
+
+    public static int countPartitions(int[] a, int maxSum) {
+
+        int n = a.length;
+        int partitions = 1;
+        long subarraysSum =0;
+
+        for(int i=0; i < n; i++){
+
+            if(subarraysSum +a[i] <= maxSum){
+
+                subarraysSum += a[i];
+            }
+            else{
+                partitions++;
+                subarraysSum = a[i];
+            }
+        }
+        return partitions;
+
+    }
     
     public static int largestSubarraySumMinimized(int[] a, int k) {
+
         return 0;
     }
     public static void main(String[] args) {
