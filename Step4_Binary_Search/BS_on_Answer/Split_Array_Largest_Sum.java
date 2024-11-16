7024b7de113e3aec3287468fa8c4f3639378c549
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-16 11:20:50 +0530

    Split_Array_Largest_Sum

---
+++ Step4_Binary_Search/BS_on_Answer/Split_Array_Largest_Sum.java
@@ -0,0 +1,14 @@
+package BS_on_Answer;
+
+public class Split_Array_Largest_Sum {
+    
+    public static int largestSubarraySumMinimized(int[] a, int k) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40};
+        int k = 2;
+        int ans = largestSubarraySumMinimized(a, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
