d10f82fc9ca7262cdae9c71e40b01e19165f93fe
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-18 23:22:13 +0530

    basic_of_linkedList

--- Step5_Linked_List/basic_of_linkedList.java
+++ Step5_Linked_List/basic_of_linkedList.java
@@ -1,6 +1,22 @@
+package Step5_Linked_List;
+
+class Node{
+
+    int data;
+    Node next;
+
+    Node(int datas){
+        this.data = datas;
+        this.next = null;
+    }
+}
+
 public class basic_of_linkedList {
     
     public static void main(String[] args) {
-        
+        int []arr = {10,20,30,40,50};
+        Node y = new Node(arr[2]);
+        System.out.println(y.data);
+
     }
 }
