dae7cd26cca96aff413a3c7bb64b1ae21d78ce59
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-18 23:36:57 +0530

    convertarr2LL

--- Step5_Linked_List/basic_of_linkedList.java
+++ Step5_Linked_List/basic_of_linkedList.java
@@ -9,9 +9,27 @@ class Node{
         this.data = datas;
         this.next = null;
     }
+    Node(int datas , Node next){
+        this.data = datas;
+        this.next = next;
+    }
 }
 
 public class basic_of_linkedList {
+
+    private static Node  convertarr2LL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i= 1; i< arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+
+
+    }
     
     public static void main(String[] args) {
         int []arr = {10,20,30,40,50};
