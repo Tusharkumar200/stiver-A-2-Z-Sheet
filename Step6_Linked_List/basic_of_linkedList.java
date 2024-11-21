64d14e54b37565ff8c112781e06e6ea2f65a7b6a
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-22 00:01:57 +0530

    insertPostion && insertBeforeValue

--- Step6_Linked_List/basic_of_linkedList.java
+++ Step6_Linked_List/basic_of_linkedList.java
@@ -1,7 +1,6 @@
 package Step6_Linked_List;
 
 class Node{
-
     int data;
     Node next;
 
@@ -17,7 +16,7 @@ class Node{
 
 public class basic_of_linkedList {
 
-    private static Node  convertarr2LL(int arr[]){
+    private static Node convertarr2LL(int arr[]){
         if(arr.length == 0) return null;
         Node head = new Node(arr[0]);
         Node mover = head;
@@ -28,16 +27,13 @@ public class basic_of_linkedList {
             mover = temp;
         }
         return head;
-
-
     }
 
     private static int lengthOfLL(Node head){
         int count =0;
-        Node temp = head; // use to save the orignal head;
+        Node temp = head; // use to save the original head;
 
         while(temp != null){
-
             System.out.println(temp.data);
             temp = temp.next;
             count++;
@@ -45,8 +41,7 @@ public class basic_of_linkedList {
         return count;
     }
     
-    static boolean searchKey( Node head, int key) {
-        
+    static boolean searchKey(Node head, int key) {
         while(head != null){
             if(head.data == key) return true;
             head = head.next;
@@ -54,8 +49,7 @@ public class basic_of_linkedList {
         return false;
     }
 
-    
-    //* Deletion in the Linklist. 
+    //* Deletion in the LinkedList. 
     public static Node RemoveHead(Node head){
         if(head == null) return head;
         head = head.next;
@@ -63,63 +57,47 @@ public class basic_of_linkedList {
     }
     
     public static Node RemoveTail(Node head){
-
         if(head == null || head.next == null) return head;
         Node temp = head;
         while (temp.next.next != null) {
             temp = temp.next;
         }
+        temp.next = null; // fix: actually remove the tail
         return head;
     }
     
     public static Node RemoveK(Node head , int k){
-        if(head == null)return head;
+        if(head == null) return head;
         if(k == 1){
-            Node temp = head;
-            temp = temp.next;
-            return head;
+            return head.next;
         }
 
-        int count = 0;
-        Node temp  = head;
-        Node prev = null;
-
-        while(temp != null){
-            count++;
-
-            if(count == k){
-                prev.next = prev.next.next;
-                break;
-            }
-            prev = temp;
+        int count = 1;
+        Node temp = head;
+        while(temp != null && count < k - 1){
             temp = temp.next;
+            count++;
+        }
 
+        if(temp != null && temp.next != null){
+            temp.next = temp.next.next;
         }
         return head;
     }
 
     public static Node RemoveElement(Node head , int el){
-        if(head == null)return head;
+        if(head == null) return head;
         if(head.data == el){
-            Node temp = head;
-            temp = temp.next;
-            return head;
+            return head.next;
         }
 
-        
-        Node temp  = head;
-        Node prev = null;
-
-        while(temp != null){
-            
-
-            if(temp.data == el){
-                prev.next = prev.next.next;
+        Node temp = head;
+        while(temp.next != null){
+            if(temp.next.data == el){
+                temp.next = temp.next.next;
                 break;
             }
-            prev = temp;
             temp = temp.next;
-
         }
         return head;
     }
@@ -141,45 +119,76 @@ public class basic_of_linkedList {
         temp.next = newNode;
         return head;
     }
+
+    public static Node insertPostion(Node head, int el, int k){
+        if(head == null){
+            if(k == 1){
+                return new Node(el);
+            }
+            else{
+                return head;
+            }
+        }
+        int cnt = 0;
+        Node temp = head;
+        while (temp != null) {
+            cnt++;
+            if(cnt == (k-1)){
+                Node x = new Node(el, temp.next);
+                temp.next = x;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static Node insertBeforeValue(Node head, int el, int val){
+        if(head == null){return head;}
+
+        
+        Node temp = head;
+        while (temp.next != null) {
+        
+            if(temp.next.data == val){
+                Node x = new Node(el, temp.next);
+                temp.next = x;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+
+    private static void printLinkedList(Node head) {
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int []arr = {10,20,30,40,50};
 
-        // Node y = new Node(arr[2]);
-        // System.out.println(y.data);
-
         Node head = convertarr2LL(arr);
-        // System.out.println(head.data );
+        printLinkedList(head);
 
-        // Node temp = head;
-        // while(temp != null){
-        //     System.out.print ( temp.data + " -> ");
-        //     temp = temp.next;
-        // }
-
-        // Length of the LL
         int len = lengthOfLL(head);
-        System.out.println("length = "+len);
+        System.out.println("length = " + len);
 
-        // boolean search = searchKey(head, 40);
-        // System.out.println(search);
-        
-        // Remove the Head
-        // head = RemoveHead(head);
-        // Remove tail
-        //  head = RemoveTail(head);
-        
-        // System.out.println(head.data +" tail removed");
+        head = RemoveK(head, 2);
+        System.out.print("After removing 2nd element: ");
+        printLinkedList(head);
 
-         head = RemoveK(head, 2);
-        System.out.println(head +" k elment removed ");
+        head = insertPostion(head, 80, 3);
+        System.out.print("After inserting 80 at position 3: ");
+        printLinkedList(head);
 
-        head = RemoveElement(head , 20);
-        System.out.println(head);
-        
-        
-        
-
+        head = insertBeforeValue(head, 875, 30);
+        System.out.print("insert 875 Before Value  30 : ");
+        printLinkedList(head);
     }
-
-    
 }
