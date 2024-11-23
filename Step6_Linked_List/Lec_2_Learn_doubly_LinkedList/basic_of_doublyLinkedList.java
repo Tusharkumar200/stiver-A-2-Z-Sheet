f7d3f98273bf90c739b352537d54bfd23c1c0a5f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-23 23:22:46 +0530

    node created

---
+++ Step6_Linked_List/Lec_2_Learn_doubly_LinkedList/basic_of_doublyLinkedList.java
@@ -0,0 +1,27 @@
+package Step6_Linked_List.Lec_2_Learn_doubly_LinkedList;
+
+ class Node{
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int datas){
+        this.data = datas;
+        this.next = null;
+        this.prev = null;
+    }
+    Node(int datas , Node next , Node prev){
+        this.data = datas;
+        this.next = next;
+        this.prev  = prev;
+    }
+}
+
+public class basic_of_doublyLinkedList {
+    
+
+    public static void main(String[] args) {
+        int []arr = {10,20,30,40,50};
+
+    }
+}
