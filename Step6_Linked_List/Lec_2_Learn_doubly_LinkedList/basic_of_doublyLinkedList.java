2e0a5fcf4447fc500f80a19fe871e87db9417e3f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-23 23:36:40 +0530

    convertArr2DLL

--- Step6_Linked_List/Lec_2_Learn_doubly_LinkedList/basic_of_doublyLinkedList.java
+++ Step6_Linked_List/Lec_2_Learn_doubly_LinkedList/basic_of_doublyLinkedList.java
@@ -19,9 +19,20 @@ package Step6_Linked_List.Lec_2_Learn_doubly_LinkedList;
 
 public class basic_of_doublyLinkedList {
     
+    public static Node convertArr2DLL(int[]arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
 
+        for( int i = 1; i<arr.length; i++){
+           Node temp = new Node(arr[i], null, prev);
+           prev.next = temp;
+           prev = temp;
+        }
+        return head;
+    }
     public static void main(String[] args) {
         int []arr = {10,20,30,40,50};
+        Node head = convertArr2DLL(arr);
 
     }
 }
