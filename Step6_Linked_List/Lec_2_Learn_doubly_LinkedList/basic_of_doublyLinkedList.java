6704508bca7054c0b1afaa580141a0c6879b2b43
Tushar Kumar <tusharnwd2003@gmail.com>
2024-11-24 17:12:39 +0530

    removeKthElement

--- Step6_Linked_List/Lec_2_Learn_doubly_LinkedList/basic_of_doublyLinkedList.java
+++ Step6_Linked_List/Lec_2_Learn_doubly_LinkedList/basic_of_doublyLinkedList.java
@@ -1,38 +1,39 @@
 package Step6_Linked_List.Lec_2_Learn_doubly_LinkedList;
 
- class Node{
+class Node {
     int data;
     Node next;
     Node prev;
 
-    Node(int datas){
+    Node(int datas) {
         this.data = datas;
         this.next = null;
         this.prev = null;
     }
-    Node(int datas , Node next , Node prev){
+
+    Node(int datas, Node next, Node prev) {
         this.data = datas;
         this.next = next;
-        this.prev  = prev;
+        this.prev = prev;
     }
 }
 
 public class basic_of_doublyLinkedList {
-    
-    public static Node convertArr2DLL(int[]arr){
+
+    public static Node convertArr2DLL(int[] arr) {
         Node head = new Node(arr[0]);
         Node prev = head;
 
-        for( int i = 1; i<arr.length; i++){
-           Node temp = new Node(arr[i], null, prev);
-           prev.next = temp;
-           prev = temp;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
         }
         return head;
     }
 
-    public static Node DeleteHead(Node head){
-        if(head == null || head.next == null){
+    public static Node DeleteHead(Node head) {
+        if (head == null || head.next == null) {
             return null;
         }
 
@@ -42,35 +43,64 @@ public class basic_of_doublyLinkedList {
         temp.next = null;
         return head;
     }
-    public static Node deleteTail(Node head){
-        if(head == null || head.next == null)return null;
+
+    public static Node deleteTail(Node head) {
+        if (head == null || head.next == null) return null;
         Node tail = head;
 
-        while(tail.next != null){
+        while (tail.next != null) {
             tail = tail.next;
-
         }
-        Node newTail= tail.prev;
+        Node newTail = tail.prev;
         newTail.next = null;
         tail.prev = null;
 
         return head;
     }
-    
+
+    public static Node removeKthElement(Node head, int k) {
+        if (head == null || k <= 0) return head;
+        if (k == 1) return DeleteHead(head);
+
+        int cnt = 1;
+        Node kNode = head;
+        while (kNode != null && cnt < k) {
+            cnt++;
+            kNode = kNode.next;
+        }
+
+        if (kNode == null) return head;
+
+        Node prev = kNode.prev;
+        Node front = kNode.next;
+
+        if (prev == null && front == null) return null;
+        else if (prev == null) return deleteTail(head);
+
+        prev.next = front;
+        if (front != null) front.prev = prev;
+
+        kNode.next = null;
+        kNode.prev = null;
+
+        return head;
+    }
+
     private static void printLinkedList(Node head) {
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
-    
+
     public static void main(String[] args) {
-        int []arr = {10,20,30,40,50};
+        int[] arr = {10, 20, 30, 40, 50};
         Node head = convertArr2DLL(arr);
-        head = DeleteHead(head);
-        head = deleteTail(head);
+        // head = DeleteHead(head);
+        // head = deleteTail(head);
+        head = removeKthElement(head, 5); // Change to a valid k value for testing
         printLinkedList(head);
     }
 }
