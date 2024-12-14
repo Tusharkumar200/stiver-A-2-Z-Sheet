cd50aa7a1eadd32eacb1afcc5990428e0fe4d6b7
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-14 15:15:17 +0530

    Step6_Linked_List.Lec_3_Medium_Problems_of_LL

---
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Find_middle_element_in_a_Linked_List.java
@@ -0,0 +1,21 @@
+package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+
+class Node{
+
+    int data;
+    Node next;
+
+    Node(int datas){
+        this.data = datas;
+        this.next = null;
+    }
+    Node(int datas , Node next){
+        this.data = datas;
+        this.next = next;
+    }
+}
+
+public class Find_middle_element_in_a_Linked_List {
+    
+    
+}
