c604654d5963644450ccc93a95e5934d450daf66
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-15 21:21:19 +0530

    Reverse_a_Linked_List

---
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Reverse_a_Linked_List.java
@@ -0,0 +1,16 @@
+package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Reverse_a_Linked_List {
+    
+}
