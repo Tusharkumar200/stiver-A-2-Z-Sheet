2c8750ac79cd883f75c4a45141d2b5ca3ea4c9ee
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-17 14:30:29 +0530

    DetectCycle

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Detect_a_Cycle_in_a_Linked_List.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Detect_a_Cycle_in_a_Linked_List.java
@@ -15,11 +15,42 @@ public class Detect_a_Cycle_in_a_Linked_List {
     
     public static boolean DetectCycle(Node head){
 
-        
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast ) return true;
+        }
+
         return false;
     }
 
     public static void main(String[] args) {
         
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        // Create a loop
+        fifth.next = third;
+
+        // Check if there is a loop
+        // in the linked list
+        if (DetectCycle(head)) {
+            System.out.println("Loop detected in the linked list.");
+        } else {
+            System.out.println("No loop detected in the linked list.");
+        }
+
     }
 }
