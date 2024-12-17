c841a554f277a772fe27a21e0f4e88cab7362884
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-17 14:21:40 +0530

    Detect_a_Cycle_in_a_Linked_List

---
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Detect_a_Cycle_in_a_Linked_List.java
@@ -0,0 +1,25 @@
+package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Detect_a_Cycle_in_a_Linked_List {
+    
+    public static boolean DetectCycle(Node head){
+
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
