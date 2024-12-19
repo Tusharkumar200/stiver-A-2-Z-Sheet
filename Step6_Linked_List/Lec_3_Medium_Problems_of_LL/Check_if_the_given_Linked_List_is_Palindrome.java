ff2f35ac4fe2bd1842cc7e4c001f8bd62985da5b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-19 13:24:52 +0530

    isPalindrome

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Check_if_the_given_Linked_List_is_Palindrome.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Check_if_the_given_Linked_List_is_Palindrome.java
@@ -25,7 +25,33 @@ public class Check_if_the_given_Linked_List_is_Palindrome {
     }
 
     public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
 
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next !=null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        Node newHead = reverseLinkedList(slow.next);
+        Node first = head;
+        Node second = newHead;
+
+        while (second !=null) {
+            if(first.data != second.data){
+                reverseLinkedList(newHead);
+                return false;
+
+            }
+            first = first.next;
+            second = second.next;
+
+        }
+        reverseLinkedList(newHead);
+        return true;
+        
     }
     
     public static void printLinkedList(Node head) {
