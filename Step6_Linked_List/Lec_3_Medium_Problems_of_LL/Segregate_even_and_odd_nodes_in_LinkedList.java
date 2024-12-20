636299f46fc6186a5bb15dfd669909a665c26dac
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-20 16:43:46 +0530

    ListNode created

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Segregate_even_and_odd_nodes_in_LinkedList.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Segregate_even_and_odd_nodes_in_LinkedList.java
@@ -1,7 +1,26 @@
 package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
 
+class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
 public class Segregate_even_and_odd_nodes_in_LinkedList {
     
+
+    static void PrintList(ListNode head) // Function to print the LinkedList
+{
+    ListNode curr = head;
+    for (; curr != null; curr = curr.next)
+        System.out.print(curr.val+"-->");
+    System.out.println("null");
+}
     public static void main(String[] args) {
         
     }
