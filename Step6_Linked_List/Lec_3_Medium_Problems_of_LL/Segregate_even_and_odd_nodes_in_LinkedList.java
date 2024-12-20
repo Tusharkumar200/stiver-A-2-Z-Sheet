c53b0e21ca2fe76006fb72f8ea5ba75dd9fc9ffe
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-20 16:16:19 +0530

    Segregate_even_and_odd_nodes_in_LinkedList

---
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Segregate_even_and_odd_nodes_in_LinkedList.java
@@ -0,0 +1,8 @@
+package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+
+public class Segregate_even_and_odd_nodes_in_LinkedList {
+    
+    public static void main(String[] args) {
+        
+    }
+}
