f9a25c753f417a7007469996f55af8272001ceea
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-21 12:43:32 +0530

    Remove_Nth_node_from_the_end_of_a_LL

---
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Remove_Nth_node_from_the_end_of_a_LL.java
@@ -0,0 +1,26 @@
+package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+
+
+class Node {
+    public int data;
+    public Node next;
+
+    // Constructor for Node with data and next node
+    public Node(int data1, Node next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor for Node with only data (next set to null)
+    public Node(int data1) {
+        data = data1;
+        next = null;
+    }
+}
+public class Remove_Nth_node_from_the_end_of_a_LL {
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
