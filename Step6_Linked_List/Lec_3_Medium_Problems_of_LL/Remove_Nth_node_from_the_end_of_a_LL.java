061836c7bd1e8137fdb6e45308916718317f3336
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-21 12:46:09 +0530

    created main function

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Remove_Nth_node_from_the_end_of_a_LL.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Remove_Nth_node_from_the_end_of_a_LL.java
@@ -21,6 +21,16 @@ public class Remove_Nth_node_from_the_end_of_a_LL {
     
 
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2, 3, 4, 5};
+        int N = 3;
+        Node head = new Node(arr[0]);
+        head.next = new Node(arr[1]);
+        head.next.next = new Node(arr[2]);
+        head.next.next.next = new Node(arr[3]);
+        head.next.next.next.next = new Node(arr[4]);
+
+        // Delete the Nth node from the end and print the modified linked list
+        head = DeleteNthNodefromEnd(head, N);
+        printLL(head);
     }
 }
