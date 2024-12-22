8359c8a951ab4ccc4e77f6f9e4d82f8f140b9b86
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-22 13:13:40 +0530

    main method written

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Delete_the_Middle_Node_of_the_LL.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Delete_the_Middle_Node_of_the_LL.java
@@ -1,9 +1,60 @@
 package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
 
+class Node {
+    // Data stored in the node
+    int data; 
+     // Pointer to the next node in the list
+    Node next;     
+
+    // Constructor with both data
+    // and next node as parameters
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    // Constructor with only data as
+    // a parameter, sets next to null
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 public class Delete_the_Middle_Node_of_the_LL {
     
 
+    public Node deleteMiddle(Node head) {
+
+    }
+    
+    static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
+        Main main = new Main();
         
+        // Creating a sample linked list:
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        // Display the original linked list
+        System.out.print("Original Linked List: ");
+        printLL(head);
+
+        // Deleting the middle node
+        head = main.deleteMiddle(head);
+
+        // Displaying the updated linked list
+        System.out.print("Updated Linked List: ");
+        printLL(head);
     }
 }
