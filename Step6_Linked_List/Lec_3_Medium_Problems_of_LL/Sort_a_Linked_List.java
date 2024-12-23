3d3510a3c61b914684d9fbfbaae4ff4925260064
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-23 15:21:20 +0530

    Node created

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Sort_a_Linked_List.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Sort_a_Linked_List.java
@@ -1,7 +1,33 @@
 package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
 
+class Node {
+    // Data stored in the node
+    int data;
+    
+    // Pointer to the next node in the list
+    Node next;
+
+    // Constructor with both data and
+    // next node as parameters
+    Node(int data1, Node next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor with only data as a
+    // parameter, sets next to null
+    Node(int data1) {
+        data = data1;
+        next = null;
+    }
+}
+
 public class Sort_a_Linked_List {
     
+    static Node sortLL(Node head){
+
+    }
+
     void printLinkedList(Node head) {
         Node temp = head;
         while (temp != null) {
