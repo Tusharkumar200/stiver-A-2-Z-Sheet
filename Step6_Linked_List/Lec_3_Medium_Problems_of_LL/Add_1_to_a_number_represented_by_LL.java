be2b920ad3bbd11c455f00a489a400ada6b6172d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-25 22:02:29 +0530

    node created

--- Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Add_1_to_a_number_represented_by_LL.java
+++ Step6_Linked_List/Lec_3_Medium_Problems_of_LL/Add_1_to_a_number_represented_by_LL.java
@@ -1,19 +1,35 @@
 package Step6_Linked_List.Lec_3_Medium_Problems_of_LL;
+class Node {
+    int data;
+    Node next;
 
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 public class Add_1_to_a_number_represented_by_LL {
     
+    static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-     // Create a hard-coded linked list:
-    // 1 -> 9 -> 9 -> 9
-    Node *head = new Node(1);
-    head->next = new Node(9);
-    head->next->next = new Node(9);
-    head->next->next->next = new Node(9);
-  
-    head = addOne(head);
 
-    printList(head);
+        // Create a hard-coded linked list:
+        // 1 -> 9 -> 9 -> 9
+        Node head = new Node(1);
+        head.next = new Node(9);
+        head.next.next = new Node(9);
+        head.next.next.next = new Node(9);
 
-    return 0;
+        head = addOne(head);
+
+        printList(head);
     }
 }
