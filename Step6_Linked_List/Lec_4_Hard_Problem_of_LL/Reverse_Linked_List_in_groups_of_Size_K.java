4f5152182d8026e6216787155ec3baba995caf11
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-27 23:12:34 +0530

    Reverse_Linked_List_in_groups_of_Size_K

---
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Reverse_Linked_List_in_groups_of_Size_K.java
@@ -0,0 +1,46 @@
+package Step6_Linked_List.Lec_4_Hard_Problem_of_LL;
+
+class Node {
+    // Data stored in the node
+    int data;      
+    // Pointer to the next
+    // node in the list
+    Node next;     
+
+    // Constructor with both data
+    // and next node as parameters
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    // Constructor with only data as
+    // a parameter, sets next to null
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Reverse_Linked_List_in_groups_of_Size_K {
+    
+    public static void main(String[] args) {
+         Node head = new Node(5);
+        head.next = new Node(4);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(7);
+        head.next.next.next.next = new Node(9);
+        head.next.next.next.next.next = new Node(2);
+
+        // Print the original linked list
+        System.out.print("Original Linked List: ");
+        printLinkedList(head);
+
+        // Reverse the linked list
+        head = kReverse(head, 4);
+
+        // Print the reversed linked list
+        System.out.print("Reversed Linked List: ");
+        printLinkedList(head);
+    }
+    
+}
