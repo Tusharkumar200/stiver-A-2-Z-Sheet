58afd6f42204eb16aba7c45bb3d01bc86740a9b5
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-28 23:15:51 +0530

    kReverse

--- Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Reverse_Linked_List_in_groups_of_Size_K.java
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Reverse_Linked_List_in_groups_of_Size_K.java
@@ -2,10 +2,10 @@ package Step6_Linked_List.Lec_4_Hard_Problem_of_LL;
 
 class Node {
     // Data stored in the node
-    int data;      
+    int data;
     // Pointer to the next
     // node in the list
-    Node next;     
+    Node next;
 
     // Constructor with both data
     // and next node as parameters
@@ -21,45 +21,77 @@ class Node {
         this.next = null;
     }
 }
+
 public class Reverse_Linked_List_in_groups_of_Size_K {
 
     static Node reverseLinkedList(Node head) {
         Node temp = head;
         Node prev = null;
 
-        while(temp != null){
+        while (temp != null) {
             Node front = temp.next;
             temp.next = prev;
-            prev= temp;
+            prev = temp;
             temp = front;
         }
         return prev;
     }
 
     static Node getKthNode(Node temp, int k) {
-            k = 1;
-            while (temp != null && k> 0) {
-                k--;
-                temp = temp.next;
+        k = 1;
+        while (temp != null && k > 0) {
+            k--;
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    static Node kReverse(Node head, int k) {
+
+        Node temp = head;
+        Node prevLast = null;
+
+        while (temp != null) {
+            Node kThNode = getKthNode(temp, k);
+
+            if (kThNode == null) {
+                if (prevLast != null) {
+                    prevLast.next = temp;
+                }
+
+                break;
             }
-            return temp;
+
+            Node nextNode = kThNode.next;
+            kThNode.next = null;
+            reverseLinkedList(temp);
+
+            if (temp == head) {
+                head = kThNode;
+
+            } else {
+                prevLast.next = kThNode;
+            }
+
+            prevLast = temp;
+            temp = nextNode;
+
+        }
+        return head;
     }
 
-    static Node kReverse(Node head, int k){
-
-    }
-
-    public static void printLinkedList(Node head){
+    public static void printLinkedList(Node head) {
         Node temp = head;
 
-        while(temp != null){
+        while (temp != null) {
             System.out.println(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-         Node head = new Node(5);
+        Node head = new Node(5);
         head.next = new Node(4);
         head.next.next = new Node(3);
         head.next.next.next = new Node(7);
@@ -77,5 +109,5 @@ public class Reverse_Linked_List_in_groups_of_Size_K {
         System.out.print("Reversed Linked List: ");
         printLinkedList(head);
     }
-    
+
 }
