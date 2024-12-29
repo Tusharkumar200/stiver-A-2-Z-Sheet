2034ae92bbace255dfd703d5b436cb14fd76b71d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-29 13:35:45 +0530

    error solved

--- Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Reverse_Linked_List_in_groups_of_Size_K.java
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Reverse_Linked_List_in_groups_of_Size_K.java
@@ -38,6 +38,7 @@ public class Reverse_Linked_List_in_groups_of_Size_K {
     }
 
     static Node getKthNode(Node temp, int k) {
+        k -= 1;
         while (temp != null && k > 1) {
             k--;
             temp = temp.next;
@@ -48,34 +49,63 @@ public class Reverse_Linked_List_in_groups_of_Size_K {
     static Node kReverse(Node head, int k) {
 
         Node temp = head;
+        
+        // Initialize a pointer to track the
+        // last node of the previous group
         Node prevLast = null;
-
+        
+        // Traverse through the linked list
         while (temp != null) {
+            
+            // Get the Kth node of the current group
             Node kThNode = getKthNode(temp, k);
-
+            
+            // If the Kth node is NULL
+            // (not a complete group)
             if (kThNode == null) {
+               
+                // If there was a previous group,
+                // link the last node to the current node
                 if (prevLast != null) {
                     prevLast.next = temp;
                 }
-
+                
+                // Exit the loop
                 break;
             }
-
+            
+            // Store the next node
+            // after the Kth node
             Node nextNode = kThNode.next;
+            
+            // Disconnect the Kth node
+            // to prepare for reversal
             kThNode.next = null;
+            
+            // Reverse the nodes from
+            // temp to the Kth node
             reverseLinkedList(temp);
-
+            
+             // Adjust the head if the reversal
+            // starts from the head
             if (temp == head) {
                 head = kThNode;
-
             } else {
+                // Link the last node of the previous
+                // group to the reversed group
                 prevLast.next = kThNode;
             }
-
+            
+            // Update the pointer to the
+            // last node of the previous group
             prevLast = temp;
+            
+            // Move to the next group
             temp = nextNode;
-
         }
+        
+        // Return the head of the
+        // modified linked list
         return head;
     }
 
