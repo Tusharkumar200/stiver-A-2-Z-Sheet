34e1e9d4389e698e1728ffe3a0caa7028afd066d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-30 22:10:39 +0530

    rotateRight

--- Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Rotate_a_LL.java
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Rotate_a_LL.java
@@ -27,6 +27,28 @@ public class Rotate_a_LL {
 
     static Node rotateRight(Node head,int k) {
 
+        if(head == null || head.next == null || k == 0) return head;
+        Node temp = head;
+        int length =1;
+
+        while(temp !=null){
+            length++;
+            temp = temp.next;
+        }
+
+        temp.next = head;
+        k = k%length;
+        int end = length -k;
+
+        while (end-- != 0) {
+            temp = temp.next;
+        }
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+
+
     }    
 
     static void printList(Node head) {
