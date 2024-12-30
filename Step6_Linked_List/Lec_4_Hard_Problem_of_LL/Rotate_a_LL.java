7a7fa901d1c59f3fba68f858398edaa4464322f9
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-30 21:50:59 +0530

    Node created

---
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Rotate_a_LL.java
@@ -0,0 +1,16 @@
+
+class Node {
+    int num;
+    Node next;
+    Node(int a) {
+      num = a;
+      next = null;
+    }
+  }
+  
+public class Rotate_a_LL {
+    
+    public static void main(String[] args) {
+        
+    }
+}
