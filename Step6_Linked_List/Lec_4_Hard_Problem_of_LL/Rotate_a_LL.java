a02cb365772cd90af5d1dc25040762bbca8bdd10
Tushar Kumar <tusharnwd2003@gmail.com>
2024-12-30 21:54:36 +0530

    function created

--- Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Rotate_a_LL.java
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/Rotate_a_LL.java
@@ -7,10 +7,33 @@ class Node {
       next = null;
     }
   }
-  
+
 public class Rotate_a_LL {
-    
-    public static void main(String[] args) {
+
+    static Node insertNode(Node head,int val) {
         
+    }
+
+    static Node rotateRight(Node head,int k) {
+
+    }    
+    public static void main(String[] args) {
+        Node head = null;
+    //inserting Node
+    head=insertNode(head,1);
+    head=insertNode(head,2);
+    head=insertNode(head,3);
+    head=insertNode(head,4);
+    head=insertNode(head,5);
+    
+    System.out.println("Original list: ");
+    printList(head);
+    
+    int k = 2;
+    Node newHead = rotateRight(head,k);//calling function for rotating right 
+    of the nodes by k times
+    
+    System.out.println("After "+k+" iterations: ");
+    printList(newHead);
     }
 }
