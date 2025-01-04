e83cae7669d574628a4138ea898566cc155a9d31
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-04 17:38:02 +0530

    Main methord completeed

---
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/FlattenLinkedList.java
@@ -0,0 +1,29 @@
+public class FlattenLinkedList {
+    
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.child = new Node(14);
+
+        head.next = new Node(10);
+        head.next.child = new Node(4);
+
+        head.next.next = new Node(12);
+        head.next.next.child = new Node(20);
+        head.next.next.child.child = new Node(13);
+
+        head.next.next.next = new Node(7);
+        head.next.next.next.child = new Node(17);
+
+        // Print the original
+        // linked list structure
+        System.out.println("Original linked list:");
+        printOriginalLinkedList(head, 0);
+
+        // Flatten the linked list
+        // and print the flattened list
+        Node flattened = flattenLinkedList(head);
+        System.out.println("\nFlattened linked list: ");
+        printLinkedList(flattened);
+    }
+}
