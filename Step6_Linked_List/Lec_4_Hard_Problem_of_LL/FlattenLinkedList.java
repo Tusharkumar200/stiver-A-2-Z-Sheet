27f317fa2d8d819b7ee4c049746517464e03cffb
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-04 19:09:31 +0530

    flattenLinkedList

--- Step6_Linked_List/Lec_4_Hard_Problem_of_LL/FlattenLinkedList.java
+++ Step6_Linked_List/Lec_4_Hard_Problem_of_LL/FlattenLinkedList.java
@@ -28,6 +28,38 @@ public class FlattenLinkedList {
 
     public static Node merge(Node list1, Node list2) {
 
+        Node dummyNode = new Node(-1);
+        Node res = dummyNode;
+
+        while (list1 !=null && list2 !=null) {
+            
+            if(list1.data < list2.data){
+
+                res.child = list1;
+                res = list1;
+                list1 = list1.child;
+            }
+            else{
+                res.child = list2;
+                res = list2;
+                list2 = list2.child;
+            }
+            res.next = null;
+        }
+
+        if(list1 != null){
+            res.child = list1;
+        }
+        else{
+            res.child = list2;
+        }
+
+        if(dummyNode.child != null){
+            dummyNode.child.next = null;
+        }
+
+        return dummyNode.child;
+
     }
 
     public static Node flattenLinkedList(Node head) {
