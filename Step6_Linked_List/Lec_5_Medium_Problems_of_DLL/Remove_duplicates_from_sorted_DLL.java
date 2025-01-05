67f51cb4b82a477aa0de3aceae2cfbabcb11068e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 15:28:14 +0530

    Remove_duplicates_from_sorted_DLL

---
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Remove_duplicates_from_sorted_DLL.java
@@ -0,0 +1,33 @@
+package Step6_Linked_List.Lec_5_Medium_Problems_of_DLL;
+
+class Node
+{
+    int data;
+    Node next,prev;
+    
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
+public class Remove_duplicates_from_sorted_DLL {
+    
+    static Node removeDuplicates(Node head){
+        
+        Node temp = head;
+
+        while(temp != null || temp.next != null){
+            Node nextNode = temp.next;
+
+            while(nextNode !=null || nextNode.va)
+        }
+
+        return head;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
