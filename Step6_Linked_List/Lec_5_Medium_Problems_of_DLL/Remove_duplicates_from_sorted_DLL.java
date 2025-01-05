bda20660159f43638cea34f2963ef033b34f7fda
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 15:32:50 +0530

    printList

--- Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Remove_duplicates_from_sorted_DLL.java
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Remove_duplicates_from_sorted_DLL.java
@@ -33,7 +33,35 @@ public class Remove_duplicates_from_sorted_DLL {
         return head;
         
     }
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.prev = head;
+        head.next.next = new Node(2);
+        head.next.next.prev = head.next;
+        head.next.next.next = new Node(3);
+        head.next.next.next.prev = head.next.next;
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.prev = head.next.next.next;
+    
+        // Print the original list
+        System.out.print("Original list: ");
+        printList(head);
+    
+        // Remove duplicates
+        head = removeDuplicates(head);
+    
+        // Print the modified list
+        System.out.print("Modified list: ");
+        printList(head);
     }
 }
