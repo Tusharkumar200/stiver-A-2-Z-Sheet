b3fb37447d49c40189d80fd24521fb39721ae51c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 13:57:08 +0530

    Lec_5_Medium_Problems_of_DLL

---
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Delete_all_occurrences_of_a_key_in_DLL.java
@@ -0,0 +1,21 @@
+package Step6_Linked_List.Lec_5_Medium_Problems_of_DLL;
+
+// Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+public class Delete_all_occurrences_of_a_key_in_DLL {
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
