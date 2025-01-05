de2414eee640428f035f2b080d7a2d62991720b7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 14:51:42 +0530

    Node created

--- Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Find_pairs_with_given_sum_in_doubly_linked_list.java
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Find_pairs_with_given_sum_in_doubly_linked_list.java
@@ -2,10 +2,25 @@ package Step6_Linked_List.Lec_5_Medium_Problems_of_DLL;
 
 import java.util.ArrayList;
 
+class Node
+{
+    int data;
+    Node next,prev;
+    
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
 public class Find_pairs_with_given_sum_in_doubly_linked_list {
     
     public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
-            
+        ArrayList<ArrayList<Integer>> ans;
+
+
+        return ans;
     }
 
     public static void main(String[] args) {
