284e11d72344f3c6469a330d9c33bc1574266f4b
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 14:50:05 +0530

    Find_pairs_with_given_sum_in_doubly_linked_list

---
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Find_pairs_with_given_sum_in_doubly_linked_list.java
@@ -0,0 +1,14 @@
+package Step6_Linked_List.Lec_5_Medium_Problems_of_DLL;
+
+import java.util.ArrayList;
+
+public class Find_pairs_with_given_sum_in_doubly_linked_list {
+    
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
+            
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
