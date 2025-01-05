258c31b40fde862a3729f36e52b9c9850c060690
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-05 14:04:55 +0530

    function completed

--- Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Delete_all_occurrences_of_a_key_in_DLL.java
+++ Step6_Linked_List/Lec_5_Medium_Problems_of_DLL/Delete_all_occurrences_of_a_key_in_DLL.java
@@ -19,11 +19,61 @@ public class Delete_all_occurrences_of_a_key_in_DLL {
         Node temp = head;
         while (temp != null) {
             
+            if(temp.data == x){
+
+                if(temp == head){
+                    head = head.next;
+                }
+                Node nextNode = temp.next;
+                Node prevNode = temp.prev;
+                
+                if(nextNode != null){
+                    nextNode.prev = prevNode;
+                }
+                if(prevNode != null){
+                    prevNode.next = nextNode;
+                }
+
+                temp = nextNode;
+
+            }
+            else{
+                temp = temp.next;
+            }
         }
         return head;
     }
 
+
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.prev = head;
+        head.next.next = new Node(3);
+        head.next.next.prev = head.next;
+        head.next.next.next = new Node(2);
+        head.next.next.next.prev = head.next.next;
+        head.next.next.next.next = new Node(4);
+        head.next.next.next.next.prev = head.next.next.next;
+    
+        // Print the original list
+        System.out.print("Original list: ");
+        printList(head);
+    
+        // Delete all occurrences of key 2
+        int key = 2;
+        head = deleteAllOccurOfX(head, key);
+    
+        // Print the modified list
+        System.out.print("Modified list: ");
+        printList(head);
     }
 }
