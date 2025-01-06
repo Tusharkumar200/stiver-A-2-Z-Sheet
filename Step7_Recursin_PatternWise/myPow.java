5ef4602c61c7b0b1cc1d5758c629b23bd81ffbe7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-06 22:19:28 +0530

    optimal solution

--- Step7_Recursin_PatternWise/myPow.java
+++ Step7_Recursin_PatternWise/myPow.java
@@ -9,6 +9,25 @@ public class myPow {
         }
         return ans;
     }
+
+    // optimal solution
+    public static double myPow2(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0) nn = -1 * nn;
+        while (nn > 0) {
+          if (nn % 2 == 1) {
+            ans = ans * x;
+            nn = nn - 1;
+          } else {
+            x = x * x;
+            nn = nn / 2;
+          }
+        }
+        if (n < 0) ans = (double)(1.0) / (double)(ans);
+        return ans;
+      }
+      
     public static void main(String[] args) {
         System.out.println(Pow(2, 10));
     }
