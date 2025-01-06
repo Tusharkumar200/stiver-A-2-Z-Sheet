641b32db960122248d404f6c5dddec5922bbd45c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-06 22:09:47 +0530

    pow function created

---
+++ Step7_Recursin_PatternWise/myPow.java
@@ -0,0 +1,11 @@
+package Step7_Recursin_PatternWise;
+
+public class myPow {
+    
+    public static double Pow(double x, int n) {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(Pow(2, 10));
+    }
+}
