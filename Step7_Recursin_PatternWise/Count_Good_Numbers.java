4fffcb5e670c4bc15807388808c7db77f8baa00f
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-08 07:36:39 +0530

    Pow function

--- Step7_Recursin_PatternWise/Count_Good_Numbers.java
+++ Step7_Recursin_PatternWise/Count_Good_Numbers.java
@@ -2,7 +2,7 @@
 public class Count_Good_Numbers{
     
     private long MOD = 1_000_000_007;
-    
+
     public int countGoodNumbers(long n) {
         
         long even = (n + 1)/2;
@@ -14,6 +14,18 @@ public class Count_Good_Numbers{
         return (int) ((first * second)%MOD);
     }
 
+    private long pow(long x, long n){
+        if(n == 0)return 1;
+
+        long temp = pow(x,n/2);
+
+        if(n % 2 == 0){
+            return (temp * temp)%MOD;
+        }
+        else{
+            return (x * temp * temp)%MOD;
+        }
+    }
     
     public static void main(String[] args) {
         
