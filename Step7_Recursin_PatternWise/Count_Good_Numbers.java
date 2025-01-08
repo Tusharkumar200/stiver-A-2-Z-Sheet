07522e163cf6906e1922bfe3f84f7c95e5ceb3af
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-08 07:36:00 +0530

    countGoodNumbers

--- Step7_Recursin_PatternWise/Count_Good_Numbers.java
+++ Step7_Recursin_PatternWise/Count_Good_Numbers.java
@@ -1,6 +1,19 @@
 
 public class Count_Good_Numbers{
     
+    private long MOD = 1_000_000_007;
+    
+    public int countGoodNumbers(long n) {
+        
+        long even = (n + 1)/2;
+        long odd = (n/2);
+
+        long first = pow(5 , even)%MOD;
+        long second = pow(4 , odd)%MOD;
+
+        return (int) ((first * second)%MOD);
+    }
+
     
     public static void main(String[] args) {
         
