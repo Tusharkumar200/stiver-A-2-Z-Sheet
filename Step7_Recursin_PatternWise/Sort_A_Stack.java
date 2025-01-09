08e9c9b19a740f8804e9dce86d1b93c7a92c7cd6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-09 21:32:44 +0530

    sortedInsert

--- Step7_Recursin_PatternWise/Sort_A_Stack.java
+++ Step7_Recursin_PatternWise/Sort_A_Stack.java
@@ -4,11 +4,18 @@ public class Sort_A_Stack {
     
     public static void sortedInsert(Stack<Integer> stack, int key)  
     { 
-        
+        if(stack.isEmpty() || key > stack.peek()){
+            stack.push(key);
+            return;
+        }
+
+        int top = stack.pop();
+        sortedInsert(stack, key);
+        stack.push(top);
      }
 
     public Stack<Integer> sort(Stack<Integer> s) {
-        // add code here.
+        
     }
     public static void main(String[] args) {
         
