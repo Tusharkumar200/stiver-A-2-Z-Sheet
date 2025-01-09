73637877de7fd7983547ea03b3ea2444855bf9ea
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-09 21:40:46 +0530

    Main Function

--- Step7_Recursin_PatternWise/Sort_A_Stack.java
+++ Step7_Recursin_PatternWise/Sort_A_Stack.java
@@ -25,6 +25,20 @@ public class Sort_A_Stack {
         
     }
     public static void main(String[] args) {
-        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(3);
+        stack.push(31);
+        stack.push(98);
+        stack.push(92);
+        stack.push(23);
+
+        Sort_A_Stack sorter = new Sort_A_Stack();
+        stack = sorter.sort(stack);
+
+        System.out.println("Sorted stack:");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 }
