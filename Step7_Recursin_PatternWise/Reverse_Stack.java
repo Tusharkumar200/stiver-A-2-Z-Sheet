542b8bd8d6e61e5d449aac108d19ba1ca3e9f3bd
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-09 22:12:13 +0530

    main

--- Step7_Recursin_PatternWise/Reverse_Stack.java
+++ Step7_Recursin_PatternWise/Reverse_Stack.java
@@ -19,6 +19,19 @@ public class Reverse_Stack {
         stack.push(topElem);
     }
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(3);
+        stack.push(31);
+        stack.push(98);
+        stack.push(92);
+        stack.push(23);
+
         
+
+        System.out.println("Sorted stack:");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 }
