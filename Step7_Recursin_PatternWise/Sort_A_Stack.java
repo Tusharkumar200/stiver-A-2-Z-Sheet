12983a58135e4bc8b976faefbfbb9639bdb5fe99
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-09 21:18:14 +0530

    Sort_A_Stack

---
+++ Step7_Recursin_PatternWise/Sort_A_Stack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Sort_A_Stack {
+    
+    public static void sortedInsert(Stack<Integer> stack, int key)  
+    { 
+        
+     }
+
+    public Stack<Integer> sort(Stack<Integer> s) {
+        // add code here.
+    }
+    public static void main(String[] args) {
+        
+    }
+}
