15f8301a6d67ef12860f7cf2b528a1c21fe2b2dc
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-10 22:19:14 +0530

    All_Binary_Strings

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_all_binary_strings.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_all_binary_strings.java
@@ -4,14 +4,27 @@ public class Generate_all_binary_strings {
     
     static void All_Binary_Strings(String str, int num)
     {
+        int len = str.length();
 
+        if(len == num){
+            System.out.println(str + " ");
+            return;
+        }
+        else if(str.charAt(len-1)== '1'){
+            All_Binary_Strings(str +" 0", num);
+        }
+        else{
+            All_Binary_Strings(str + '0', num);
+            All_Binary_Strings(str + '1', num);
+        }
     }
 
     static void print(int num)
     {
-        
+
     }
     public static void main(String[] args) {
-        
+        int n = 4;
+        print(n);
     }
 }
