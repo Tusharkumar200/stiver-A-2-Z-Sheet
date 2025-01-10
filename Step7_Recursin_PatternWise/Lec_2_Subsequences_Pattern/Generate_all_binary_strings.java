69f445a6197a09cd0da0ef83fb08d4cc2a6a0195
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-10 22:01:38 +0530

    function created

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_all_binary_strings.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_all_binary_strings.java
@@ -2,6 +2,15 @@ package Lec_2_Subsequences_Pattern;
 
 public class Generate_all_binary_strings {
     
+    static void All_Binary_Strings(String str, int num)
+    {
+
+    }
+
+    static void print(int num)
+    {
+        
+    }
     public static void main(String[] args) {
         
     }
