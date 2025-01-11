5aa051fb9eff49ed3dfec682c454a13967192d6d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-11 10:25:36 +0530

    generateParentheses

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_Parentheses.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_Parentheses.java
@@ -6,15 +6,25 @@ Input: n = 3 Output: ["((()))","(()())","(())()","()(())","()()()"]
 Input: n = 2 Output: [“()()”, “(())”]
  */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Generate_Parentheses {
     public static List<String> generateParentheses(int n) {
             
+        List<String> Ans = new ArrayList<>();
+        char[] currentString = new char[2*n];
+        recurse(n,n,0,currentString,Ans);
+        return Ans;
     }
 
     private static void recurse(int forwardParensNeeded, int backwardsParensNeeded, int currentIndex, 
                             char[] currentString, List<String> validAnswers) {
+                if(forwardParensNeeded == 0 && backwardsParensNeeded == 0){
+                    validAnswers.add(new String(currentString));
+                    return;
+                }
+                
                                 
                     
     }
