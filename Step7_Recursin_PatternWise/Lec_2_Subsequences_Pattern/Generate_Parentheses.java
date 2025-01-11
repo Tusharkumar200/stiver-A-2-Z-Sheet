28cd5de30a7fe9dbc9e7e90b856099de8de9c5a2
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-11 09:54:15 +0530

    problem statement ready

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_Parentheses.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Generate_Parentheses.java
@@ -1,3 +1,11 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Input: n = 3 Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Input: n = 2 Output: [“()()”, “(())”]
+ */
+
 public class Generate_Parentheses {
 
     public static void main(String[] args) {
