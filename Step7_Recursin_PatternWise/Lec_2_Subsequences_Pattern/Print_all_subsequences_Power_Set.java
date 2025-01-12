10188bd32a2e1dc234330e213f0542186795f5e5
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-12 22:00:50 +0530

    subsets()

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Print_all_subsequences_Power_Set.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Print_all_subsequences_Power_Set.java
@@ -1,23 +1,52 @@
 package Lec_2_Subsequences_Pattern;
-public class Print_all_subsequences_Power_Set{
 
-    
-    public static void subsequences(int i , String s, String f){
+import java.util.ArrayList;
+import java.util.List;
 
-        if( i == s.length()){
+public class Print_all_subsequences_Power_Set {
+
+    public static void subsequences(int i, String s, String f) {
+
+        if (i == s.length()) {
 
             System.out.print(f + " ");
             return;
         }
 
-        subsequences(i+1,s,f+s.charAt(i));
+        subsequences(i + 1, s, f + s.charAt(i));
 
-        subsequences(i+1, s, f);
+        subsequences(i + 1, s, f);
     }
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        // Include the current element
+        current.add(nums[index]);
+        generateSubsets(index + 1, nums, current, result);
+
+        // Exclude the current element
+        current.remove(current.size() - 1);
+        generateSubsets(index + 1, nums, current, result);
+
+    }
+
     public static void main(String[] args) {
-    String s = "abc";
-	String f = "";
-	System.out.println("All possible subsequences are: ");
-	subsequences(0, s, f);
+        String s = "abc";
+        String f = "";
+        System.out.println("All possible subsequences are: ");
+        subsequences(0, s, f);
+
+        int[] num = { 1, 2, 3 };
     }
 }
