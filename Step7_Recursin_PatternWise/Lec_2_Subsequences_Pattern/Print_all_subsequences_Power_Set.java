0651d733fd7b0dbdbcff553170092f8aed21dbe9
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-12 18:54:11 +0530

    Print_all_subsequences_Power_Set

---
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Print_all_subsequences_Power_Set.java
@@ -0,0 +1,11 @@
+
+public class Print_all_subsequences_Power_Set{
+
+
+    public static void main(String[] args) {
+    String s = "abc";
+	String f = "";
+	System.out.println("All possible subsequences are: ");
+	subsequences(0, s, f);
+    }
+}
