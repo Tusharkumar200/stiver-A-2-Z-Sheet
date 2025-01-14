4d7d26a9d2fa91c6ec45527143dbed887910b162
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-14 09:57:38 +0530

    function completed

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/PrintSum.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/PrintSum.java
@@ -1,16 +1,44 @@
+package Lec_2_Subsequences_Pattern;
+
 import java.util.List;
+import java.util.ArrayList;
 
 public class PrintSum {
     
-    boolean printS(int ind , List<List<Integer>> ds, int sum , int arr[] , int n){
-        
+    boolean printS(int ind, List<Integer> ds, int s, int sum, int arr[], int n) {
+        if (ind == n) {
+            if (s == sum) {
+                for (Integer i : ds) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+                return true;
+            } else {
+                return false;
+            }
+        }
+        ds.add(arr[ind]);
+        s += arr[ind];
+
+        if (printS(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        ds.remove(ds.size() - 1);
+        s -= arr[ind];
+
+        if (printS(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        return false;
     }
-    
+
     public static void main(String[] args) {
-        int arr[] = {1,2,1};
-        int n = 3;
-        int sum =2;
-        List<List<Integer>> ds;
-        printS(0,ds ,0,sum, arr, n);
+        PrintSum ps = new PrintSum();
+        int[] arr = {1, 2, 1};
+        int sum = 2;
+        List<Integer> ds = new ArrayList<>();
+        ps.printS(0, ds, 0, sum, arr, arr.length);
     }
 }
