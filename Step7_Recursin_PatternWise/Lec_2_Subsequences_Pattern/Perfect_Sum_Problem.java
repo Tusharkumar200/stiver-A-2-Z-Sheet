655b145100ae795b07a8e1f7f87823cb3c697ddb
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-15 17:19:54 +0530

    perfectSum

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Perfect_Sum_Problem.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Perfect_Sum_Problem.java
@@ -1,7 +1,25 @@
 public class Perfect_Sum_Problem {
     
     public int perfectSum(int[] arr, int sum) {
-        
+        int n = arr.length;
+        int[][] dp = new int[n + 1][sum + 1];
+
+        // Initialize the first column to 1, as there's always one way to make sum 0
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (arr[i - 1] <= j) {
+                    dp[i][j] += dp[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][sum];
     }
 
     public static void main(String[] args) {
