5b789bee74b3a62c5ca1fe2df97a67c28911076a
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-15 17:28:22 +0530

    isSubsetPresent

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Check_if_there_exists_a_subsequence_with_sum_K.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Check_if_there_exists_a_subsequence_with_sum_K.java
@@ -2,7 +2,9 @@ package Lec_2_Subsequences_Pattern;
 
 public class Check_if_there_exists_a_subsequence_with_sum_K {
     
-
+    public static boolean isSubsetPresent(int n, int sum,int []arr) {
+        
+    }
     public static void main(String[] args) {
         
     }
