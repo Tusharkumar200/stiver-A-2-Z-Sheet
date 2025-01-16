a1dc2a259f77562014570ef877548e0e8cc1d604
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-16 15:25:18 +0530

    findCombinations()

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Combination_Sum_2.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Combination_Sum_2.java
@@ -1,5 +1,6 @@
 package Lec_2_Subsequences_Pattern;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,19 @@ public class Combination_Sum_2 {
     
     static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
 
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i = ind; i<arr.length; i++){
+            if( i> ind && arr[i] == arr[i-1])continue;
+            if(arr[i] > target) break;
+
+            ds.add(arr[i]);
+            findCombinations(i+1, arr,  target-arr[i], ans, ds);
+            ds.remove(ds.size()-1);
+        }
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
