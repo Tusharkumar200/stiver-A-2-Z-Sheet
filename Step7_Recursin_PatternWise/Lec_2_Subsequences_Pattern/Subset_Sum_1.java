e2bafc4466f7cd521aace8b0da07ada3cf132416
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-17 12:43:06 +0530

    function completed

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_Sum_1.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_Sum_1.java
@@ -6,11 +6,21 @@ import java.util.Collections;
 public class Subset_Sum_1 {
     
     static void subsetSumsHelper(int ind, int sum, ArrayList < Integer > arr, int N, ArrayList < Integer > sumSubset) {
-            
+        if(ind == N){
+            sumSubset.add(sum);
+            return;
+        }
+
+        subsetSumsHelper(ind +1,  sum + arr.get(ind), arr, N, sumSubset);
+
+        subsetSumsHelper(ind +1, sum, arr, N, sumSubset);
     }
 
     static ArrayList < Integer > subsetSums(ArrayList < Integer > arr, int N) {
-
+        ArrayList<Integer> subset = new ArrayList<>();
+        subsetSumsHelper(0, 0, arr, N, subset);
+        Collections.sort(subset);
+        return subset;
     }
 
     public static void main(String[] args) {
