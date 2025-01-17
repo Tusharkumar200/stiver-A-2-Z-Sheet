413275b1afe1850f0b52523b5f4d451f81e63ee6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-17 12:14:53 +0530

    Main()

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_Sum_1.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_Sum_1.java
@@ -1,8 +1,20 @@
 package Lec_2_Subsequences_Pattern;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Subset_Sum_1 {
     
     public static void main(String[] args) {
         
+        ArrayList < Integer > arr = new ArrayList < > ();
+        arr.add(3);
+        arr.add(1);
+        arr.add(2);
+        ArrayList < Integer > ans = subsetSums(arr, arr.size());
+        Collections.sort(ans);
+        System.out.println("The sum of each subset is ");
+        for (int i = 0; i < ans.size(); i++)
+            System.out.print(ans.get(i) + " ");
     }
 }
