2b9c65de4f2a14cdb50a2c249b1d3a6aea6a45eb
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-18 12:29:15 +0530

    error fixed

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_2.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_2.java
@@ -1,45 +1,39 @@
 package Lec_2_Subsequences_Pattern;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset_2 {
 
-    public static void fun(int[] nums, int index, List < Integer > ds, HashSet < String > res) {
-
-        if(index == nums.length){
-            Collection.sort(ds);
-            res.add(ds.toString());
-            return;
+    public static void fun(int[] nums, int index, List<Integer> ds, List<List<Integer>> res) {
+        res.add(new ArrayList<>(ds));
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) continue; // skip duplicates
+            ds.add(nums[i]);
+            fun(nums, i + 1, ds, res);
+            ds.remove(ds.size() - 1);
         }
-        ds.add(nums[index]);
-        fun(nums , index+1, ds,res);
-        ds.remove(ds.size()-1);
-        fun(nums, index+1, ds, res);
-        
     }
 
-    public static List < String > subsetsWithDup(int[] nums) {
-        List<String> ans = new ArrayList<>();
-        HashSet<String> res = new HashSet<>();
-        List<Integer>ds = new ArrayList<>();
-        fun(nums, 0, ds, res);
-        for(String i: res){
-            ans.add(i);
-            
-        }
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); // sort the array to handle duplicates
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        fun(nums, 0, ds, ans);
         return ans;
     }
 
-    static void printAns(List < String > ans) {
-        System.out.println("The unique subsets are ");
-        System.out.println(ans.toString().replace(",", " "));
+    static void printAns(List<List<Integer>> ans) {
+        System.out.println("The unique subsets are:");
+        for (List<Integer> subset : ans) {
+            System.out.println(subset);
+        }
     }
+
     public static void main(String[] args) {
-        int nums[]={1,2,2};
-        List < String > ans = subsetsWithDup(nums);
+        int nums[] = {4, 1, 0};
+        List<List<Integer>> ans = subsetsWithDup(nums);
         printAns(ans);
     }
 }
