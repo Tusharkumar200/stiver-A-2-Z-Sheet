8fba55bd94860228bba8f0037176e78ddbc08002
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-18 11:45:01 +0530

    printAns()

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_2.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Subset_2.java
@@ -1,12 +1,24 @@
+import java.util.HashSet;
+import java.util.List;
+
 public class Subset_2 {
 
     public static void fun(int[] nums, int index, List < Integer > ds, HashSet < String > res) {
 
-    }
-    public static List < String > subsetsWithDup(int[] nums) {
         
+    }
+
+    public static List < String > subsetsWithDup(int[] nums) {
+
+    }
+
+    static void printAns(List < String > ans) {
+        System.out.println("The unique subsets are ");
+        System.out.println(ans.toString().replace(",", " "));
     }
     public static void main(String[] args) {
-        
+        int nums[]={1,2,2};
+        List < String > ans = subsetsWithDup(nums);
+        printAns(ans);
     }
 }
