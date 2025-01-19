afb7a711fcee949abd33d3db1d1e21615f5117ae
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-19 15:06:07 +0530

    function written

--- Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Combination_Sum_3.java
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Combination_Sum_3.java
@@ -5,15 +5,29 @@ import java.util.List;
 
 public class Combination_Sum_3 {
 
-    List<List<Integer>> result = new ArrayList<>();
+    static List<List<Integer>> result = new ArrayList<>();
 
-    void try_combination(List<Integer> combination , int k , int n , int start){
+    static void try_combination(List<Integer> combination , int k , int n , int start){
 
+        if(k == combination.size()){
+
+            if(n == 0){
+                result.add(new ArrayList<Integer>(combination));
+            }
+
+            for(int i = start; i <=9; i++){
+                combination.add(i);
+                try_combination(combination, k,  n-i, i+1);
+                combination.remove(combination.size()-1);
+            }
+        }
     }
     
     public List<List<Integer>> combinationSum3(int k, int n) {
-        
-        
+        List<Integer> combination = new ArrayList<>();
+        try_combination(combination, k, n, 1);
+        return result;
+
     }
     public static void main(String[] args) {
         
