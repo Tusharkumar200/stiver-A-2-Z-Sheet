b94d0c96bcdbad64b6db681e85862dd8f7a02cf3
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-19 14:59:31 +0530

    Combination_Sum_3 function created

---
+++ Step7_Recursin_PatternWise/Lec_2_Subsequences_Pattern/Combination_Sum_3.java
@@ -0,0 +1,21 @@
+package Lec_2_Subsequences_Pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combination_Sum_3 {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    void try_combination(List<Integer> combination , int k , int n , int start){
+
+    }
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
