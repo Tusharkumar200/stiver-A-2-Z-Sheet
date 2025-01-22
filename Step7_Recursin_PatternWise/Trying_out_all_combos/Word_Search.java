51b92393e4b2affa193bd4d06fd6a9774513cd1b
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-22 07:56:08 +0530

    searchNext

--- Step7_Recursin_PatternWise/Trying_out_all_combos/Word_Search.java
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/Word_Search.java
@@ -7,7 +7,23 @@ public class Word_Search {
     private boolean searchNext(char[][] board, String word, int row, int col, 
     int index, int m, int n) {
 
-    }
+        if(index == word.length()){
+            return true;
+        }
+
+        if(row < 0 || col < 0 || row ==m || col == n || board[row][col] != word.charAt(index) || board[row][col] == '!') return false;
+
+        char c = board[row][col];
+        board[row][col] = '!';
+
+        boolean top = searchNext(board, word, row -1, col, index +1, m, n);
+        boolean right = searchNext(board, word, row, col+1, index +1, m, n);
+        boolean bottom = searchNext(board, word, row +1, col, index +1, m, n);
+        boolean left = searchNext(board, word, row , col-1, index +1, m, n);
+        
+        board[row][col] =c;
+        return top || right || bottom || left;
+
     public static void main(String[] args) {
         char[][] board = { { 'A', 'B', 'C', 'E' },
                 { 'S', 'F', 'C', 'S' },
