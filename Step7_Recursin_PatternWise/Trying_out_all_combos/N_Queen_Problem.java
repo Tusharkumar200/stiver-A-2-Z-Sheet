75c0a035902862898530efea341a4d6bcbe321b0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-23 22:23:31 +0530

    function created

--- Step7_Recursin_PatternWise/Trying_out_all_combos/N_Queen_Problem.java
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/N_Queen_Problem.java
@@ -1,6 +1,17 @@
+import java.util.List;
 
 class N_Queen_Problem{
+     public static List < List < String >> solveNQueens(int n) {
 
+     }
+
+     static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+
+     }
+
+     static List < String > construct(char[][] board) {
+        
+     }
     public static void main(String[] args) {
         int N = 4;
         List < List < String >> queen = solveNQueens(N);
