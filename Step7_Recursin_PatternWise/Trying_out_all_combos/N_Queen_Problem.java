ed837a85bf9f637f0abd163b56bf65639c1f05f9
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-23 22:49:37 +0530

    solve() completed & try and tested

--- Step7_Recursin_PatternWise/Trying_out_all_combos/N_Queen_Problem.java
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/N_Queen_Problem.java
@@ -7,7 +7,7 @@ class N_Queen_Problem{
             char[][] board = new char[n][n];
             for(int i=0; i<n; i++){
                 for(int j= 0; j< n; j++){
-                    board[i][j] = ".";
+                    board[i][j] = '.';
                 }
             }
              List < List < String >> res = new ArrayList < List < String >> ();
@@ -22,7 +22,26 @@ class N_Queen_Problem{
 
      static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
 
+        if(col == board.length){
+            res.add(construct(board));
+            return;
+        }
+
+        for(int row=0; row < board.length; row++){
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+
+                board[row][col] = 'Q';
+                leftRow[row]=1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length -1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
+            }
         
+        }
      }
 
      static List < String > construct(char[][] board) {
