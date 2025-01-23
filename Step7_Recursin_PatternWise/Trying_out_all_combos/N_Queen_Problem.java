baf55bbbaab537e690554836959da03b3e0bb616
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-23 21:41:43 +0530

    N_Queen_Problem

---
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/N_Queen_Problem.java
@@ -0,0 +1,18 @@
+
+class N_Queen_Problem{
+
+    public static void main(String[] args) {
+        int N = 4;
+        List < List < String >> queen = solveNQueens(N);
+        int i = 1;
+        for (List < String > it: queen) {
+            System.out.println("Arrangement " + i);
+            for (String s: it) {
+                System.out.println(s);
+            }
+            System.out.println();
+            i += 1;
+        }
+        
+    }
+}
