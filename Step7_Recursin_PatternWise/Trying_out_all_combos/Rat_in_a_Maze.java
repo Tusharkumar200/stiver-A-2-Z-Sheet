ca34bc17e16827799c46edc410a21e78a52572e1
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-24 22:34:40 +0530

    corrent version of code

--- Step7_Recursin_PatternWise/Trying_out_all_combos/Rat_in_a_Maze.java
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/Rat_in_a_Maze.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 class Solution {
-    private static void solve(int i, int j, int a[][], int n, ArrayList<String> ans, String move,int vis[][]) {
+    private static void solve(int i, int j, int a[][], int n, ArrayList<String> ans, String move,
+                              int vis[][], int di[], int dj[]) {
 
         if (i == n - 1 && j == n - 1) {
             ans.add(move);
@@ -16,47 +17,38 @@ class Solution {
 
             if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && vis[nexti][nextj] == 0 && a[nexti][nextj] == 1) {
                 vis[i][j] = 1;
-                solve(nexti, nextj, a, n, ans, move, dir.charAt(ind), vis[i][j] = 0);
-            }
-        }
-    }
-
-    public static ArrayList<String> findPath(int[][] m, int n) {
-        int vis[][] = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+                solve(nexti, nextj, a, n, ans, move + dir.charAt(ind), vis, di, dj);
                 vis[i][j] = 0;
             }
         }
-
-        int di[] = {
-                +1,
-                0,
-                0,
-                -1
-        };
-        int dj[] = {
-                0,
-                -1,
-                1,
-                0
-        };
-        ArrayList<String> ans = new ArrayList<>();
-        if (m[0][0] == 1)
-            solve(0, 0, m, n, ans, "", vis, di, dj);
-        return ans;
     }
-}
 
-public class Rat_in_a_Maze {
-
-    public static void main(String[] args) {
-        int n = 4;
-        int[][] a = { { 1, 0, 0, 0 }, { 1, 1, 0, 1 }, { 1, 1, 0, 0 }, { 0, 1, 1, 1 } };
-
-        Solution obj = new Solution();
-        ArrayList<String> res = obj.findPath(a, n);
+    public ArrayList<String> findPath(int[][] m, int n) {
+            int vis[][] = new int[n][n];
+    
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    vis[i][j] = 0;
+                }
+            }
+    
+            int di[] = { +1, 0, 0, -1 };
+            int dj[] = { 0, -1, 1, 0 };
+            ArrayList<String> ans = new ArrayList<>();
+            if (m[0][0] == 1)
+                solve(0, 0, m, n, ans, "", vis, di, dj);
+            return ans;
+        }
+    }
+    
+    public class Rat_in_a_Maze {
+    
+        public static void main(String[] args) {
+            int n = 4;
+            int[][] a = { { 1, 0, 0, 0 }, { 1, 1, 0, 1 }, { 1, 1, 0, 0 }, { 0, 1, 1, 1 } };
+    
+            Solution obj = new Solution();
+            ArrayList<String> res = obj.findPath(a, n);
         if (res.size() > 0) {
             for (int i = 0; i < res.size(); i++)
                 System.out.print(res.get(i) + " ");
