2b6525df0a321b246eb168a66d692a143b81cd8e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-25 18:04:21 +0530

    isValid()

--- Step7_Recursin_PatternWise/Lec_3_Trying_out_all_combos/Sudoku_Solver.java
+++ Step7_Recursin_PatternWise/Lec_3_Trying_out_all_combos/Sudoku_Solver.java
@@ -2,11 +2,21 @@ public class Sudoku_Solver {
 
     public static boolean isValid(char[][] board, int row, int col, char c) {
 
+        for(int i=0; i <9; i++){
+            if(board[i][col] == c) return false;
+           
+            if(board[row][i] == c) return false;
+
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)return false;
+            
+            return false;
+        }   
+        return true;
     }
 
     
   public static boolean solveSudoku(char[][] board) {
-    
+
   }
     public static void main(String[] args) {
         char[][] board = {
