ff3123641e5701a7013f263a93a9f83a1759386c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-25 17:17:25 +0530

    solve

--- Step7_Recursin_PatternWise/Trying_out_all_combos/M_Coloring_Problem.java
+++ Step7_Recursin_PatternWise/Trying_out_all_combos/M_Coloring_Problem.java
@@ -12,7 +12,17 @@ public class M_Coloring_Problem {
     }
 
     private static boolean solve(int node, List < Integer > [] G, int[] color, int n, int m) {
-        
+
+        if(node == n) return true;
+
+        for(int i=1; i<=m; i++){
+            if(isSafe(node, G, color, n, i)){
+                color[node] = i;
+                if(solve( node +1, G, color, n, m) == true) return true;
+                color[node] = 0;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         int N = 4, M = 3;
