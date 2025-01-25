2e90c372df349c11569ec8810bb2b41dd599ae49
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-25 18:09:50 +0530

    solveSudoku

--- Step7_Recursin_PatternWise/Lec_3_Trying_out_all_combos/Sudoku_Solver.java
+++ Step7_Recursin_PatternWise/Lec_3_Trying_out_all_combos/Sudoku_Solver.java
@@ -16,7 +16,24 @@ public class Sudoku_Solver {
 
     
   public static boolean solveSudoku(char[][] board) {
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
 
+            if(board[i][j] == '.'){
+                for(char c ='1'; c<='9'; c++){
+                    if(isValid(board, i, j, c)){
+                        board[i][j] =c;
+                    }
+                    if(solveSudoku(board)) return true;
+                    else{
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
   }
     public static void main(String[] args) {
         char[][] board = {
