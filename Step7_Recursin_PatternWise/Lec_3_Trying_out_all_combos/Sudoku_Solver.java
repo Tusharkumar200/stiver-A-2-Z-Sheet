b52f7cb0d2302ff31b483e4adae843302a96715c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-25 17:33:43 +0530

    folder name change and solveSudoku

---
+++ Step7_Recursin_PatternWise/Lec_3_Trying_out_all_combos/Sudoku_Solver.java
@@ -0,0 +1,31 @@
+public class Sudoku_Solver {
+
+    public static boolean isValid(char[][] board, int row, int col, char c) {
+
+    }
+
+    
+  public static boolean solveSudoku(char[][] board) {
+    
+  }
+    public static void main(String[] args) {
+        char[][] board = {
+                { '9', '5', '7', '.', '1', '3', '.', '8', '4' },
+                { '4', '8', '3', '.', '5', '7', '1', '.', '6' },
+                { '.', '1', '2', '.', '4', '9', '5', '3', '7' },
+                { '1', '7', '.', '3', '.', '4', '9', '.', '2' },
+                { '5', '.', '4', '9', '7', '.', '3', '6', '.' },
+                { '3', '.', '9', '5', '.', '8', '7', '.', '1' },
+                { '8', '4', '5', '7', '9', '.', '6', '1', '3' },
+                { '.', '9', '1', '.', '3', '6', '.', '7', '5' },
+                { '7', '.', '6', '1', '8', '5', '4', '.', '9' }
+        };
+        solveSudoku(board);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++)
+                System.out.print(board[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
