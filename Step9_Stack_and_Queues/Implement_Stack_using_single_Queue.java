87c369654ad24bb78cc5851f2b5e37bed9e4d6af
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-30 11:04:00 +0530

    Implement_Stack_using_single_Queue

---
+++ Step9_Stack_and_Queues/Implement_Stack_using_single_Queue.java
@@ -0,0 +1,15 @@
+public class Implement_Stack_using_single_Queue {
+    
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(3);
+        s.push(2);
+        s.push(4);
+        s.push(1);
+        System.out.println("Top of the stack: " + s.top());
+        System.out.println("Size of the stack before removing element: " + s.size());
+        System.out.println("The deleted element is: " + s.pop());
+        System.out.println("Top of the stack after removing element: " + s.top());
+        System.out.println("Size of the stack after removing element: " + s.size());
+    }
+}
