3fc8c5dac530d5a4d250189d4f2dee4c4ee35c83
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-30 11:08:48 +0530

    function completed

--- Step9_Stack_and_Queues/Implement_Stack_using_single_Queue.java
+++ Step9_Stack_and_Queues/Implement_Stack_using_single_Queue.java
@@ -1,5 +1,26 @@
-public class Implement_Stack_using_single_Queue {
-    
+import java.util.LinkedList;
+import java.util.Queue;
+
+    Queue<Integer> q = new LinkedList<>();
+    Queue < Integer > q = new LinkedList < > ();
+    void push(int x) {
+        q.add(x);
+        for(int i=0; i< q.Size()-1; i++){
+            q.add(q.remove());
+        }
+    }
+
+    int pop() {
+        return q.remove();
+    }
+
+    int top(){
+       return q.peek();
+    }
+
+    int size(){
+        return q.size();
+    }
     public static void main(String[] args) {
         stack s = new stack();
         s.push(3);
