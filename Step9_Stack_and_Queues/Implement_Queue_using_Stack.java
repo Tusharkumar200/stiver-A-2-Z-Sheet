a6fc53239101e8c82fea217fddd103891d5af33e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-01-31 21:41:42 +0530

    Implement_Queue_using_Stack

---
+++ Step9_Stack_and_Queues/Implement_Queue_using_Stack.java
@@ -0,0 +1,17 @@
+public class Implement_Queue_using_Stack {
+    
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.push(3);
+        q.push(4);
+        System.out.println("The element poped is " + q.pop());
+        q.push(5);
+        System.out.println("The top element is " + q.peek());
+        System.out.println("The size of the queue is " + q.size());
+    }
+}
+
+class MyQueue {
+
+    
+}
