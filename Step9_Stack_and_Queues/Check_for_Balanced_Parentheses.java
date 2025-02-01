1226f593d4793b333eb8fbc20331ec5d580120f6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-01 11:44:23 +0530

    Main function created

--- Step9_Stack_and_Queues/Check_for_Balanced_Parentheses.java
+++ Step9_Stack_and_Queues/Check_for_Balanced_Parentheses.java
@@ -1,7 +1,15 @@
 public class Check_for_Balanced_Parentheses {
     
+    public static boolean isValid(String s) {
+
+    }
 
     public static void main(String[] args){
         
+		String s="()[{}()]";
+		if(isValid(s)==true)
+		System.out.println("True");
+		else
+		System.out.println("False");
     }
 }
