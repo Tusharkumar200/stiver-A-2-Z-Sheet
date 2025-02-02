c72939183dca518f2387c2b525246e64af35beaf
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-02 11:07:20 +0530

    function completed

--- Step9_Stack_and_Queues/Implement_Min_Stack.java
+++ Step9_Stack_and_Queues/Implement_Min_Stack.java
@@ -1,21 +1,42 @@
+import java.util.Stack;
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Implement_Min_Stack {
     
     
-    
-    public void push(int val) {
-        
+     Stack < Pair > st;
+    public Implement_Min_Stack() {
+        st = new Stack < > ();
+    }
+
+    public void push(int x) {
+        int min;
+        if(st.isEmpty()){
+            min = x;
+        }
+        else{
+            min = Math.min(st.peek().y, x);
+        }
+            st.push(new Pair(x, min));
     }
     
     public void pop() {
-        
+        st.pop();
     }
     
     public int top() {
-        
+        return st.peek().x;
     }
     
     public int getMin() {
-        
+        return st.pop().y;
     }
 
     public static void main(String[] args) {
