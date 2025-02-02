03825698b6672a8a6d167cc10c6dc9536d1606ce
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-02 10:07:11 +0530

    Implement_Min_Stack

---
+++ Step9_Stack_and_Queues/Implement_Min_Stack.java
@@ -0,0 +1,26 @@
+public class Implement_Min_Stack {
+    
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
