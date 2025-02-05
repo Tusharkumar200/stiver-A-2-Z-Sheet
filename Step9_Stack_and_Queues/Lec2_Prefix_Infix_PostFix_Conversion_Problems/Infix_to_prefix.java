1465064b1c05a36676a27a5f2881b5139a3d9cdc
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-05 07:32:48 +0530

    prec

--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/Infix_to_prefix.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/Infix_to_prefix.java
@@ -1,8 +1,23 @@
 
 class Infix_to_prefix{
+    
+    static int Prec(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
 
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
     static String infixToPrefix(String exp) {
-        
+
     }
     public static void main(String[] args){
 
