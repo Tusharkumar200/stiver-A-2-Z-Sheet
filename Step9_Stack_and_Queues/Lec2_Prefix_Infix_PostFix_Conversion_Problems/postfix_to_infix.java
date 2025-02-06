fbbf405b0bdc1d0a62e786fb5ab91515e78ea0d8
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-06 08:05:31 +0530

    getInfix() completed

--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_infix.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_infix.java
@@ -1,7 +1,32 @@
+import java.util.Stack;
+
 public class postfix_to_infix {
-    
-    
+
+    static boolean isOperand(char x) {
+        return (x >= 'a' && x <= 'z') ||
+                (x >= 'A' && x <= 'Z');
+    }
+
+    public static String getInfix(String exp) {
+
+        Stack<String> s = new Stack<String>();
+        for (int i = 0; i < exp.length(); i++) {
+            // Push operands
+            if (isOperand(exp.charAt(i))) {
+                s.push(exp.charAt(i) + "");
+            } else {
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+                s.push("(" + op2 + exp.charAt(i) +
+                        op1 + ")");
+            }
+        }
+        return s.peek();
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
