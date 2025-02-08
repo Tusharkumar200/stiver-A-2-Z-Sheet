a413551f2510df5190f1c60225fe4d0505d59b0b
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-08 13:04:53 +0530

    postfix_to_prefix

--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_prefix.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_prefix.java
@@ -1,3 +1,10 @@
 public class postfix_to_prefix {
+    static boolean isOperand(char x) {
+        return (x >= 'a' && x <= 'z') ||
+                (x >= 'A' && x <= 'Z');
+    }
     
+    public static void main(String[] args) {
+        
+    }
 }
--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/pre_To_Post.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/pre_To_Post.java
@@ -1,3 +1,7 @@
 public class pre_To_Post {
     
+    
+    public static void main(String[] args) {
+        
+    }
 }
