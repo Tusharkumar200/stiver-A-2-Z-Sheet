b2ed35b653aef505e33ab4fbdec39a2a322623f8
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-08 13:15:43 +0530

    pre_To_Post

--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/pre_To_Post.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/pre_To_Post.java
@@ -1,6 +1,26 @@
+import java.util.Stack;
+
 public class pre_To_Post {
     
     
+    static String preToPost(String exp) {
+        Stack<String> s = new Stack<String>();
+        for (int i = exp.length() - 1; i >= 0; i--) {
+
+            if (isOperand(exp.charAt(i))) {
+                s.push(exp.charAt(i) + "");
+            } else {
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+                s.push("(" + op1 + exp.charAt(i) + op2 + ")");
+            }
+        }
+        return s.peek();
+    }
+}
+     
     public static void main(String[] args) {
         
     }
