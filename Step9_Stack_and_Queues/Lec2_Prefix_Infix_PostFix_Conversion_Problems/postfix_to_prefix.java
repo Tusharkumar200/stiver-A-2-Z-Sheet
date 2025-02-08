0788c26d2019202b7f8cf23c9bc92f218d8f78af
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-08 13:13:25 +0530

    postfix_to_prefix

--- Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_prefix.java
+++ Step9_Stack_and_Queues/Lec2_Prefix_Infix_PostFix_Conversion_Problems/postfix_to_prefix.java
@@ -1,10 +1,30 @@
+import java.util.Stack;
+
 public class postfix_to_prefix {
     static boolean isOperand(char x) {
         return (x >= 'a' && x <= 'z') ||
                 (x >= 'A' && x <= 'Z');
     }
-    
+
+    static String postToPre(String exp) {
+         Stack<String> s = new Stack<String>();
+        for (int i = 0; i < exp.length(); i++) {
+           
+            if (isOperand(exp.charAt(i))) {
+                s.push(exp.charAt(i) + "");
+            } else {
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+                s.push(exp.charAt(i) +  op2 + op1 );
+            }
+        }
+        return s.peek();
+       
+    }
     public static void main(String[] args) {
-        
+        String exp = "ab-de+f*/";
+        System.out.println(postToPre(exp));
     }
 }
