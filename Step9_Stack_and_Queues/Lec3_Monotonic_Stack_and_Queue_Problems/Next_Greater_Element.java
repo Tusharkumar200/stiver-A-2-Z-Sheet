9ac7958dd34405b6de6e1e0ee1025433b0f5f54c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-11 11:16:49 +0530

    nextGreaterElement

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Next_Greater_Element.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Next_Greater_Element.java
@@ -1,16 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Next_Greater_Element {
     
     
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+          Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+      
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+      
+        int n = nums1.length;
+        int[] result = new int[n];
+      
+        for (int i = 0; i < n; ++i) {
+            // If nums1[i] has a next greater element in nums2, use it; otherwise, use -1
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+        return result;
         
     }
     public static void main(String[] args) {
-        int arr[]={5,7,1,2,6,0};
+        Next_Greater_Element nge = new Next_Greater_Element();
+        int[] nums1 = {5, 7, 1, 2, 6, 0};
+        int[] nums2 = {6, 5, 7, 1, 2, 0};
 
-        int arr2[] = nextGreaterElements(arr);
-        System.out.println("The next greater elements are ");
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
+        int[] result = nge.nextGreaterElement(nums1, nums2);
+        System.out.println("The next greater elements are:");
+        for (int i : result) {
+            System.out.print(i + " ");
         }
     }
 }
