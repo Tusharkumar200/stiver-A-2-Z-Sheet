15b9737055f42a2df40d186a91eef730240798e3
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-12 11:04:48 +0530

    nextGreaterElements

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Next_Greater_Element2.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Next_Greater_Element2.java
@@ -1,8 +1,31 @@
+import java.util.Stack;
 
 class Next_Greater_Element2{
 
-    
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+       int nge[] = new int[n];
+       Stack < Integer > st = new Stack < > ();
+
+       for(int i = 2*n -1 ; i>=0;i-- ){
+
+           while(!st.isEmpty() && st.peek() <= nums[i % n]){
+               st.pop();
+           }
+           if(i<n){
+               nge[i] = st.isEmpty() ? -1 : st.peek();
+           }
+           st.push(nums[i % n]);
+       }
+       return nge;
+   }
     public static void main(String[] args) {
-        
+        Next_Greater_Element2 obj = new Next_Greater_Element2();
+        int[] nums = {1, 2, 1};
+        int[] result = obj.nextGreaterElements(nums);
+        System.out.println("Next Greater Elements: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
