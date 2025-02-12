c2f30be0be84371d0c5f05e52e05379b8836f075
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-12 11:18:28 +0530

    Next_Smaller_Element

---
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Next_Smaller_Element.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Next_Smaller_Element {
+    
+     public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < A.size(); i++) {
+            while (!stack.isEmpty() && stack.peek() >= A.get(i)) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                result.add(-1);
+            } else {
+                result.add(stack.peek());
+            }
+
+            stack.push(A.get(i));
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        NearestSmallerElement nse = new NearestSmallerElement();
+        ArrayList<Integer> A = new ArrayList<>();
+        A.add(4);
+        A.add(5);
+        A.add(2);
+        A.add(10);
+        A.add(8);
+
+        ArrayList<Integer> result = nse.prevSmaller(A);
+        System.out.println(result);
+    }
+}
