a90398306cdbade3718ecd7b703e8abbc57d1352
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-12 11:22:50 +0530

    Number_of_NGEs_to_the_right

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Number_of_NGEs_to_the_right.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Number_of_NGEs_to_the_right.java
@@ -1,7 +1,21 @@
 public class Number_of_NGEs_to_the_right {
     
     public static int[] count_NGEs(int N, int arr[], int queries, int indices[]) {
-        
+        int[] ngeCount = new int[N];
+        int[] result = new int[queries];
+
+        for (int i = 0; i < queries; i++) {
+            int index = indices[i];
+            int count = 0;
+            for (int j = index + 1; j < N; j++) {
+                if (arr[j] > arr[index]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+
+        return result;
     }
     public static void main(String[] args) {
          int[] arr = {3, 4, 2, 7, 5, 8, 10, 6};
