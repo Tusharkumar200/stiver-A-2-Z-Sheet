1144df84b41de365b555de228791a9a38a9e399b
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-14 07:07:54 +0530

    main()

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Trapping_Rainwater.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Trapping_Rainwater.java
@@ -1,7 +1,11 @@
  class Trapping_Rainwater{
 
-    
+     static int trap(int[] height) {
+
+     }
+     
     public static void main(String[] args) {
-        
+        int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println("The duplicate element is " + trap(arr));
     }
  }
