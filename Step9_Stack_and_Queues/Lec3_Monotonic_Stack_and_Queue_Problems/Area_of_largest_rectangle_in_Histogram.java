6971c9f811882e3ddeaa8e6b116ea4fbe475bddd
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-18 20:19:08 +0530

    largestarea

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
@@ -2,7 +2,17 @@ public class Area_of_largest_rectangle_in_Histogram {
     
 
     static int largestarea(int arr[], int n) {
+            int maxArea =0;
 
+            for(int i=0; i<n; i++){
+                int minHeight = Integer.MAX_VALUE;
+                for(int j=i; j< n; j++){
+
+                    minHeight = Math.min(minHeight, arr[j]);
+                    maxArea = Math.max(maxArea, minHeight*(j-i+1));
+                }
+            }
+            return maxArea;
     }
 
     public static void main(String[] args) {
