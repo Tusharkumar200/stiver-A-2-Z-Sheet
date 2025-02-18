0caf2985c38222deaa9db97f7a2be4c1ff988ef5
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-18 20:10:38 +0530

    Area_of_largest_rectangle_in_Histogram

---
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
@@ -0,0 +1,11 @@
+public class Area_of_largest_rectangle_in_Histogram {
+    
+
+    static int largestarea(int arr[], int n) {
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
