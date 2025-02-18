36b2142c80a46d5878706ad8cdea64ccd7f7a315
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-18 20:13:50 +0530

    main() created

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
@@ -4,8 +4,10 @@ public class Area_of_largest_rectangle_in_Histogram {
     static int largestarea(int arr[], int n) {
 
     }
-    
+
     public static void main(String[] args) {
-        
+        int arr[] = {2, 1, 5, 6, 2, 3, 1};
+        int n = 7;
+        System.out.println("The largest area in the histogram is " + largestarea(arr, n));
     }
 }
