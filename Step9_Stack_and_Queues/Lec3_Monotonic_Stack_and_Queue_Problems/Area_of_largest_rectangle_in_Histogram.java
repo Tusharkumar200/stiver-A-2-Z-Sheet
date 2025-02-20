cd72a27356cc074c3897485d9f5b477b104808a0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-20 20:48:18 +0530

    optimal solution

--- Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
+++ Step9_Stack_and_Queues/Lec3_Monotonic_Stack_and_Queue_Problems/Area_of_largest_rectangle_in_Histogram.java
@@ -1,22 +1,27 @@
 public class Area_of_largest_rectangle_in_Histogram {
-    
-// Brute Force
+
+    // Brute Force
     static int largestarea(int arr[], int n) {
-            int maxArea =0;
+        int maxArea = 0;
 
-            for(int i=0; i<n; i++){
-                int minHeight = Integer.MAX_VALUE;
-                for(int j=i; j< n; j++){
+        for (int i = 0; i < n; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
 
-                    minHeight = Math.min(minHeight, arr[j]);
-                    maxArea = Math.max(maxArea, minHeight*(j-i+1));
-                }
+                minHeight = Math.min(minHeight, arr[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
             }
-            return maxArea;
+        }
+        return maxArea;
+    }
+
+    // optimal solution
+    static int optimalLargestArea(int arr[], int n) {
+        return 0;
     }
 
     public static void main(String[] args) {
-        int arr[] = {2, 1, 5, 6, 2, 3, 1};
+        int arr[] = { 2, 1, 5, 6, 2, 3, 1 };
         int n = 7;
         System.out.println("The largest area in the histogram is " + largestarea(arr, n));
     }
