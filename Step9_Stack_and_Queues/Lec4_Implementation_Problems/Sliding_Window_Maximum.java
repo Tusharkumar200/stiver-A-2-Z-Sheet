93f2fb43168de486ce799ad8d78d5b506b68cea1
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-22 12:00:32 +0530

    Main()

--- Step9_Stack_and_Queues/Lec4_Implementation_Problems/Sliding_Window_Maximum.java
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/Sliding_Window_Maximum.java
@@ -1,6 +1,11 @@
 public class Sliding_Window_Maximum {
     
     public static void main(String[] args) {
-        
+        int i, j, n, k = 3, x;
+        int arr[]={4,0,-1,3,5,3,6,8};
+        int ans[] = maxSlidingWindow(arr, k);
+        System.out.println("Maximum element in every " + k + " window ");
+        for (i = 0; i < ans.length; i++)
+            System.out.print(ans[i] + "  ");
     }
 }
