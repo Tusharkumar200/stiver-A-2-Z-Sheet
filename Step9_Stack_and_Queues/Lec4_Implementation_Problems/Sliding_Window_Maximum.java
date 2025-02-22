1e2182af27dbb165aa9b63f5c02f2ce7b8f517d0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-22 12:09:32 +0530

    MaxSlidingWindow BruteForce approach

--- Step9_Stack_and_Queues/Lec4_Implementation_Problems/Sliding_Window_Maximum.java
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/Sliding_Window_Maximum.java
@@ -3,8 +3,28 @@ import java.util.ArrayList;
 public class Sliding_Window_Maximum {
     
     //BruteForce Solution
-     static ArrayList < Integer > MaxSlidingWindow(int arr[], int k) {
+    static void GetMax(int arr[], int l, int r, ArrayList < Integer > maxx) {
+        int i, maxi = Integer.MIN_VALUE;
+        for (i = l; i <= r; i++)
+            maxi = Math.max(maxi, arr[i]);
+        maxx.add(maxi);
+    }
 
+     static ArrayList < Integer > MaxSlidingWindow(int arr[], int k) {
+        int left = 0, right = 0;
+       
+        ArrayList < Integer > maxx = new ArrayList < > ();
+
+        while(right < k-1){
+            right++;
+        }
+
+        while(right < arr.length){
+            GetMax(arr, left, right, maxx);
+            left++;
+            right++;
+        }
+        return maxx;
      }
 
     // optimal solution
@@ -12,7 +32,7 @@ public class Sliding_Window_Maximum {
 
     }
 
-    
+
     public static void main(String[] args) {
         int i, j, n, k = 3, x;
         int arr[]={4,0,-1,3,5,3,6,8};
