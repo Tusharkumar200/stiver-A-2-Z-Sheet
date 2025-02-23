ec04e71065d775853ca908cbcb484c3a86e0f66e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-23 11:09:09 +0530

    Stock_span_problem() completed

--- Step9_Stack_and_Queues/Lec4_Implementation_Problems/Stock_span_problem.java
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/Stock_span_problem.java
@@ -1,5 +1,22 @@
+import java.util.Stack;
+
 public class Stock_span_problem {
     
+    Stack<int[]> s;
+
+    public Stock_span_problem(){
+        s= new Stack<>();
+    }
+
+    public int next(int price){
+        int span =1;
+        while(!s.isEmpty() && price >= s.peek()[0]){
+            span += s.peek()[1];
+            s.pop();
+        }
+        s.push(new int[]{price, span});
+        return span;
+    }
     public static void main(String[] args) {
         
     }
