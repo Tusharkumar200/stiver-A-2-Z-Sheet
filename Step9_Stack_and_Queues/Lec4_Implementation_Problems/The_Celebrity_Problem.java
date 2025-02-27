ed1f2019e87286ebb7539f9e22e8d466e4912357
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-27 22:46:44 +0530

    findCelebrity

--- Step9_Stack_and_Queues/Lec4_Implementation_Problems/The_Celebrity_Problem.java
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/The_Celebrity_Problem.java
@@ -2,8 +2,26 @@
 // the celebrity problem
 public class The_Celebrity_Problem {
     
-    
-    public static void main(String[] args) {
-        
+    private static int findCelebrity(int[][] matrix, int n) {
+
+        return 0;
     }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {0, 1, 0},
+            {0, 0, 0},
+            {0, 1, 0}
+        };
+
+        int n = matrix.length;
+        int celebrity = findCelebrity(matrix, n);
+        
+                if (celebrity == -1) {
+                    System.out.println("No celebrity found.");
+                } else {
+                    System.out.println("Celebrity is person " + celebrity);
+                }
+            }
+        
+           
 }
