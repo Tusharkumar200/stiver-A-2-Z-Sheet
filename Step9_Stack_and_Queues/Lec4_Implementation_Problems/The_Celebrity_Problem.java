f4527bc67fa88d4ddf6675d70c5d10bcafa8d87d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-02-28 23:08:43 +0530

    The_Celebrity_Problem

--- Step9_Stack_and_Queues/Lec4_Implementation_Problems/The_Celebrity_Problem.java
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/The_Celebrity_Problem.java
@@ -3,8 +3,35 @@
 public class The_Celebrity_Problem {
     
     private static int findCelebrity(int[][] matrix, int n) {
-        
-        return 0;
+        int top=0, down = n-1;
+
+        while(top<down){
+
+            if(matrix[top][down] ==1){
+                top = top +1;
+            }
+            else if(matrix[down][top] == 1){
+                down = down -1;
+            }
+            else{
+                top--;
+                down--;
+            }
+        }
+
+        if(top > down){
+            return -1;
+        }
+        // top == down;
+        for(int i=0; i<n-1; i++){
+            if(i == top)continue;
+            if(matrix[top][i] == 0 && matrix[i][top] == 1);
+
+            else{
+                return -1;
+            }
+        }
+        return top;
     }
     public static void main(String[] args) {
         int[][] matrix = {
