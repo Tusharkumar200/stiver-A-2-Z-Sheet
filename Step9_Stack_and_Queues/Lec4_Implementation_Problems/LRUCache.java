388f331aac2d064f499d8f263003c3133a1d01a5
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-01 11:46:30 +0530

    LRUCache

---
+++ Step9_Stack_and_Queues/Lec4_Implementation_Problems/LRUCache.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    Node prev, next;
+    int key, value;
+    Node(int _key, int _value) {
+        key = _key;
+        value = _value;
+    }
+}
+
+public class LRUCache{
+    Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map < Integer, Node > map = new HashMap();
+    int capacity;
+
+    public LRUCache(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+}
