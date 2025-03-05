5cf8ea074ab3754948339101ae19ef85973504b4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-05 12:04:11 +0530

    totalElements

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Fruit_Into_Baskets.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Fruit_Into_Baskets.java
@@ -1,7 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fruit_Into_Baskets {
     
-
-    public static void main(String[] args) {
+    public static int totalElements(Integer[] arr) {
         
+        int left = 0, maxLength = 0;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int right = 0; right < arr.length; right++) {
+            freqMap.put(arr[right], freqMap.getOrDefault(arr[right], 0) + 1);
+
+            while (freqMap.size() > 2) {
+                freqMap.put(arr[left], freqMap.get(arr[left]) - 1);
+                if (freqMap.get(arr[left]) == 0) {
+                    freqMap.remove(arr[left]);
+                }
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 1, 2, 3};
+        System.out.println(totalElements(arr));
     }
 }
