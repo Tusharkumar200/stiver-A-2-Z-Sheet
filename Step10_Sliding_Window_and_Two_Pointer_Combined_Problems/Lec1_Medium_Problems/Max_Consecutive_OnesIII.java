631c17887b211b2fd66990cdf081ac68153cd1b0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-05 07:26:32 +0530

    longestOnes

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Max_Consecutive_OnesIII.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Max_Consecutive_OnesIII.java
@@ -2,7 +2,23 @@
 
 public class Max_Consecutive_OnesIII {
     
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length-1;
+        int l=0,r=0,zero=0,maxlen=0;
 
+        while(r<n){
+            if(nums[n]==0)zero--;
+            if(zero > k){
+                if(nums[l]==0){
+                    zero--;
+                    l++;
+                }
+
+            }
+            r++;
+        }
+        return maxlen;
+    }
     public static void main(String[] args) {
         
     }
