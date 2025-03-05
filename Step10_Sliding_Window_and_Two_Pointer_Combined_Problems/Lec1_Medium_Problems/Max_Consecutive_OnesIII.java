a207479a659a5f7922f71fe91163ba60dad8f62f
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-05 07:35:45 +0530

    longestOnes error solved

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Max_Consecutive_OnesIII.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Max_Consecutive_OnesIII.java
@@ -3,19 +3,19 @@
 public class Max_Consecutive_OnesIII {
     
     public int longestOnes(int[] nums, int k) {
-        int n = nums.length-1;
-        int l=0,r=0,zero=0,maxlen=0;
+        int n = nums.length;
+        int l=0,zero=0,maxlen=0;
 
-        while(r<n){
-            if(nums[n]==0)zero--;
-            if(zero > k){
-                if(nums[l]==0){
+        for(int r=0; r<n; r++){
+            if(nums[r] ==0){zero++;}
+
+            while(zero > k){
+                if(nums[l] == 0){
                     zero--;
-                    l++;
                 }
-
+                l++;
             }
-            r++;
+            maxlen = Math.max(maxlen , r-l+1);
         }
         return maxlen;
     }
