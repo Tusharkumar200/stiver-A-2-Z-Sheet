d3861ee1f91649a74294cfa2077d534c7ce162dc
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-06 07:47:08 +0530

    characterReplacement

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/longest_repeating_character_replacement.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/longest_repeating_character_replacement.java
@@ -1,7 +1,26 @@
+import java.util.HashMap;
+
 public class longest_repeating_character_replacement {
     
     public int characterReplacement(String s, int k) {
-        
+         HashMap<Character, Integer> freqs = new HashMap<>();
+        int res = 0, i = 0, maxFreq = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            freqs.put(c, freqs.getOrDefault(c, 0) + 1);
+            maxFreq = Math.max(maxFreq, freqs.get(c));
+
+            while ((j - i + 1) - maxFreq > k) {
+                char left = s.charAt(i);
+                freqs.put(left, freqs.get(left) - 1);
+                i++;
+            }
+
+            res = Math.max(res, j - i + 1);
+        }
+
+        return res;
     }
     public static void main(String[] args) {
         
