5cac00772504560deb7e1c84de1b53f5c1ada873
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-07 07:21:58 +0530

    numSubarraysWithSum

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Binary_Subarrays_With_Sum.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Binary_Subarrays_With_Sum.java
@@ -1,6 +1,26 @@
 public class Binary_Subarrays_With_Sum {
-    
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+       return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private int atMost(int[] nums, int goal) {
+        int head, tail = 0, sum = 0, result = 0;
+        for (head = 0; head < nums.length; head++) {
+            sum += nums[head];
+            while (sum > goal && tail <= head) {
+                sum -= nums[tail];
+                tail++;
+            }
+            result += head - tail + 1;
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        
+        Binary_Subarrays_With_Sum solution = new Binary_Subarrays_With_Sum();
+        int[] nums = {1, 0, 1, 0, 1};
+        int goal = 2;
+        int result = solution.numSubarraysWithSum(nums, goal);
+        System.out.println("Number of subarrays with sum " + goal + ": " + result);
     }
 }
