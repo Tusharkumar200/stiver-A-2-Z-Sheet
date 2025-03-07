fdfa5600979c30b546dac3944956052a678a027c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-07 07:48:08 +0530

    numberOfSubarrays

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Count_number_of_nice_subarrays.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Count_number_of_nice_subarrays.java
@@ -1,5 +1,25 @@
 public class Count_number_of_nice_subarrays {
 
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+
+        for(int i=0; i<n; i++){
+            nums[i] %=2;
+        }
+        int[] prefixCount = new int[n+1];
+        prefixCount[0] =1;
+        int s =0;
+        int ans =0;
+
+        for(int num:nums){
+            s+=num;
+            if(s >=k){
+                ans += prefixCount[s-k];
+            }
+            prefixCount[s]++;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
