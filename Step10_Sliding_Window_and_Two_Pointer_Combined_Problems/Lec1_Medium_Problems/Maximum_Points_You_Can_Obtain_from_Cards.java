1c814ed6d19c90950d88aac0d74d026d264fa6b5
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-08 11:03:46 +0530

    Maximum_Points_You_Can_Obtain_from_Cards

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Maximum_Points_You_Can_Obtain_from_Cards.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Maximum_Points_You_Can_Obtain_from_Cards.java
@@ -3,12 +3,12 @@ public class Maximum_Points_You_Can_Obtain_from_Cards {
     public int maxScore(int[] nums, int k) {
         int lsum=0 , rsum=0, maxSum=0;
         int n = nums.length;
-        for(int i=0; i< k-1; i++){
+        for(int i=0; i< k; i++){
             lsum = lsum + nums[i];
             maxSum = lsum;
         }
         int rightIndx = n-1;
-        for(int i = k-1 ; i>=0;i--){
+        for(int i = k-1 ; i>=0; i--){
             lsum = lsum - nums[i];
             rsum = rsum + nums[rightIndx];
             rightIndx = rightIndx-1;
