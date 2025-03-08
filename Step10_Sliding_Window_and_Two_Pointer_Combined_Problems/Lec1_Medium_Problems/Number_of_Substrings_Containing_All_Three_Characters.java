e11d7e93236623eccda59bced6553a556eb96b31
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-08 10:56:59 +0530

    Testing done

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Number_of_Substrings_Containing_All_Three_Characters.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec1_Medium_Problems/Number_of_Substrings_Containing_All_Three_Characters.java
@@ -2,7 +2,27 @@
 
 public class Number_of_Substrings_Containing_All_Three_Characters {
     
+    public int numberOfSubstrings(String s) {
+        int lastSeen[] = new int[3];
+        lastSeen[0] = -1;
+        lastSeen[1] = -1;
+        lastSeen[2] = -1;
+        int count =0;
+
+        for(int i=0; i<s.length();i++){
+            lastSeen[s.charAt(i) - 'a'] = i;
+
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+                count = count + ( 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2])));
+            }
+        }
+        return count;
+
+    }
     public static void main(String[] args) {
-        
+        Number_of_Substrings_Containing_All_Three_Characters solution = new Number_of_Substrings_Containing_All_Three_Characters();
+        String s = "abcabc";
+        int result = solution.numberOfSubstrings(s);
+        System.out.println("Number of substrings containing all three characters: " + result);
     }
 }
