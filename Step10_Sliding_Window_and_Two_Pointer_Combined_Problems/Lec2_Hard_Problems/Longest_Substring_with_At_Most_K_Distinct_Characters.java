d2705a5fa17734125ab4decda7ad9921707f977f
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-11 07:43:40 +0530

    longestSubstring

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Longest_Substring_with_At_Most_K_Distinct_Characters.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -1,5 +1,9 @@
 public class Longest_Substring_with_At_Most_K_Distinct_Characters {
     // Longest substring
+
+    public static int  longestSubstring(String s , int k){
+        
+    }
     public static void main(String[] args) {
         
     }
