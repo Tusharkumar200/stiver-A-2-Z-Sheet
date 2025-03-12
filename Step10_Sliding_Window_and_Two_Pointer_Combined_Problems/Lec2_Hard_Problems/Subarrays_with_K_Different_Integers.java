fee0c59c86faa25fd2006465255a7639d5fa7b46
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-12 10:52:48 +0530

    Subarrays_with_K_Different_Integers

---
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Subarrays_with_K_Different_Integers.java
@@ -0,0 +1,9 @@
+public class Subarrays_with_K_Different_Integers {
+    
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
