15cbf78f7e62807960be37802d1d1ae11d8b1d70
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-12 11:51:41 +0530

    main() retun

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Subarrays_with_K_Different_Integers.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Subarrays_with_K_Different_Integers.java
@@ -1,9 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Subarrays_with_K_Different_Integers {
     
     public int subarraysWithKDistinct(int[] nums, int k) {
         
+         int l=0, r=0 ,count=0;
+         Map<Integer, Integer> map = new HashMap<>();
+        
+         while (r < nums.length) {
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+
+            while(map.size() <=k){
+                map.put(nums[l], map.get(nums[l]) - 1);
+
+                if(map.get(nums[l]) == 0){
+                    map.remove(nums[l]);
+                }
+                l = l-1;
+            }
+            count = count + (r-l+1);
+            r= r+1;
+         }
+         return count;
     }
     public static void main(String[] args) {
-        
+        Subarrays_with_K_Different_Integers solution = new Subarrays_with_K_Different_Integers();
+        int[] nums = {1, 2, 1, 2, 3};
+        int k = 2;
+        int result = solution.subarraysWithKDistinct(nums, k);
+        System.out.println("Number of subarrays with " + k + " different integers: " + result);
     }
 }
