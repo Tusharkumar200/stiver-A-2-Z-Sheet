dc437fdbf0c3a3e6d44260ff7ee03750b8355c86
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-15 21:37:22 +0530

    removeOuterParentheses

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Remove_outermost_Paranthesis.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Remove_outermost_Paranthesis.java
@@ -2,6 +2,25 @@ package Step5_String.Lec1_Basic_and_Easy_String_Problems;
 
 public class Remove_outermost_Paranthesis {
 
+    public String removeOuterParentheses(String s) {
+        int cnt=0;
+        StringBuilder  ans = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ')'){
+                cnt--;
+            }
+
+            if(cnt !=0){
+                ans.append(s.charAt(i));
+            }
+
+            if(s.charAt(i) == '('){
+                cnt++;
+            }
+        }
+        return ans.toString();
+    }
     public static void main(String[] args) {
         
     }
