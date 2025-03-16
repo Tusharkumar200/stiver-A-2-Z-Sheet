13310f90d7356e87d3720bec7b661c4e4f9703fd
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-16 10:29:07 +0530

    reverseWords

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Reverse_Words_in_a_String.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Reverse_Words_in_a_String.java
@@ -3,7 +3,12 @@ package Step5_String.Lec1_Basic_and_Easy_String_Problems;
 public class Reverse_Words_in_a_String {
     
     public String reverseWords(String s) {
-        
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]).append(i > 0 ? " " : "");
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
         
