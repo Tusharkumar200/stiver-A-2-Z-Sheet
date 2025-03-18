fa7d1ce5b687305b8c5889e7c9cbe70fa222de60
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-18 23:34:00 +0530

    largeOddNum

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Largest_odd_number_in_a_string.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Largest_odd_number_in_a_string.java
@@ -1,5 +1,11 @@
 public class Largest_odd_number_in_a_string {
-    
+    // function of largest odd number
+
+    public static int largeOddNum(int n){
+
+        return 0;
+    }
+
     public static void main(String[] args){
         
     }
