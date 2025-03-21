23793d8873118474b0a2ecf988eea8f774796104
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-21 21:27:30 +0530

    main()

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Largest_odd_number_in_a_string.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Largest_odd_number_in_a_string.java
@@ -6,7 +6,7 @@ public class Largest_odd_number_in_a_string {
             return num;
         int i = num.length() - 1;
         while (i >= 0) {
-            int n = num.charAt(i);
+            int n = Character.getNumericValue(num.charAt(i));
             if (n % 2 == 1)
                 return num.substring(0, i + 1);
             i--;
@@ -15,6 +15,11 @@ public class Largest_odd_number_in_a_string {
     }
 
     public static void main(String[] args) {
+        Largest_odd_number_in_a_string solution = new Largest_odd_number_in_a_string();
+        String num = "4206";
+        System.out.println("Largest odd number in the string \"" + num + "\": " + solution.largestOddNumber(num));
 
+        num = "35427";
+        System.out.println("Largest odd number in the string \"" + num + "\": " + solution.largestOddNumber(num));
     }
 }
