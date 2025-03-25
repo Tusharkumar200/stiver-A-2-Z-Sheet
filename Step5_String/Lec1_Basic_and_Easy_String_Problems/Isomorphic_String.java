4c979201a6ebbd0922bceb668342c032554f029c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-25 22:42:50 +0530

    isIsomorphic

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Isomorphic_String.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Isomorphic_String.java
@@ -1,7 +1,31 @@
 public class Isomorphic_String {
     
      public boolean isIsomorphic(String s, String t) {
+        int[] indexS = new int[200]; 
+        int[] indexT = new int[200]; 
         
+        
+        int len = s.length();
+        
+       
+        if(len != t.length()) {
+            return false;
+        }
+        
+     
+        for(int i = 0; i < len; i++) {
+            
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
+                return false; 
+            }
+            
+           
+            indexS[s.charAt(i)] = i + 1; 
+            indexT[t.charAt(i)] = i + 1; 
+        }
+        
+        
+        return true;
     }
     
     public static void main(String[] args) {
