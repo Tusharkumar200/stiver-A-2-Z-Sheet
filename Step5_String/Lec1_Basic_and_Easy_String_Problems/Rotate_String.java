1eab5fca8582bec2715a29acad29984c82d5e65c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-28 22:41:32 +0530

    rotateString

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Rotate_String.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Rotate_String.java
@@ -1,9 +1,19 @@
 public class Rotate_String {
     
     public boolean rotateString(String s, String goal) {
-        
+        int m = s.length();
+        int n = goal.length();
+
+        if(m !=n){
+            return false;
+        }
+
+        return (s+s).contains(goal);
     }
     public static void main(String[] args) {
-        
+        Rotate_String rs = new Rotate_String();
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println(rs.rotateString(s, goal)); // Expected output: true
     }
 }
