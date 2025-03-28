2fa9a350e836bf41689f093e529040e8c1907a2e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-28 23:07:38 +0530

    isAnagram

--- Step5_String/Lec1_Basic_and_Easy_String_Problems/Valid_Anagram.java
+++ Step5_String/Lec1_Basic_and_Easy_String_Problems/Valid_Anagram.java
@@ -1,7 +1,32 @@
 public class Valid_Anagram {
     
     public boolean isAnagram(String s, String t) {
-        
+        int m = s.length();
+        int n = t.length();
+
+        if(m!=n){
+            return false;
+        }
+        else{
+            int cnt[] = new int[26];
+
+            for(int i=0; i<m;i++){
+                cnt[s.charAt(i) - 'a']++;
+            }
+
+            for(int i=0; i<n;i++){
+                cnt[t.charAt(i) - 'a']--;
+            }
+            for(int i=0; i<cnt.length;i++){
+                
+                if(cnt[i] !=0){
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+
     }
     public static void main(String[] args) {
         
