d2d7cb444a2506e3a654953d2decaec1d63f7a36
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-30 21:20:47 +0530

    main()

--- Step5_String/Medium_String_Problems/Maximum_Nesting_Depth_of_Paranthesis.java
+++ Step5_String/Medium_String_Problems/Maximum_Nesting_Depth_of_Paranthesis.java
@@ -21,6 +21,9 @@ public class Maximum_Nesting_Depth_of_Paranthesis {
     }
         
     public static void main(String[] args) {
-        
+        Maximum_Nesting_Depth_of_Paranthesis obj = new Maximum_Nesting_Depth_of_Paranthesis();
+        String testString = "(1+(2*3)+((8)/4))+1";
+        int result = obj.maxDepth(testString);
+        System.out.println("The maximum nesting depth of the parentheses is: " + result);
     }
 }
---
+++ Step5_String/Medium_String_Problems/Roman_to_Integer.java
@@ -0,0 +1,7 @@
+
+class Roman_to_Integer{
+
+    public static void main(String[] args) {
+        
+    }
+}
