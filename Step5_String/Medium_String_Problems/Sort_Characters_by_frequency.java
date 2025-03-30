191cc9c86578c040327aabf72f7716ec49167db2
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-30 20:05:08 +0530

    main()

--- Step5_String/Medium_String_Problems/Sort_Characters_by_frequency.java
+++ Step5_String/Medium_String_Problems/Sort_Characters_by_frequency.java
@@ -3,30 +3,34 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Sort_Characters_by_frequency {
-    
+
     public String frequencySort(String s) {
-         Map<Character, Integer> hm = new HashMap<>();
-        
+        Map<Character, Integer> hm = new HashMap<>();
+
         for (char c : s.toCharArray()) {
             hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-        
+
         PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
-            (a, b) -> b.getValue() - a.getValue()
-        );
-        
+                (a, b) -> b.getValue() - a.getValue());
+
         pq.addAll(hm.entrySet());
-        
+
         StringBuilder result = new StringBuilder();
         while (!pq.isEmpty()) {
             Map.Entry<Character, Integer> entry = pq.poll();
             result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
         }
-        
+
         return result.toString();
     }
-    
+
     public static void main(String[] args) {
-        
+        Sort_Characters_by_frequency sorter = new Sort_Characters_by_frequency();
+        String input = "tree";
+        String sorted = sorter.frequencySort(input);
+        System.out.println("Input: " + input);
+        System.out.println("Sorted by frequency: " + sorted);
     }
+
 }
