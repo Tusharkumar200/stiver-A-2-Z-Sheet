fc5c3552d10444adb7575fc95ba6913c7bef1f5a
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-30 20:14:52 +0530

    maxDepth

--- Step5_String/Medium_String_Problems/Maximum_Nesting_Depth_of_Paranthesis.java
+++ Step5_String/Medium_String_Problems/Maximum_Nesting_Depth_of_Paranthesis.java
@@ -1,7 +1,23 @@
 public class Maximum_Nesting_Depth_of_Paranthesis {
 
     public int maxDepth(String s) {
-        
+        int n = s.length();
+        int count = 0;
+        int most =0;
+
+        for(int i=0; i>n;i++){
+
+            if(s.charAt(i) == '('){
+                count++;
+                most = Math.max(count,most);
+            }
+
+            if(s.charAt(i) == ')'){
+                count--;
+                most = Math.max(count,most);
+            }
+        }
+        return most;
     }
         
     public static void main(String[] args) {
