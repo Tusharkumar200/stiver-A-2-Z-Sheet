8f3b8f554f1df8e82a59a6534506cfd456fb029a
Tushar Kumar <tusharnwd2003@gmail.com>
2025-03-31 12:01:47 +0530

    code written and bug fixed

--- Step5_String/Medium_String_Problems/Implement_Atoi.java
+++ Step5_String/Medium_String_Problems/Implement_Atoi.java
@@ -1,7 +1,33 @@
 public class Implement_Atoi {
     
     public int myAtoi(String s) {
-        
+        int n = s.length();
+        int i=0;
+
+        while(i<n && s.charAt(i) == ' '){
+            i++;
+        }
+        int sign = 1;
+
+        if(i<n &&(s.charAt(i) =='-' || s.charAt(i) =='+')){
+            if(s.charAt(i) == '-'){
+                sign =-1;
+            }
+            i++;
+        }
+
+        long res =0;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            res = res *10+s.charAt(i)-'0';
+            if(res*sign > Integer.MAX_VALUE){
+                return  Integer.MAX_VALUE;
+            }
+            if(res*sign < Integer.MIN_VALUE){
+                return  Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int) (res*sign);
     }
     public static void main(String[] args) {
         
