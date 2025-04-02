09b6f60eae7a3a8a760db1021a8c8324f64b76a6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-02 08:49:42 +0530

    Count_Number_of_Substrings

--- Step5_String/Medium_String_Problems/Count_Number_of_Substrings.java
+++ Step5_String/Medium_String_Problems/Count_Number_of_Substrings.java
@@ -1,7 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Count_Number_of_Substrings {
     
      int countSubstr(String s, int k) {
-        
+         Map<Character, Integer> charCountMap = new HashMap<>();
+        int n = s.length();
+        int longestSubstringLength = 0; 
+         int left = 0;
+
+         for(int right=0; right <n; right++)
+         {
+         char currentChar = s.charAt(right);
+         charCountMap.put(currentChar, charCountMap.getOrDefault(currentChar, 0) + 1);
+         
+         while(charCountMap.size() > k){
+            char leftChar = s.charAt(left);
+            charCountMap.put(leftChar, charCountMap.get(leftChar) - 1);
+             if (charCountMap.get(leftChar) == 0) {
+                    charCountMap.remove(leftChar);
+                }
+                left++;
+            }
+            longestSubstringLength = Math.max(longestSubstringLength, right - left + 1);
+         }
+     }
+        return longestSubstringLength;
     }
     public static void main(String[] args) {
         
