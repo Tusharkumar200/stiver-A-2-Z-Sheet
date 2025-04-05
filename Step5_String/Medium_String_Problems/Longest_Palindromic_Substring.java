06afc1b9e131cf6f76a15463d7e2d70389521222
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-05 20:35:54 +0530

    longestPalindrome

--- Step5_String/Medium_String_Problems/Longest_Palindromic_Substring.java
+++ Step5_String/Medium_String_Problems/Longest_Palindromic_Substring.java
@@ -1,5 +1,24 @@
 public class Longest_Palindromic_Substring {
     
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+           return s;
+       }
+
+       int maxLen = 1;
+       String maxStr = s.substring(0, 1);
+
+       for (int i = 0; i < s.length(); i++) {
+           for (int j = i + maxLen; j <= s.length(); j++) {
+               if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                   maxLen = j - i;
+                   maxStr = s.substring(i, j);
+               }
+           }
+       }
+
+       return maxStr;
+   }
     public static void main(String[] args) {
         
     }
