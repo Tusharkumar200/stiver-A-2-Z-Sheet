6386b9b5f6ea893b74195e5e0c0eb6805fa9a126
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-13 23:49:45 +0530

    and operator

--- Step8_Bit_Manipulation/Lec1_Learn_Bit_Manipulation/Introduction_to_Bit_Manipulation.java
+++ Step8_Bit_Manipulation/Lec1_Learn_Bit_Manipulation/Introduction_to_Bit_Manipulation.java
@@ -3,14 +3,17 @@ package Step8_Bit_Manipulation.Lec1_Learn_Bit_Manipulation;
 
 // bitwise operators
 
-// AND
+
 
 public class Introduction_to_Bit_Manipulation {
     
     public static void bitwiseOperator(){
-        
+        int a = 5; 
+        int b = 3;
+
+        System.out.println(a & b);
     }
     public static void main(String[] args) {
-        
+        bitwiseOperator();
     }
 }
