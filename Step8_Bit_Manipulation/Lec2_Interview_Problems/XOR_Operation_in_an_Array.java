eb6dca7685cd1db23b4d2d134210bfef9928d4c2
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-22 10:57:31 +0530

    xorOperation

--- Step8_Bit_Manipulation/Lec2_Interview_Problems/XOR_Operation_in_an_Array.java
+++ Step8_Bit_Manipulation/Lec2_Interview_Problems/XOR_Operation_in_an_Array.java
@@ -1,6 +1,17 @@
 
 class XOR_Operation_in_an_Array{
 
+    public int xorOperation(int n, int start) {
+        
+        int ans=0;
+        int[] arr = new int[n];
+        for(int i=0; i<n;i++){
+             arr[i] = start+2*i;
+            ans ^= arr[i];
+        }
+        return ans;
+    }
+    
     public static void main(String[] args) {
         
     }
