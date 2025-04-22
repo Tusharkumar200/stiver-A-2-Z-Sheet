5661234443c756323154c39b490e857bdcbb7fa6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-22 10:58:59 +0530

    xorOperation

--- Step8_Bit_Manipulation/Lec2_Interview_Problems/XOR_Operation_in_an_Array.java
+++ Step8_Bit_Manipulation/Lec2_Interview_Problems/XOR_Operation_in_an_Array.java
@@ -11,8 +11,12 @@ class XOR_Operation_in_an_Array{
         }
         return ans;
     }
-    
+
     public static void main(String[] args) {
-        
+        XOR_Operation_in_an_Array obj = new XOR_Operation_in_an_Array();
+        int n = 5; 
+        int start = 0; 
+        int result = obj.xorOperation(n, start);
+        System.out.println("The XOR operation result is: " + result);
     }
 }
