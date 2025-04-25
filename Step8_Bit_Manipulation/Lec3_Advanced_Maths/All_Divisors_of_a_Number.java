870cb519c6d71b382d7c9d8e79d58e1e83a87416
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-25 23:42:55 +0530

    print_divisors

--- Step8_Bit_Manipulation/Lec3_Advanced_Maths/All_Divisors_of_a_Number.java
+++ Step8_Bit_Manipulation/Lec3_Advanced_Maths/All_Divisors_of_a_Number.java
@@ -2,7 +2,10 @@ package Step8_Bit_Manipulation.Lec3_Advanced_Maths;
 
 public class All_Divisors_of_a_Number {
     
-    
+    public static void print_divisors(int n) {
+        
+        
+    }
     public static void main(String[] args) {
         
     }
