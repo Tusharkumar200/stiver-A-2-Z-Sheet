2e6c9fbd53f6130c8dbe2ad8280722c2ac06a356
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-26 14:33:45 +0530

    countPrimes

--- Step8_Bit_Manipulation/Lec3_Advanced_Maths/Sieve_of_Eratosthenes.java
+++ Step8_Bit_Manipulation/Lec3_Advanced_Maths/Sieve_of_Eratosthenes.java
@@ -1,9 +1,27 @@
 package Step8_Bit_Manipulation.Lec3_Advanced_Maths;
 
+import java.util.Arrays;
+
 public class Sieve_of_Eratosthenes {
     
     public int countPrimes(int n) {
+         if (n <= 2) return 0;
         
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+                // Use long to avoid overflow for i*i for large values of i.
+                for (long j = (long) i * i; j < n; j += i) {
+                    isPrime[(int) j] = false;
+                }
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
         
