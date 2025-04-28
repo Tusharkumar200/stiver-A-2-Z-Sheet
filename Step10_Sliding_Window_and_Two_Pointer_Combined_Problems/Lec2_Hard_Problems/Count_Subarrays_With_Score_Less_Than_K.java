7bedc93844df640480862b71fc41d85a63a439bb
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-28 22:29:56 +0530

    countSubarrays

--- Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Count_Subarrays_With_Score_Less_Than_K.java
+++ Step10_Sliding_Window_and_Two_Pointer_Combined_Problems/Lec2_Hard_Problems/Count_Subarrays_With_Score_Less_Than_K.java
@@ -1,6 +1,17 @@
 public class Count_Subarrays_With_Score_Less_Than_K {
     
-    
+    public long countSubarrays(int[] nums, long k) {
+        long count = 0, sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left++];
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
