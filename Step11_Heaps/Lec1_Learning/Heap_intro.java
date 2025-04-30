29b0bfe3685e430922c952d307895a3950fe76ff
Tushar Kumar <tusharnwd2003@gmail.com>
2025-04-30 12:07:05 +0530

    heap

--- Step11_Heaps/Lec1_Learning/Heap_intro.java
+++ Step11_Heaps/Lec1_Learning/Heap_intro.java
@@ -1,10 +1,21 @@
+import java.util.PriorityQueue;
 
 class Heap_intro{
 
     // heap data structure
     public static void heap(){
-        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        // adding element
+        minHeap.add(10);
+        minHeap.add(90);
+        minHeap.add(70);
+
+        while (!minHeap.isEmpty()) {
+            System.out.println(minHeap.poll());  
+        }
     }
     public static void main(String[] args) {
+        heap();
     }
 }
