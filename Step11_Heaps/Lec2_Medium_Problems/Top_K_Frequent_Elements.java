29573606abf587df88c3ec7b72c4a068f37d8231
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-01 11:33:32 +0530

    Top_K_Frequent_Elements

--- Step11_Heaps/Lec2_Medium_Problems/Top_K_Frequent_Elements.java
+++ Step11_Heaps/Lec2_Medium_Problems/Top_K_Frequent_Elements.java
@@ -1,7 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class Top_K_Frequent_Elements {
     
     public int[] topKFrequent(int[] nums, int k) {
-        
+         Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums)
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            minHeap.offer(entry);
+            if (minHeap.size() > k)
+                minHeap.poll();
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--)
+            result[i] = minHeap.poll().getKey();
+
+        return result;
     }
     public static void main(String[] args) {
         
