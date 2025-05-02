da902ac57bea1ba20dd898a4fd71e32c55b0d761
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-02 14:47:34 +0530

    Connect_N_Ropes_With_Minimum_Cost

--- Step11_Heaps/Lec3_Hard_Problems/Connect_N_Ropes_With_Minimum_Cost.java
+++ Step11_Heaps/Lec3_Hard_Problems/Connect_N_Ropes_With_Minimum_Cost.java
@@ -1,6 +1,34 @@
+import java.util.PriorityQueue;
 
 class Connect_N_Ropes_With_Minimum_Cost{
 
+     public static long connectRopes(int[] arr)
+    {   int n = arr.length;long totalCost = 0;
+        if(n == 1){
+            return (long) 0;
+        }
+        PriorityQueue<Integer> ropes = new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            ropes.add(arr[i]);
+        }
+
+        while(!ropes.isEmpty())
+        {
+            int minmumCost = 0;
+            int fmin = ropes.poll();
+            int smin = ropes.poll();
+            minmumCost = fmin+smin;
+
+            if(ropes.size()>=1){
+            totalCost += (long)minmumCost;
+            ropes.add(minmumCost);
+            }
+        else{
+            totalCost+= (long)minmumCost;
+        }
+        }
+        return totalCost;
+    }
     public static void main(String args[]{
 
     }
