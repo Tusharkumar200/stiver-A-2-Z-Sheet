cac39e64e004ccf439eddb9052cf85c52db1459e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-05 14:27:56 +0530

    Merge_M_sorted_Lists

--- Step11_Heaps/Lec2_Medium_Problems/Merge_M_sorted_Lists.java
+++ Step11_Heaps/Lec2_Medium_Problems/Merge_M_sorted_Lists.java
@@ -1,6 +1,23 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Merge_M_sorted_Lists {
     
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
 
@@ -27,6 +44,27 @@ public class Merge_M_sorted_Lists {
         return mergeTwoLists(l1, l2);
     }
     public static void main(String[] args) {
-        
+        // Create sample input: an array of k sorted linked lists
+        ListNode[] lists = new ListNode[3];
+
+        // First sorted list: 1 -> 4 -> 5
+        lists[0] = new ListNode(1, new ListNode(4, new ListNode(5)));
+
+        // Second sorted list: 1 -> 3 -> 4
+        lists[1] = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        // Third sorted list: 2 -> 6
+        lists[2] = new ListNode(2, new ListNode(6));
+
+        // Merge all k sorted lists
+        Merge_M_sorted_Lists solution = new Merge_M_sorted_Lists();
+        ListNode mergedList = solution.mergeKLists(lists);
+
+        // Print the merged sorted list
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " -> ");
+            mergedList = mergedList.next;
+        }
+        System.out.println("null");
     }
 }
