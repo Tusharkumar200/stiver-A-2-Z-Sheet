17e4ab8e26930c3d553c58db8b11780b89478ba4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-10 11:32:12 +0530

    Kth_largest_element_in_a_stream_of_running_integers

--- Step11_Heaps/Lec3_Hard_Problems/Kth_largest_element_in_a_stream_of_running_integers.java
+++ Step11_Heaps/Lec3_Hard_Problems/Kth_largest_element_in_a_stream_of_running_integers.java
@@ -1,5 +1,26 @@
+import java.util.PriorityQueue;
+
 public class Kth_largest_element_in_a_stream_of_running_integers {
     
+    PriorityQueue<Integer>pq;
+    int k;
+    public Kth_largest_element_in_a_stream_of_running_integers(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int num:nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        if(pq.size()<k || val > pq.peek()){
+            pq.offer(val);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
     public static void main(String[] args) {
         
     }
