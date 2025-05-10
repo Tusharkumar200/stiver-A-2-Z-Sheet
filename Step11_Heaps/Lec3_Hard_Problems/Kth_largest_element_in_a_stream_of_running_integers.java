585f2a82f14e5f31103d81629ecdf0c56cb7f527
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-10 11:37:25 +0530

    Main()

--- Step11_Heaps/Lec3_Hard_Problems/Kth_largest_element_in_a_stream_of_running_integers.java
+++ Step11_Heaps/Lec3_Hard_Problems/Kth_largest_element_in_a_stream_of_running_integers.java
@@ -22,6 +22,14 @@ public class Kth_largest_element_in_a_stream_of_running_integers {
         return pq.peek();
     }
     public static void main(String[] args) {
-        
+        int k = 3;
+        int[] nums = {4, 5, 8, 2};
+        Kth_largest_element_in_a_stream_of_running_integers kthLargest = new Kth_largest_element_in_a_stream_of_running_integers(k, nums);
+
+        System.out.println(kthLargest.add(3)); // Output: 4
+        System.out.println(kthLargest.add(5)); // Output: 5
+        System.out.println(kthLargest.add(10)); // Output: 5
+        System.out.println(kthLargest.add(9)); // Output: 8
+        System.out.println(kthLargest.add(4)); // Output: 8
     }
 }
