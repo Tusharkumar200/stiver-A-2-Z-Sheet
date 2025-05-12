dd8624cf06712d73fbb6e2fb69522a7b17a68e62
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-12 21:53:07 +0530

    function created

--- Step11_Heaps/Lec3_Hard_Problems/Design_twitter.java
+++ Step11_Heaps/Lec3_Hard_Problems/Design_twitter.java
@@ -1,5 +1,25 @@
 public class Design_twitter {
     
+    public Twitter() {
+        
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        
+    }
+    
     public static void main(String[] args) {
         
     }
