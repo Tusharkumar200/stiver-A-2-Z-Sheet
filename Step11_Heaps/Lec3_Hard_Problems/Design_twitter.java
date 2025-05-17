b952f9d56de451ba22ee98c2905009e9a61cb677
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-17 21:59:01 +0530

    postTweet

--- Step11_Heaps/Lec3_Hard_Problems/Design_twitter.java
+++ Step11_Heaps/Lec3_Hard_Problems/Design_twitter.java
@@ -1,4 +1,10 @@
 // tweet 
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 class Tweet implements Comparable<Tweet>{
     int time;
     int tweetId;
@@ -7,18 +13,45 @@ class Tweet implements Comparable<Tweet>{
         tweetId = id;
 
     }
-    public int CompareTo(Tweet that){
+    public int compareTo(Tweet that){
         return that.time - this.time;
+    }
+}
+//User
+
+public class User{
+    int userId;
+    HashSet<Integer> followers;
+    List<Tweet> tweets;
+
+    User(int userId){
+        this.userId = userId;
+        followers = new HashSet<>();
+        tweets = new LinkedList<>();
+    }
+
+    public void addTweet(Tweet t){
+        tweets.add(0,t);
     }
 }
 
 public class Design_twitter {
+    HashMap<Integer,User> userMap;
+    int TimeCounter;
     public Design_twitter() {
-        
+        userMap = new HashMap<>();
+        TimeCounter =0;
     }
     
     public void postTweet(int userId, int tweetId) {
-        
+        TimeCounter++;
+
+        if(!userMap.containsKey(userId)){
+            userMap.put((userId), new User(userId));
+        }
+
+        User user = userMap.get(userId);
+        user.addTweet(new Tweet(TimeCounter, tweetId));
     }
     
     public List<Integer> getNewsFeed(int userId) {
