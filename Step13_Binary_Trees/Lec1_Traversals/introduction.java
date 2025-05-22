bcb000c7fc2c633f2262deae35455867684457b7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-22 22:48:13 +0530

    Node created

--- Step13_Binary_Trees/Lec1_Traversals/introduction.java
+++ Step13_Binary_Trees/Lec1_Traversals/introduction.java
@@ -1,8 +1,18 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
 
-
+    public Node(int val){
+        this.data = val;
+    }
+}
 class introduction{
 
     public static void main(String[] args) {
-        
+        Node root = new Node(23);
+        root.left = new Node(32);
+        root.right = new Node(50);
+        root.right.left = new Node(45);
     }
 }
