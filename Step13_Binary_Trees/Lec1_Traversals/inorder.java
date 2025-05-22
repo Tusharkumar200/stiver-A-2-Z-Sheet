fb3f4d3fbef667d95258e15a7412cfcf6d5467d4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-22 23:28:22 +0530

    Node created for all traversals

---
+++ Step13_Binary_Trees/Lec1_Traversals/inorder.java
@@ -0,0 +1,27 @@
+package Step13_Binary_Trees.Lec1_Traversals;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class inorder {
+    
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Step13_Binary_Trees/Lec1_Traversals/postorder.java
@@ -0,0 +1,28 @@
+package Step13_Binary_Trees.Lec1_Traversals;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class postorder {
+    
+    public static void main(String[] args) {
+        
+    }
+}
