edcf5a3a203aa8497540d05fad41fad0c4d98693
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-23 22:20:26 +0530

    tree Node creaded

--- Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
@@ -2,6 +2,24 @@ package Step13_Binary_Trees.Lec1_Traversals;
 
 import java.util.List;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Level_Order_Traversal_of_Binary_Tree {
     
      public List<List<Integer>> levelOrder(TreeNode root) {
