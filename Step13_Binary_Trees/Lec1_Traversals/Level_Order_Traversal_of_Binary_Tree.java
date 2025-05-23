169c7747a33904b43782cc72a5b12b0ccd19da0d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-23 22:31:51 +0530

    Level_Order_Traversal_of_Binary_Tree

--- Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
@@ -1,6 +1,8 @@
 package Step13_Binary_Trees.Lec1_Traversals;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class TreeNode {
     int val;
@@ -23,7 +25,28 @@ class TreeNode {
 public class Level_Order_Traversal_of_Binary_Tree {
     
      public List<List<Integer>> levelOrder(TreeNode root) {
-        
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> wraplist = new LinkedList<List<Integer>>();
+
+        if(root == null)return wraplist;
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int levelNum = q.size();
+            List<Integer>sublist = new LinkedList<Integer>();
+            for(int i=0; i<levelNum;i++){
+                if(q.peek().left !=null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right !=null){
+                    q.offer(q.peek().right);
+                }
+                sublist.add(q.poll().val);
+            }
+            wraplist.add(sublist);
+        }
+
+        return wraplist;
     }
     public static void main(String[] args) {
         
