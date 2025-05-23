4ba0cb34d8b72be79b84541aeffa8c771ad9ea82
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-23 22:18:36 +0530

    levelOrder

--- Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec1_Traversals/Level_Order_Traversal_of_Binary_Tree.java
@@ -1,7 +1,12 @@
 package Step13_Binary_Trees.Lec1_Traversals;
 
+import java.util.List;
+
 public class Level_Order_Traversal_of_Binary_Tree {
     
+     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+    }
     public static void main(String[] args) {
         
     }
