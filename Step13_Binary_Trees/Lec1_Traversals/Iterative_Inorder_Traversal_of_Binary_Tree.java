d29b57186f1ad328fd192e8eff3a820c4e7eefaa
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-24 11:37:22 +0530

    Iterative_Inorder_Traversal_of_Binary_Tree

--- Step13_Binary_Trees/Lec1_Traversals/Iterative_Inorder_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec1_Traversals/Iterative_Inorder_Traversal_of_Binary_Tree.java
@@ -1,5 +1,6 @@
 package Step13_Binary_Trees.Lec1_Traversals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
@@ -17,9 +18,37 @@ class TreeNode {
 public class Iterative_Inorder_Traversal_of_Binary_Tree {
     
     public static void inorder(Node root, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, arr);
+        arr.add(root.data);
+        inorder(root.right, arr);
+    }
+
+    public static List<Integer> inOrder(Node root) {
+        List<Integer> arr = new ArrayList<>();
         
+        inorder(root, arr);
+       
+        return arr;
     }
     public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
         
+        List<Integer> result = inOrder(root);
+
+        System.out.print("Inorder Traversal: ");
+        
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
