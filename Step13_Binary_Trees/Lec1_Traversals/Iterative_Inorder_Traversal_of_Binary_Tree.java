1b3bcc537d1fbef0840644796b8145d145487d6a
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-24 10:49:18 +0530

    Iterative_Inorder_Traversal_of_Binary_Tree

--- Step13_Binary_Trees/Lec1_Traversals/Iterative_Inorder_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec1_Traversals/Iterative_Inorder_Traversal_of_Binary_Tree.java
@@ -1,5 +1,17 @@
 package Step13_Binary_Trees.Lec1_Traversals;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Iterative_Inorder_Traversal_of_Binary_Tree {
     
     public static void main(String[] args) {
