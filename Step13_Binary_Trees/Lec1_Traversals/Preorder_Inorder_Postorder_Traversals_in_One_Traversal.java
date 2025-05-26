4d0af10c89a3491a949e483c7da3f21fdf9da879
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-26 12:02:02 +0530

    node created

--- Step13_Binary_Trees/Lec1_Traversals/Preorder_Inorder_Postorder_Traversals_in_One_Traversal.java
+++ Step13_Binary_Trees/Lec1_Traversals/Preorder_Inorder_Postorder_Traversals_in_One_Traversal.java
@@ -1,3 +1,19 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    // Constructor to initialize
+    // the node with a value
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
+
 public class Preorder_Inorder_Postorder_Traversals_in_One_Traversal {
     
     public static void main(String[] args) {
