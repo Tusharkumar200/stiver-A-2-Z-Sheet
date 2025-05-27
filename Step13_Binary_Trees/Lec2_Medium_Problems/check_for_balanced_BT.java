60ed06f88cd87661099f21da45e8d15c63dd7041
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-27 10:45:24 +0530

    check_for_balanced_BT

---
+++ Step13_Binary_Trees/Lec2_Medium_Problems/check_for_balanced_BT.java
@@ -0,0 +1,20 @@
+
+package Step13_Binary_Trees.Lec2_Medium_Problems;
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+public class check_for_balanced_BT{
+
+    public static void main(String[] args) {
+        
+    }
+}
