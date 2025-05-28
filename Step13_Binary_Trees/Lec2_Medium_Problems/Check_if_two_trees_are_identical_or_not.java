4f8682caf2a1129d8875345905982f7b4dab6259
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-28 11:24:02 +0530

    TreeNode

--- Step13_Binary_Trees/Lec2_Medium_Problems/Check_if_two_trees_are_identical_or_not.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Check_if_two_trees_are_identical_or_not.java
@@ -1,5 +1,29 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
 public class Check_if_two_trees_are_identical_or_not {
     
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    }
 
     public static void main(String[] args) {
         
