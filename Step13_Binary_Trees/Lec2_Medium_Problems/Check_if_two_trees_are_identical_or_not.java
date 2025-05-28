79f4daef19b0e1dcd90e1103da97af3c69f0234e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-28 11:22:34 +0530

    Check_if_two_trees_are_identical_or_not

---
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Check_if_two_trees_are_identical_or_not.java
@@ -0,0 +1,7 @@
+public class Check_if_two_trees_are_identical_or_not {
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
--- Step13_Binary_Trees/Lec2_Medium_Problems/Diameter_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Diameter_of_Binary_Tree.java
@@ -14,7 +14,33 @@ class Node {
 
 class Diameter_of_Binary_Tree{
 
+    int diameter = 0;
+
+    int calculateHeight(Node node) {
+
+        if(node == null)return 0;
+
+        int lh = calculateHeight(node.left);
+        int rh = calculateHeight(node.right);
+
+        diameter = Math.max(diameter , lh + rh);
+        return 1 +  Math.max(lh , rh);
+    }
+
+     int diameterOfBinaryTree(Node root) {
+
+        calculateHeight(root);
+        return diameter;
+     }
     public static void main(String[] args) {
-        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Diameter_of_Binary_Tree tree = new Diameter_of_Binary_Tree();
+        int result = tree.diameterOfBinaryTree(root);
+        System.out.println("Diameter of the binary tree is: " + result);
     }
 }
