e9d4e3c3ac8fffdc6a25a61bd602c9ef4d9ff131
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-28 11:38:28 +0530

    Check_if_two_trees_are_identical_or_not

--- Step13_Binary_Trees/Lec2_Medium_Problems/Check_if_two_trees_are_identical_or_not.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Check_if_two_trees_are_identical_or_not.java
@@ -31,11 +31,6 @@ public class Check_if_two_trees_are_identical_or_not {
     }
 
     public static void main(String[] args) {
-        TreeNode tree1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-        TreeNode tree2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-
-        Check_if_two_trees_are_identical_or_not checker = new Check_if_two_trees_are_identical_or_not();
-        boolean result = checker.isSameTree(tree1, tree2);
-        System.out.println("Are the two trees identical? " + result);
+        
     }
 }
