df08dc778790a1496ae66dc58a111e4f65f996d5
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-28 10:56:12 +0530

    NODE created

--- Step13_Binary_Trees/Lec2_Medium_Problems/Diameter_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Diameter_of_Binary_Tree.java
@@ -1,4 +1,17 @@
 
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+   
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
 class Diameter_of_Binary_Tree{
 
     public static void main(String[] args) {
