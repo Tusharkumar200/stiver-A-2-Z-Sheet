39e0611bc870fd63e1a332f79c6fb3aba2947c6d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-29 22:48:23 +0530

    Zig_Zag_Traversal_of_Binary_Tree

--- Step13_Binary_Trees/Lec2_Medium_Problems/Zig_Zag_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Zig_Zag_Traversal_of_Binary_Tree.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class TreeNode {
     int val;
@@ -24,7 +27,68 @@ public class Zig_Zag_Traversal_of_Binary_Tree {
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
+        
+      
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+       
+        Queue<TreeNode> nodesQueue = new LinkedList<>();
+        nodesQueue.add(root);
+
+        
+        boolean leftToRight = true;
+
+        
+        while (!nodesQueue.isEmpty()) {
+            
+            int size = nodesQueue.size();
+
+           
+            List<Integer> row = new ArrayList<>();
+
+            
+            for (int i = 0; i < size; i++) {
+                
+                TreeNode node = nodesQueue.poll();
+
+                
+                int index = leftToRight ? i : (size - 1 - i);
+
+               
+                row.add(index, node.val);
+
+                if (node.left != null) {
+                    nodesQueue.add(node.left);
+                }
+                if (node.right != null) {
+                    nodesQueue.add(node.right);
+                }
+            }
+
+           
+            leftToRight = !leftToRight;
+
+          
+            result.add(row);
+        }
+
+       
+        return result;
     }
+
+    static void printResult(List<List<Integer>> result) {
+        for (List<Integer> row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
     public static void main(String[] args) {
         
