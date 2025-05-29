ece7ed65d93734c42356470e842aa9eaaf93df49
Tushar Kumar <tusharnwd2003@gmail.com>
2025-05-29 22:34:54 +0530

    zigzagLevelOrder()

--- Step13_Binary_Trees/Lec2_Medium_Problems/Zig_Zag_Traversal_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Zig_Zag_Traversal_of_Binary_Tree.java
@@ -1,5 +1,30 @@
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Zig_Zag_Traversal_of_Binary_Tree {
     
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+    }
 
     public static void main(String[] args) {
         
