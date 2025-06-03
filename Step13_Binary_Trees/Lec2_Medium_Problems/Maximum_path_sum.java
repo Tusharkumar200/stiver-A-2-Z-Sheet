5e83d86d2405635dde9d899fad9ddd3234a31eac
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-03 23:26:44 +0530

    node created

--- Step13_Binary_Trees/Lec2_Medium_Problems/Maximum_path_sum.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Maximum_path_sum.java
@@ -1,3 +1,19 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    // Constructor to initialize
+    // the node with a value
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
+
 public class Maximum_path_sum {
     
     public static void main(String[] args) {
