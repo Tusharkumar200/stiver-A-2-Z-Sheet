45c4562c78984d4729c0c5130f0718e6795d32b1
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-06 15:27:04 +0530

    added left and right boundaries of bt

--- Step13_Binary_Trees/Lec2_Medium_Problems/Boundary_Traversal_of_BT.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Boundary_Traversal_of_BT.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 
 class Node {
     int data;
     Node left;
     Node right;
 
-   
     Node(int val) {
         data = val;
         left = null;
@@ -12,9 +13,52 @@ class Node {
     }
 }
 
-class  Boundary_Traversal_of_BT{
+class Boundary_Traversal_of_BT {
+
+    boolean isLeaf(Node root) {
+        return root.left == null && root.right == null;
+    }
+
+    void addLeftBoundary(Node root, List<Integer> res) {
+        Node curr = root.left;
+        while (curr != null) {
+
+            if (!isLeaf(curr)) {
+                res.add(curr.data);
+            }
+
+            if (curr.left != null) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+    }
+
+    void addRightBoundary(Node root, List<Integer> res) {
+        Node curr = root.right;
+        List<Integer> temp = new ArrayList<>();
+        while (curr != null) {
+           
+            if (!isLeaf(curr)) {
+                temp.add(curr.data);
+            }
+            // Move to the right child if it exists,
+            // otherwise move to the left child
+            if (curr.right != null) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+        // Reverse and add the values from
+        // the temporary list to the result
+        for (int i = temp.size() - 1; i >= 0; --i) {
+            res.add(temp.get(i));
+        }
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
