9a1992c85413e6429b14c9c6e52dc8c867e74839
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-06 15:18:19 +0530

    Boundary_Traversal_of_BT

--- Step13_Binary_Trees/Lec2_Medium_Problems/Boundary_Traversal_of_BT.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Boundary_Traversal_of_BT.java
@@ -1,4 +1,17 @@
 
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+   
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
 class  Boundary_Traversal_of_BT{
 
     public static void main(String[] args) {
