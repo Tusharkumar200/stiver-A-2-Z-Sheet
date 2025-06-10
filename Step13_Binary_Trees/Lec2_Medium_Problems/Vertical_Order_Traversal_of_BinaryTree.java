b829a8925c74957c22c9ea254ffb9aff68a2ae33
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-10 15:59:43 +0530

    Node Created

--- Step13_Binary_Trees/Lec2_Medium_Problems/Vertical_Order_Traversal_of_BinaryTree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Vertical_Order_Traversal_of_BinaryTree.java
@@ -1,3 +1,17 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    
+    public Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
 public class Vertical_Order_Traversal_of_BinaryTree {
     
     public static void main(String[] args) {
