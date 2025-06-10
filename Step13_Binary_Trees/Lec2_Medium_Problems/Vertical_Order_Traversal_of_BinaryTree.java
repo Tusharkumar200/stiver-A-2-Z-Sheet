ed5fa1444341e2c637e72dbfe9fb66970aacdde9
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-10 16:10:06 +0530

    printResult

--- Step13_Binary_Trees/Lec2_Medium_Problems/Vertical_Order_Traversal_of_BinaryTree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Vertical_Order_Traversal_of_BinaryTree.java
@@ -1,10 +1,10 @@
+import java.util.List;
 
 class Node {
     int data;
     Node left;
     Node right;
 
-    
     public Node(int val) {
         data = val;
         left = null;
@@ -12,9 +12,36 @@ class Node {
     }
 }
 
+private static void printResult(List<List<Integer>> result) {
+    for (List<Integer> level : result) {
+        for (int node : level) {
+            System.out.print(node + " ");
+        }
+        System.out.println();
+    }
+    System.out.println();
+}
+
 public class Vertical_Order_Traversal_of_BinaryTree {
-    
+
     public static void main(String[] args) {
-        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(10);
+        root.left.left.right = new Node(5);
+        root.left.left.right.right = new Node(6);
+        root.right = new Node(3);
+        root.right.right = new Node(10);
+        root.right.left = new Node(9);
+
+        Vertical_Order_Traversal_of_BinaryTree solution = new Vertical_Order_Traversal_of_BinaryTree();
+
+        // Get the Vertical traversal
+        List<List<Integer>> verticalTraversal = solution.findVertical(root);
+
+        // Print the result
+        System.out.print("Vertical Traversal: ");
+        printResult(verticalTraversal);
     }
 }
