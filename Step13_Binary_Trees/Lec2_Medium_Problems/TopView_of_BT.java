6a6c7e90944c19744ce299350fba41aa50159c66
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-11 16:50:52 +0530

    Node created

--- Step13_Binary_Trees/Lec2_Medium_Problems/TopView_of_BT.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/TopView_of_BT.java
@@ -1,3 +1,16 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    
+    public Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
 public class TopView_of_BT {
     
     public static void main(String[] args) {
