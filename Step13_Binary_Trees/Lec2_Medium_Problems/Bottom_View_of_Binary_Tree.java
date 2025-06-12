fee334801dd070829a08e4a39c09a091d953e86f
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-12 22:43:56 +0530

    main

--- Step13_Binary_Trees/Lec2_Medium_Problems/Bottom_View_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Bottom_View_of_Binary_Tree.java
@@ -9,8 +9,7 @@ class Node {
     int data;
     Node left;
     Node right;
-    
-    
+
     public Node(int val) {
         data = val;
         left = null;
@@ -19,51 +18,66 @@ class Node {
 }
 
 public class Bottom_View_of_Binary_Tree {
-    
-     public List<Integer> bottomView(Node root) {
-       
+
+    public List<Integer> bottomView(Node root) {
+
         List<Integer> ans = new ArrayList<>();
-        
-        
-        if(root == null) {
+
+        if (root == null) {
             return ans;
         }
-       
+
         Map<Integer, Integer> mpp = new TreeMap<>();
         Queue<Pair<Node, Integer>> q = new LinkedList<>();
 
-         q.add(new Pair<>(root, 0));
+        q.add(new Pair<>(root, 0));
 
-         while(!q.isEmpty()){
-             Pair<Node, Integer> pair = q.poll();
+        while (!q.isEmpty()) {
+            Pair<Node, Integer> pair = q.poll();
             Node node = pair.getKey();
             int line = pair.getValue();
 
             mpp.put(line, node.data);
-            
-            // Process left child
-            if(node.left != null) {
-                // Push the left child with a decreased
-                // vertical position into the queue
+
+            if (node.left != null) {
+
                 q.add(new Pair<>(node.left, line - 1));
             }
 
-              if(node.right != null) {
-                // Push the right child with an increased
-                // vertical position into the queue
+            if (node.right != null) {
+
                 q.add(new Pair<>(node.right, line + 1));
             }
         }
-        
-        // Transfer values from the
-        // map to the result list
-        for(Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
+
+        for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
             ans.add(entry.getValue());
         }
-        
+
         return ans;
-         }
+    }
+
     public static void main(String[] args) {
-        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(10);
+        root.left.left.right = new Node(5);
+        root.left.left.right.right = new Node(6);
+        root.right = new Node(3);
+        root.right.right = new Node(10);
+        root.right.left = new Node(9);
+
+        Bottom_View_of_Binary_Tree solution = new Bottom_View_of_Binary_Tree();
+
+        // Get the Bottom View traversal
+        List<Integer> bottomView = solution.bottomView(root);
+
+        // Print the result
+        System.out.println("Bottom View Traversal: ");
+        for (int node : bottomView) {
+            System.out.print(node + " ");
+        }
+
     }
 }
