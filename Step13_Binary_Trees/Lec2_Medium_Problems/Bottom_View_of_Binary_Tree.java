3ac9747b1f1225e0ccb2c4830d6d3a0c89b67d46
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-12 22:28:54 +0530

    node created

--- Step13_Binary_Trees/Lec2_Medium_Problems/Bottom_View_of_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Bottom_View_of_Binary_Tree.java
@@ -1,3 +1,17 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    
+    public Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
 public class Bottom_View_of_Binary_Tree {
     
     public static void main(String[] args) {
