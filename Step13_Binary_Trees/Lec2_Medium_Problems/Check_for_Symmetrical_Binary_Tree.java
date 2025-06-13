f17d1a48b18a66c6df8ec8ac24fc622c42a57e11
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-13 16:27:16 +0530

    Main()

--- Step13_Binary_Trees/Lec2_Medium_Problems/Check_for_Symmetrical_Binary_Tree.java
+++ Step13_Binary_Trees/Lec2_Medium_Problems/Check_for_Symmetrical_Binary_Tree.java
@@ -22,9 +22,9 @@ public class Check_for_Symmetrical_Binary_Tree {
 
      private boolean isSymmetricUtil(TreeNode root1, TreeNode root2) {
 
-        if(root1 == null || root2 == null){
-            return root1 == root2;
-        }
+            if(root1 == null || root2 == null){
+                return root1 == root2;
+            }
 
          return (root1.val == root2.val)
                 && isSymmetricUtil(root1.left, root2.right)
