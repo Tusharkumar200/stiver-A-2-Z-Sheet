7eb825c04466bb99db33f217a0a2ea19a2246fff
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-16 00:06:05 +0530

    Node created

--- Step13_Binary_Trees/Lec3_Hard_Problems/LCA_in_Binary_Tree.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/LCA_in_Binary_Tree.java
@@ -1,3 +1,16 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class LCA_in_Binary_Tree {
     
 
