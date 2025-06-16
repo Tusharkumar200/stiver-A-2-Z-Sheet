d82054afa467a5598cb4c3f9d94332c0462834f7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-16 22:42:46 +0530

    node  created

--- Step13_Binary_Trees/Lec3_Hard_Problems/Maximum_width_of_a_Binary_Tree.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Maximum_width_of_a_Binary_Tree.java
@@ -1,3 +1,15 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Maximum_width_of_a_Binary_Tree {
     
     public static void main(String[] args) {
