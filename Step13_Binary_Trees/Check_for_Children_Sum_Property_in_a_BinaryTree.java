fd6bfe4013feda7adcfd4397f6525aafe61ffaec
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-17 22:10:50 +0530

    Treenode created

--- Step13_Binary_Trees/Check_for_Children_Sum_Property_in_a_BinaryTree.java
+++ Step13_Binary_Trees/Check_for_Children_Sum_Property_in_a_BinaryTree.java
@@ -1,7 +1,23 @@
 package Step13_Binary_Trees;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+
 public class Check_for_Children_Sum_Property_in_a_BinaryTree {
     
+    public void changeTree(TreeNode root) {
+        
+    }
     public static void main(String[] args) {
         
     }
