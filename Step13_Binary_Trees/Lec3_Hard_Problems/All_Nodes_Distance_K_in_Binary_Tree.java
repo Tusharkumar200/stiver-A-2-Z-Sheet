bca6d4f3729e593090b6a830665ed7ba53b96825
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-18 13:07:17 +0530

    distanek()

--- Step13_Binary_Trees/Lec3_Hard_Problems/All_Nodes_Distance_K_in_Binary_Tree.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/All_Nodes_Distance_K_in_Binary_Tree.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class All_Nodes_Distance_K_in_Binary_Tree {
+    
+     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        makeParent(parent, root, null);
+        Queue<TreeNode> q = new LinkedList<>();
+        HashSet<TreeNode> visited = new HashSet<>();
+        int curSize = 0;
+        q.offer(target);
+        visited.add(target);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            if (curSize == k) {
+                break;
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null && !visited.contains(node.left)) {
+                    q.offer(node.left);
+                    visited.add(node.left);
+                }
+                if (node.right != null && !visited.contains(node.right)) {
+                    q.offer(node.right);
+                    visited.add(node.right);
+                }
+                TreeNode par = parent.get(node);
+                if (par != null && !visited.contains(par)) {
+                    visited.add(par);
+                    q.add(par);
+                }
+            }
+            curSize++;
+        }
+        List<Integer> result= new ArrayList<>();
+        while(!q.isEmpty()){
+            result.add(q.poll().val);
+        }
+        return result;
+
+    }
     
     public static void main(String[] args) {
         
