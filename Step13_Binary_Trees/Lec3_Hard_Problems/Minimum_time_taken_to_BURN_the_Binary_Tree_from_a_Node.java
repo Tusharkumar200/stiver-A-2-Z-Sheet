8aa746dde82c74ff18dba1069be55c5e18403e13
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-23 22:24:38 +0530

    TreeNode

--- Step13_Binary_Trees/Lec3_Hard_Problems/Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node.java
@@ -1,6 +1,19 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node {
     
     public static void main(String[] args) {
-        
+        // trees
     }
 }
