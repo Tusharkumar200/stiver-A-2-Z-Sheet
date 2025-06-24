21cc0331307efefc29b7d0541c4533b2159e7dbe
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-24 14:54:18 +0530

    Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node

--- Step13_Binary_Trees/Lec3_Hard_Problems/Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node.java
@@ -1,11 +1,10 @@
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+class Node {
+    int data;
+    Node left, right;
 
-    TreeNode(int x) {
-        val = x;
+    Node(int x) {
+        data = x;
         left = null;
         right = null;
     }
@@ -13,6 +12,48 @@ class TreeNode {
 
 public class Minimum_time_taken_to_BURN_the_Binary_Tree_from_a_Node {
     
+     static int findDepth(Node root) {
+        if (root == null) return 0;
+    
+        int left = findDepth(root.left);
+        int right = findDepth(root.right);
+    
+        return Math.max(left, right) + 1;
+    }
+
+     static int findTarNode(Node root, int target, int[] ans) {
+        if(root== null) return -1;
+
+        if(root.data == target){
+            int depth = findDepth(root) -1;
+            ans[0] = Math.max(ans[0],depth);
+            return 1;
+        }
+
+         int left = findTarNode(root.left, target, ans);
+
+         if(left != -1){
+            int depth = findDepth(root.right);
+            ans[0]=Math.max(ans[0], left + depth);
+            return left + 1;
+         }
+
+         int right = findTarNode(root.right, target, ans);
+
+         if(right != -1){
+            int depth = findDepth(root.left);
+            ans[0] = Math.max(ans[0], right+depth);
+            return right + 1;
+         }
+
+         return -1;
+     }
+
+     static int minTime(Node root, int target) {
+        int[]ans = {0};
+        findTarNode(root, target , ans);
+        return ans[0];
+     }
     public static void main(String[] args) {
         // trees
     }
