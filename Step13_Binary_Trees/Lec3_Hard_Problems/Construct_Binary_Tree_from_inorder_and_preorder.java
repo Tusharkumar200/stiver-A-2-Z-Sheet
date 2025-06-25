b2ce75ab244928741a0b049010999035aa388239
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-25 14:16:29 +0530

    Construct_Binary_Tree_from_inorder_and_preorder

---
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Construct_Binary_Tree_from_inorder_and_preorder.java
@@ -0,0 +1,19 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+public class Construct_Binary_Tree_from_inorder_and_preorder {
+    
+    public static void main(String[] args) {
+        
+    }
+}
