f574034f7cfbf9cd308c35404b605f11e25f02f0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-26 23:20:42 +0530

    codeing....

--- Step13_Binary_Trees/Lec3_Hard_Problems/Serialize_And_Deserialize_BT.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Serialize_And_Deserialize_BT.java
@@ -1,5 +1,60 @@
 public class Serialize_And_Deserialize_BT {
     
+    public String serialize(TreeNode root) {
+        
+         if(root==null) return "null";
+
+        Queue<TreeNode> q=new LinkedList<>();
+        StringBuilder sb=new StringBuilder();
+
+        q.offer(root);
+
+        while(!q.isEmpty()){
+
+            TreeNode node=q.poll();
+
+            if(node==null){
+                sb.append("null,");
+                continue;
+            }
+            sb.append(node.val).append(",");
+            q.offer(node.left);
+            q.offer(node.right);
+        }
+        
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+       
+        if(data.equals("null")) return null;
+
+        String nodes[]=data.split(",");
+        TreeNode root=new TreeNode(Integer.parseInt(nodes[0]));
+
+        Queue<TreeNode> q=new LinkedList<>();
+
+        q.offer(root);
+        int i=1;
+        while(!q.isEmpty() && i<nodes.length){
+
+            TreeNode node=q.poll();
+
+            if(!nodes[i].equals("null")){
+                node.left=new TreeNode(Integer.parseInt(nodes[i]));
+                q.offer(node.left);
+            }
+            i++;
+
+            if(!nodes[i].equals("null")){
+                node.right=new TreeNode(Integer.parseInt(nodes[i]));
+                q.offer(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         
     }
