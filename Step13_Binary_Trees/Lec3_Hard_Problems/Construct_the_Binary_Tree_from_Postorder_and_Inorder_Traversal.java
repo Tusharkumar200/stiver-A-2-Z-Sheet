8e83aab0093a29a07981d48ed4ecda1ae70c7c9b
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-26 13:35:48 +0530

    build()

--- Step13_Binary_Trees/Lec3_Hard_Problems/Construct_the_Binary_Tree_from_Postorder_and_Inorder_Traversal.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Construct_the_Binary_Tree_from_Postorder_and_Inorder_Traversal.java
@@ -2,20 +2,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Construct_the_Binary_Tree_from_Postorder_and_Inorder_Traversal {
-    
-     Map<Integer, Integer> map = new HashMap<>();
+
+    Map<Integer, Integer> map = new HashMap<>();
     int idx;
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        for(int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
         idx = postorder.length - 1;
         return build(postorder, inorder, 0, inorder.length - 1);
     }
 
     private TreeNode build(int[] postorder, int[] inorder, int s, int e) {
-        
+
+        if (s > e)
+            return null;
+        int curr = postorder[idx--];
+        TreeNode root = new TreeNode(curr);
+        int id = map.get(curr);
+        root.right = build(postorder, inorder, id + 1, e);
+        root.left = build(postorder, inorder, s, id - 1);
+        return root;
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
