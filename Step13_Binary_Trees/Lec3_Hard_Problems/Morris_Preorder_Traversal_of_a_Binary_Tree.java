07a6ffdd76197d0de740f2ffa1450e26bab0eb11
Tushar Kumar <tusharnwd2003@gmail.com>
2025-06-28 20:22:13 +0530

    node created

--- Step13_Binary_Trees/Lec3_Hard_Problems/Morris_Preorder_Traversal_of_a_Binary_Tree.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Morris_Preorder_Traversal_of_a_Binary_Tree.java
@@ -1,3 +1,16 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Morris_Preorder_Traversal_of_a_Binary_Tree {
     
     public static void main(String[] args) {
