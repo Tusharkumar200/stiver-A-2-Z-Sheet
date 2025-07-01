4e2b0e3a4284affc7b5566d533031fa80030fc94
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-01 22:42:41 +0530

    Main()

--- Step13_Binary_Trees/Lec3_Hard_Problems/Morris_Preorder_Traversal_of_a_Binary_Tree.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Morris_Preorder_Traversal_of_a_Binary_Tree.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 class TreeNode {
     int val;
@@ -12,8 +13,23 @@ class TreeNode {
 }
 
 public class Morris_Preorder_Traversal_of_a_Binary_Tree {
-    
+
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.right = new TreeNode(6);
+
+        Morris_Preorder_Traversal_of_a_Binary_Tree sol = new Morris_Preorder_Traversal_of_a_Binary_Tree();
+
+        List<Integer> preorder = sol.getPreorder(root);
+
+        System.out.print("Binary Tree Morris Preorder Traveral: ");
+        for (int i = 0; i < preorder.size(); i++) {
+            System.out.print(preorder.get(i) + " ");
+        }
+        System.out.println();
     }
 }
