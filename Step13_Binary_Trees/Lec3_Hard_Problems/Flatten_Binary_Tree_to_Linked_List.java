43357070c05bc0ae796aacc6b8285e55181ed37d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-02 19:22:27 +0530

    Flatten_Binary_Tree_to_Linked_List

--- Step13_Binary_Trees/Lec3_Hard_Problems/Flatten_Binary_Tree_to_Linked_List.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Flatten_Binary_Tree_to_Linked_List.java
@@ -10,11 +10,23 @@ class TreeNode {
         right = null;
     }
 }
-
+// By using Morris Law's
 public class Flatten_Binary_Tree_to_Linked_List {
     
        public void flatten(TreeNode root) {
-        
+            TreeNode curr = root;
+            while(curr != null){
+                if(curr.left !=null){
+                    TreeNode pre = curr.left;
+                    while (pre.right != null) {
+                        pre = pre.right;
+                    }
+                    pre.right = curr.right;
+                    curr.right = curr.left;
+                    curr.left = null;
+                }
+            }
+            curr = curr.right;
        }
     public static void main(String[] args) {
         
