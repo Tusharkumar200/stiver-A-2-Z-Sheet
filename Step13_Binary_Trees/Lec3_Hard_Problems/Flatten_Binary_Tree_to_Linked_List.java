680fa23ea820a0070411ac98a165dbf93c4f3076
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-02 19:14:14 +0530

    node & flatten()

--- Step13_Binary_Trees/Lec3_Hard_Problems/Flatten_Binary_Tree_to_Linked_List.java
+++ Step13_Binary_Trees/Lec3_Hard_Problems/Flatten_Binary_Tree_to_Linked_List.java
@@ -1,5 +1,21 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Flatten_Binary_Tree_to_Linked_List {
     
+       public void flatten(TreeNode root) {
+        
+       }
     public static void main(String[] args) {
         
     }
