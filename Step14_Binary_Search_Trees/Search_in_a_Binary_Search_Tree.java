12cdfe1cae9b323efca75f829693575b0989a943
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-03 15:08:44 +0530

    node created

--- Step14_Binary_Search_Trees/Search_in_a_Binary_Search_Tree.java
+++ Step14_Binary_Search_Trees/Search_in_a_Binary_Search_Tree.java
@@ -1,7 +1,22 @@
 package Step14_Binary_Search_Trees;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Search_in_a_Binary_Search_Tree {
     
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+    }
     public static void main(String[] args) {
         
     }
