2e89a8bf6a1bd6a006be841c790eddb4659b3cb6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-03 22:36:34 +0530

    main()

--- Step14_Binary_Search_Trees/Intro_to_BST/MinVal_of_BST.java
+++ Step14_Binary_Search_Trees/Intro_to_BST/MinVal_of_BST.java
@@ -1,3 +1,4 @@
+package Step14_Binary_Search_Trees.Intro_to_BST;
 
 class TreeNode {
     int val;
@@ -22,6 +23,13 @@ public class MinVal_of_BST {
         return root.val;
      }
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(7);
+
+        int min = minValue(root);
+        System.out.println("Minimum value in BST: " + min);
     }
 }
