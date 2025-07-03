2b9a1e94848da62191c20b710adf81558c802e97
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-03 15:11:18 +0530

    searchBST

--- Step14_Binary_Search_Trees/Search_in_a_Binary_Search_Tree.java
+++ Step14_Binary_Search_Trees/Search_in_a_Binary_Search_Tree.java
@@ -15,7 +15,11 @@ class TreeNode {
 public class Search_in_a_Binary_Search_Tree {
     
     public TreeNode searchBST(TreeNode root, int val) {
-        
+
+        while(root != null && root.val != val){
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
     }
     public static void main(String[] args) {
         
