3c170efa1a21862edf6b6e99484a2c70d093e7c2
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-04 09:04:05 +0530

    Ceil_in_a_Binary_Search_Tree

---
+++ Step14_Binary_Search_Trees/Practice_Problems/Ceil_in_a_Binary_Search_Tree.java
@@ -0,0 +1,20 @@
+package Step14_Binary_Search_Trees.Practice_Problems;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+public class Ceil_in_a_Binary_Search_Tree {
+    
+    public static void main(String[] args) {
+        
+    }
+}
