e0b20618908c346b2cfc27dd6f1f8c113296f796
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-05 11:44:08 +0530

    main()

--- Step14_Binary_Search_Trees/Practice_Problems/Insert_into_a_Binary_Search_Tree.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Insert_into_a_Binary_Search_Tree.java
@@ -45,6 +45,25 @@ public class Insert_into_a_Binary_Search_Tree {
         return root;
      }
     public static void main(String[] args) {
-        
+        Insert_into_a_Binary_Search_Tree bst = new Insert_into_a_Binary_Search_Tree();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root = bst.insertIntoBST(root, 5);
+
+        // Simple inorder traversal to print the BST
+        printInOrder(root);
+
+        }
+
+        public static void printInOrder(TreeNode node) {
+            if (node == null) return;
+            printInOrder(node.left);
+            System.out.print(node.val + " ");
+            printInOrder(node.right);
+        }
     }
 }
--- Step14_Binary_Search_Trees/Practice_Problems/floor_in_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/floor_in_BST.java
@@ -37,6 +37,16 @@ public class floor_in_BST {
     }
 
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(6);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+
+        int key = 5;
+        int floor = floorInBST(root, key);
+        System.out.println("Floor of " + key + " in BST is: " + floor);
     }
 }
