f984038b2740a0a209825e035ada6473d74b95ad
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-05 15:36:51 +0530

    function created

--- Step14_Binary_Search_Trees/Practice_Problems/Delete_Node_in_a_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Delete_Node_in_a_BST.java
@@ -15,6 +15,23 @@ class TreeNode {
 
 public class Delete_Node_in_a_BST {
     
+     public TreeNode deleteNode(TreeNode root, int key) {
+     if(root == null){
+        return null;
+     }   
+
+     if(root.val == key){
+        return helper(root);
+     }
+    }
+
+    TreeNode helper(TreeNode root){
+
+    }
+
+    TreeNode findLastRight(TreeNode root){
+        
+    }
     public static void main(String[] args) {
         
     }
