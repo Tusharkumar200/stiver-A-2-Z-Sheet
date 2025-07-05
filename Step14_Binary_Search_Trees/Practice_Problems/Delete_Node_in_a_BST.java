92565a8bb2e61886765aa0b0383361b3427b35ff
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-05 15:48:25 +0530

    deleteNode

--- Step14_Binary_Search_Trees/Practice_Problems/Delete_Node_in_a_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Delete_Node_in_a_BST.java
@@ -16,13 +16,38 @@ class TreeNode {
 public class Delete_Node_in_a_BST {
     
      public TreeNode deleteNode(TreeNode root, int key) {
-     if(root == null){
+    if(root == null){
         return null;
      }   
 
      if(root.val == key){
         return helper(root);
      }
+
+     TreeNode dummy = root;
+
+     while(root != null){
+
+        if(root.val > key){
+            if(root.left !=null && root.left.val == key){
+                root.left = helper(root.left);
+                break;
+            }
+            else{
+                root = root.left;
+            }
+        }
+        else{
+            if(root.right != null && root.right.val == key){
+                root.right = helper(root.right);
+                break;
+            }
+            else{
+                root = root.right;
+            }
+        }
+     }
+     return dummy;
     }
 
     TreeNode helper(TreeNode root){
