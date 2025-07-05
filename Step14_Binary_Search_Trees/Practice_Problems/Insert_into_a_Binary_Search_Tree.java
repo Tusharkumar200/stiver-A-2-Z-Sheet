a65237946fa58fce2b7003aec83c39c38b91cbf4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-05 11:36:47 +0530

    Insert_into_a_Binary_Search_Tree

---
+++ Step14_Binary_Search_Trees/Practice_Problems/Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,21 @@
+package Step14_Binary_Search_Trees.Practice_Problems;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+
+public class Insert_into_a_Binary_Search_Tree {
+    
+    public static void main(String[] args) {
+        
+    }
+}
