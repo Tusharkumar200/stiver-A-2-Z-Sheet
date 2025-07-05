9dd0d9d1d1de2c1c54d957657a75a5c5c047e5b9
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-05 11:42:10 +0530

    Insert_into_a_Binary_Search_Tree

--- Step14_Binary_Search_Trees/Practice_Problems/Insert_into_a_Binary_Search_Tree.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Insert_into_a_Binary_Search_Tree.java
@@ -16,7 +16,33 @@ class TreeNode {
 public class Insert_into_a_Binary_Search_Tree {
     
      public TreeNode insertIntoBST(TreeNode root, int val) {
-        
+        if(root == null){
+            return new TreeNode(val);
+        }
+
+        TreeNode cur = root;
+
+        while(true){
+            if(cur.val <= val){
+                if(cur.right != null){
+                    cur = cur.right;
+                }
+                else{
+                    cur.right = new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(cur.left != null){
+                    cur = cur.left;
+                }
+                else{
+                    cur.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
      }
     public static void main(String[] args) {
         
