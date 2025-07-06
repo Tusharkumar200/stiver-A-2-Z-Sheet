74619df8a8d1465bbae0a9f3c8e53ae2b8e47ca4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-06 12:07:59 +0530

    bug fixed

--- Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
@@ -1,4 +1,3 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +31,24 @@ public class Find_K_th_smallest_largest_element_in_BST {
         int kSmallest = arr.get(k -1);
         return Arrays.asList(kSmallest, kLargest);
     }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+        inorder(root, arr);
+        return arr.get(k - 1);
+    }
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(8);
+
+        Find_K_th_smallest_largest_element_in_BST finder = new Find_K_th_smallest_largest_element_in_BST();
+        int k = 3;
+        List<Integer> result = finder.findKth(root, k);
+        System.out.println("Kth Smallest: " + result.get(0));
+        System.out.println("Kth Largest: " + result.get(1));
     }
 }
