e469bcdcf957fb6587e7897face2c6df31114ff7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-06 11:52:35 +0530

    inorder() & findKth()

--- Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
@@ -1,3 +1,7 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class TreeNode {
     int val;
@@ -13,6 +17,21 @@ class TreeNode {
 
 public class Find_K_th_smallest_largest_element_in_BST {
     
+     private void inorder(TreeNode node, List<Integer> arr) {
+        if(node == null)return;
+
+        inorder(node.left, arr);
+        arr.add(node.val);
+        inorder(node.right, arr);
+        return;
+     }
+    public List<Integer> findKth(TreeNode node, int k) {
+        List<Integer> arr = new ArrayList<>();
+        inorder(node, arr);
+        int kLargest = arr.get(arr.size() - k);
+        int kSmallest = arr.get(k -1);
+        return Arrays.asList(kSmallest, kLargest);
+    }
     public static void main(String[] args) {
         
     }
