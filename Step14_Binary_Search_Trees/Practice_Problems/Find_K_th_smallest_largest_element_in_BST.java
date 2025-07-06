a7ff7a502f4ad438a3a41e2c28a71a175168ed61
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-06 11:39:00 +0530

    node created

--- Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Find_K_th_smallest_largest_element_in_BST.java
@@ -1,3 +1,16 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class Find_K_th_smallest_largest_element_in_BST {
     
     public static void main(String[] args) {
