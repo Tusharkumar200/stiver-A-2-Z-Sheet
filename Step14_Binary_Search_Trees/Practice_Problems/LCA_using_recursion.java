efd26bf74a1d57d6cb59f70fa7e456882f663e62
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-08 22:54:18 +0530

    lowestCommonAncestor

--- Step14_Binary_Search_Trees/Practice_Problems/LCA_using_recursion.java
+++ Step14_Binary_Search_Trees/Practice_Problems/LCA_using_recursion.java
@@ -1,5 +1,35 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
 public class LCA_using_recursion {
-    
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+        if (root == p || root == q)
+            return root;
+
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         
     }
