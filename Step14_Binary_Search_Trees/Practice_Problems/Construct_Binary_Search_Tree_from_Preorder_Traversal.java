f77d763e0745a5fa659d25516b88bfa02ae6c37e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-09 07:03:21 +0530

    Construct_Binary_Search_Tree_from_Preorder_Traversal

---
+++ Step14_Binary_Search_Trees/Practice_Problems/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
@@ -0,0 +1,6 @@
+public class Construct_Binary_Search_Tree_from_Preorder_Traversal {
+    
+    public static void main(String[] args) {
+        
+    }
+}
--- Step14_Binary_Search_Trees/Practice_Problems/LCA_using_recursion.java
+++ Step14_Binary_Search_Trees/Practice_Problems/LCA_using_recursion.java
@@ -31,7 +31,23 @@ public class LCA_using_recursion {
     }
 
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+        TreeNode p = root.left; // Node with value 2
+        TreeNode q = root.left.right; // Node with value 4
+
+        LCA_using_recursion lcaFinder = new LCA_using_recursion();
+        TreeNode lca = lcaFinder.lowestCommonAncestor(root, p, q);
+
+        System.out.println("Lowest Common Ancestor: " + (lca != null ? lca.val : "null"));
     }
 
 }
