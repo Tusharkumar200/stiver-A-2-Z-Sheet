df3a3306226c9ea6c23c9804f284d084d4b471b6
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-09 07:32:48 +0530

    inorderSuccessor

--- Step14_Binary_Search_Trees/Practice_Problems/Inorder_Successor_in_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Inorder_Successor_in_BST.java
@@ -1,7 +1,33 @@
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
 
 class Inorder_Successor_in_BST{
 
+    TreeNode inorderSuccessor(TreeNode root , TreeNode p){
+        TreeNode successor = null;
+
+        while(root != null){
+            if(p.val >= root.val){
+
+                root = root.right;
+            }
+            else{
+                successor = root;
+                root = root.left;
+            }
+        }
+        return successor;
+    }
     public static void main(String[] args) {
         
     }
