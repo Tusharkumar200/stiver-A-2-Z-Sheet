8f09ee708cb603ca854542bd190d812825714178
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-11 00:07:25 +0530

    error solve

--- Step14_Binary_Search_Trees/Practice_Problems/BST_iterator.java
+++ Step14_Binary_Search_Trees/Practice_Problems/BST_iterator.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 class TreeNode {
     int val;
@@ -10,9 +11,31 @@ class TreeNode {
         right = null;
     }
 }
+
 public class BST_iterator {
-    
+
+    private Stack<TreeNode> st = new Stack<>();
+
+    public BST_iterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    public int next() {
+        TreeNode tn = st.pop();
+        pushAll(tn.right);
+        return tn.val;
+    }
+
+    void pushAll(TreeNode node) {
+        for (; node != null; st.push(node), node = node.left)
+            ;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
