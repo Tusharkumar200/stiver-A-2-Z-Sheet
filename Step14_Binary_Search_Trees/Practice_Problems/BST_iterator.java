71045f8c6770f962ac0aa3890d733094d3ae6466
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-10 23:25:51 +0530

    TreeNode

--- Step14_Binary_Search_Trees/Practice_Problems/BST_iterator.java
+++ Step14_Binary_Search_Trees/Practice_Problems/BST_iterator.java
@@ -1,3 +1,15 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
 public class BST_iterator {
     
     public static void main(String[] args) {
