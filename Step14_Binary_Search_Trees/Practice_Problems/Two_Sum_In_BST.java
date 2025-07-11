2089fd3de957876b2de7dc9ac4a6d30d750ee2a0
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-11 23:39:27 +0530

    BSTIterator

--- Step14_Binary_Search_Trees/Practice_Problems/Two_Sum_In_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Two_Sum_In_BST.java
@@ -1,5 +1,46 @@
+
+
+class BSTIterator{
+    Stack <TreeNode> st = new Stack<>();
+    boolean reverse = true;
+    BSTIterator(TreeNode root , boolean isReverse){
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    public int next() {
+        TreeNode tn = st.pop();
+        if( reverse == false){
+
+            pushAll(tn.right);  
+        }
+        else{
+            pushAll(tn.left);
+        }
+        return tn.val;
+    }
+
+    void pushAll(TreeNode node){
+        while(node !=null){
+            st.push(node);
+            if(reverse == true){
+                node = node.right;
+            }
+            else{
+                node = node.left;
+            }
+        }
+    }
+ }
+
 public class Two_Sum_In_BST {
     
+
     public static void main(String[] args) {
         
     }
