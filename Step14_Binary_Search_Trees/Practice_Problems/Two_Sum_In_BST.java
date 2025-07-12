84927cbbb4bb2fa4c77184ff695d8d81f3a5dbc7
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-12 21:34:04 +0530

    main()

--- Step14_Binary_Search_Trees/Practice_Problems/Two_Sum_In_BST.java
+++ Step14_Binary_Search_Trees/Practice_Problems/Two_Sum_In_BST.java
@@ -1,4 +1,4 @@
-
+import java.util.Stack;
 
 class BSTIterator{
     Stack <TreeNode> st = new Stack<>();
@@ -40,8 +40,42 @@ class BSTIterator{
 
 public class Two_Sum_In_BST {
     
+     public boolean findTarget(TreeNode root, int k) {
+        if(root == null){
+            return false;
+        }
 
+        BSTIterator l = new BSTIterator(root , false);
+        BSTIterator r = new BSTIterator(root , true);
+
+        int i = l.next(); 
+        int j = r.next();
+
+        while( i < j){
+
+            if( i + j == k)return true;
+            else if( i + j < k ){
+                i = l.next();
+            }
+            else{
+                j = r.next();
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+
+        Two_Sum_In_BST solver = new Two_Sum_In_BST();
+        int target = 9;
+        System.out.println(solver.findTarget(root, target)); // Output: true
+
+        target = 28;
+        System.out.println(solver.findTarget(root, target)); // Output: false
     }
 }
