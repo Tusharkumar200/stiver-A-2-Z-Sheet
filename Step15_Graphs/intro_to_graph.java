a67b7d31c79511b979936b15dce275d6f51a3440
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-16 07:59:21 +0530

    using list

--- Step15_Graphs/intro_to_graph.java
+++ Step15_Graphs/intro_to_graph.java
@@ -1,8 +1,11 @@
 package Step15_Graphs;
 
+import java.util.ArrayList;
+
 public class intro_to_graph {
     
     public static void main(String[] args) {
+        // using matrix
         int n =3, m =2;
         int adj[][] = new int[n+1][n+1];
 
@@ -19,7 +22,35 @@ public class intro_to_graph {
         adj[1][3] = 1;
         adj[3][1] = 1;
 
-       
+        
+       // using List
+        //    n = 3 , m=3;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+
+        // n+1;
+        for(int i=0; i<=n;i++){
+            adj.add(new ArrayList<Integer>());
+
+            // edge 1--2
+            adj.get(1).add((2));
+            adj.get(2).add((1));
+
+            // edge 2--3
+            adj.get(2).add((3));
+            adj.get(3).add((2));
+
+            for(int i=1; i<n; i++){
+
+                for(int j=0; j< adj.get(i).size(); j++){
+                    System.out.println(adj.get(i).get(j)+" ");
+                }
+                System.out.println();
+            }
+
+
+        }
+        
 
     }
 }
