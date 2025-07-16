8466fc8186c2e96c4e20cfa2d5ba08983ee7280c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-16 07:32:18 +0530

    graph representation

--- Step15_Graphs/intro_to_graph.java
+++ Step15_Graphs/intro_to_graph.java
@@ -3,6 +3,23 @@ package Step15_Graphs;
 public class intro_to_graph {
     
     public static void main(String[] args) {
-        
+        int n =3, m =2;
+        int adj[][] = new int[n+1][n+1];
+
+        // edge 1--2
+        adj[1][2] = 1;
+        adj[2][1] = 1;
+
+        // edge 2--3
+
+        adj[2][3] = 1;
+        adj[3][2] = 1;
+
+        // adge 1--3
+        adj[1][3] = 1;
+        adj[3][1] = 1;
+
+       
+
     }
 }
