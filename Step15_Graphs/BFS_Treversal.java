c6d9d8b214115da0d0daa8e450b6a00c4ec6d307
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-18 16:00:01 +0530

    Main()

--- Step15_Graphs/BFS_Treversal.java
+++ Step15_Graphs/BFS_Treversal.java
@@ -27,6 +27,14 @@ public class BFS_Treversal {
     }
 
     public static void main(String[] args) {
-
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int V = 5;
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(1).add(4);
+        BFS_Treversal obj = new BFS_Treversal();
+        System.out.println(obj.bfsOfGraph(V, adj));
     }
 }
