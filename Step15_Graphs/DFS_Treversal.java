94f82ee1eb1ade45bf22741d74e581fcd9a282da
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-19 15:02:14 +0530

    dfsOfGraph

--- Step15_Graphs/DFS_Treversal.java
+++ Step15_Graphs/DFS_Treversal.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class DFS_Treversal {
-    
+
+
+     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V + 1];
+        vis[0] = true;
+        ArrayList<Integer> ls = new ArrayList<>();
+        dfs(V, vis, adj, ls);
+        return ls;
+     }
+    public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj,
+            ArrayList<Integer> ls) {
+                
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
