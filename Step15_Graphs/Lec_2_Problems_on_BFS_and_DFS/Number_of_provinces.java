a662728a0a6f4c9e3acfdee8754ff29d10f9bdd4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-21 23:23:23 +0530

    findCircleNum

--- Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
+++ Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
@@ -2,6 +2,9 @@ package Lec_2_Problems_on_BFS_and_DFS;
 
 public class Number_of_provinces {
     
+     public int findCircleNum(int[][] isConnected) {
+        
+    }
     public static void main(String[] args) {
         
     }
