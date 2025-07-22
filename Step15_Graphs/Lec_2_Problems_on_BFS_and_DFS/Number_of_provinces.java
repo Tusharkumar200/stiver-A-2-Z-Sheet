231b544fbe42621f205f1e83f4a5183ee6ab94cc
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-22 07:09:59 +0530

    dfs

--- Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
+++ Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
@@ -1,7 +1,20 @@
 package Lec_2_Problems_on_BFS_and_DFS;
 
+import java.util.ArrayList;
+
 public class Number_of_provinces {
     
+    private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs , int vis[]){
+        
+        vis[node] = 1;
+
+        for(Integer it: adjLs.get(node)){
+            if(vis[it] == 0){
+                dfs(node, adjLs, vis);
+            }
+        }
+    }
+
      public int findCircleNum(int[][] isConnected) {
         
     }
