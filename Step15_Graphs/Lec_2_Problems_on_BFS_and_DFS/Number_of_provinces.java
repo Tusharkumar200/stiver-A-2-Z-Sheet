8f4ea44a5f76d97540b480f5be9e2329035c1426
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-22 07:15:31 +0530

    findCircleNum()

--- Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
+++ Step15_Graphs/Lec_2_Problems_on_BFS_and_DFS/Number_of_provinces.java
@@ -16,7 +16,24 @@ public class Number_of_provinces {
     }
 
      public int findCircleNum(int[][] isConnected) {
-        
+        int n = isConnected.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            if (isConnected[i][j] == 1 && i != j)
+                adj.get(i).add(j);
+                
+        int[] vis = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+            dfs(i, adj, vis);
+            count++;
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
         
