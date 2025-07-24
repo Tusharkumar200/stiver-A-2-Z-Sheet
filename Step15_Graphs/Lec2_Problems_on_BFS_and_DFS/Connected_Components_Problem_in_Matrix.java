e859c246bb36835c04e4cd53efb8313da9564c97
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-24 23:39:10 +0530

    Connected_Components_Problem_in_Matrix

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
@@ -2,6 +2,9 @@ package Lec2_Problems_on_BFS_and_DFS;
 
 public class Connected_Components_Problem_in_Matrix {
     
+    public static void connectedComponents(int n){
+        
+    }
     public static void main(String[] args) {
         
     }
