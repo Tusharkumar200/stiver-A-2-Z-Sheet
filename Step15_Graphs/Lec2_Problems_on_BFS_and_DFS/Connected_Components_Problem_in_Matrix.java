7e398b62d20b9057297fb6dccfecad7ed5cb830c
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-26 23:10:33 +0530

    numIslands

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
@@ -11,8 +11,24 @@ class Pair{
 }
 public class Connected_Components_Problem_in_Matrix {
     
-    public static void connectedComponents(int n){
+    public static int numIslands(char[][] grid){
         
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][]vis = new int[n][m];
+        int cnt =0;
+
+        for(int row=0; row <n; row++){
+
+            for(int col =0 ; col<m; col++){
+
+                if(vis[row][col] == 0 & grid[row][col] =='1'){
+                    cnt++;
+                    bfs(row,col,vis,grid);
+                }
+            }
+        }
+        return cnt;
     }
     public static void main(String[] args) {
         
