07fae58bfff2ccbd67b907e232b8736aaae5908d
Tushar Kumar <tusharnwd2003@gmail.com>
2025-07-26 22:46:29 +0530

    Pair

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Connected_Components_Problem_in_Matrix.java
@@ -1,5 +1,14 @@
 package Lec2_Problems_on_BFS_and_DFS;
 
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first , int second){
+        this.first = first;
+        this.second = second;
+    }
+}
 public class Connected_Components_Problem_in_Matrix {
     
     public static void connectedComponents(int n){
