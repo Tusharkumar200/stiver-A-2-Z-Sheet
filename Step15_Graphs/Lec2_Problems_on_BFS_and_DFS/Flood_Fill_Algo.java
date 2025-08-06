b42b7b72673d91bccc5ff4f891c6326dba41977e
Tushar Kumar <tusharnwd2003@gmail.com>
2025-08-07 00:01:20 +0530

    floodFill

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Flood_Fill_Algo.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Flood_Fill_Algo.java
@@ -2,6 +2,16 @@ package Lec2_Problems_on_BFS_and_DFS;
 
 public class Flood_Fill_Algo {
     
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int initColor = image[sr][sc];
+        int[][] ans= image;
+
+        int delRow[] = {-1,0,+1,0};
+        int delCol[] = {0,+1,0,-1};
+
+        dfs(sr,sc,ans,image,newColor, delRow,delCol,initColor);
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
