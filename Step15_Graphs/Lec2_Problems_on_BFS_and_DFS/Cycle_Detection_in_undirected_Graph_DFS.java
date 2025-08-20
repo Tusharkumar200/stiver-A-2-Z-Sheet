a876043c7a76cc0ee527962a768507f3b8831c57
Tushar Kumar <tusharnwd2003@gmail.com>
2025-08-20 23:51:19 +0530

    function created

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_DFS.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_DFS.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Cycle_Detection_in_undirected_Graph_DFS {
     
+    static boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, int s,
+            boolean vis[], int parent[])
+    {
+
+    }
+
+     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        
+    }
     public static void main(String[] args) {
         
     }
