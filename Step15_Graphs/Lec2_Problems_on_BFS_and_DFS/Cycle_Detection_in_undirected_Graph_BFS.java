407c2e29c6ce7d6806f939e296a9d98faccb7eb4
Tushar Kumar <tusharnwd2003@gmail.com>
2025-08-21 22:10:11 +0530

    bfs traversel

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_BFS.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_BFS.java
@@ -13,26 +13,26 @@ class Node {
     }
 }
 
-public class Cycle_Detection_in_undirected_Graph_DFS {
+public class Cycle_Detection_in_undirected_Graph_BFS {
 
     static boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, int s,
             boolean vis[], int parent[]) {
-        Queue<Node> q = new LinkedList<>(); // BFS
+        Queue<Node> q = new LinkedList<>();
         q.add(new Node(s, -1));
         vis[s] = true;
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
 
             int node = q.peek();
             int par = q.peek().second;
             q.remove();
 
-            for(Integer it:adj.get(node)){
-                if(vis[it] == false){
+            for (Integer it : adj.get(node)) {
+                if (vis[it] == false) {
                     q.add(new Node(it, node));
                     vis[it] = true;
-                }
-                else if(par != it) return true;
+                } else if (par != it)
+                    return true;
             }
         }
         return false;
@@ -52,9 +52,24 @@ public class Cycle_Detection_in_undirected_Graph_DFS {
                 return true;
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
 
+        Cycle_Detection_in_undirected_Graph_BFS obj = new Cycle_Detection_in_undirected_Graph_BFS();
+        boolean ans = obj.isCycle(4, adj);
+        if (ans)
+            System.out.println("1");
+        else
+            System.out.println("0");
     }
 }
