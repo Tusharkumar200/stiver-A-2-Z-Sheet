d5d658cf651b33e098aca3a405696f78133d3dca
Tushar Kumar <tusharnwd2003@gmail.com>
2025-08-22 23:06:31 +0530

    dfs method to find the cycle detection in a undirected graph.

--- Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_DFS.java
+++ Step15_Graphs/Lec2_Problems_on_BFS_and_DFS/Cycle_Detection_in_undirected_Graph_DFS.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Cycle_Detection_in_undirected_Graph_DFS {
     
+    private boolean dfs(int node, int parent, int vis[], ArrayList<ArrayList<Integer>> adj) {
+
+        return false;
+    }
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        return false;
+    }
     public static void main(String[] args) {
         
     }
